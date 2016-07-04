3916b06815585de076488ec5ad56d52f97cb7bee
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-07-03 20:29:38 -0400

    fix for some stat effects
    seems that effect.configParameter now has to be replaced with
    
    config.getParameter

--- stats/effects/fu_tileeffects/pus/puseffect.lua
+++ stats/effects/fu_tileeffects/pus/puseffect.lua
@@ -1,8 +1,8 @@
 function init()
   script.setUpdateDelta(5)
   activateVisualEffects()
-  _x = effect.configParameter("healthDown", 0)
-baseValue = effect.configParameter("healthDown",0)*(status.resourceMax("health"))
+  _x = config.getParameter("healthDown", 0)
+baseValue = config.getParameter("healthDown",0)*(status.resourceMax("health"))
 
   if (status.resourceMax("health")) * _x >= 100.0 then
      effect.addStatModifierGroup({{stat = "maxHealth", amount = baseValue }})
