a49ad1e49f803dd0d2e5141a92e62e13d6a78b39
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-07-20 14:58:10 -0400

    proto weather adjust

--- stats/effects/fu_weathereffects/protoworld/protoweather.lua
+++ stats/effects/fu_weathereffects/protoworld/protoweather.lua
@@ -16,8 +16,32 @@ function init()
   local bounds = mcontroller.boundBox()
   effect.setParentDirectives("fade=ffea00=0.027")
   baseValue = config.getParameter("biomeDmgPerTick")
+  activateVisualEffects()
 end
 
+
+function activateVisualEffects()
+  effect.setParentDirectives("fade=306630=0.8")
+  local statusTextRegion = { 0, 1, 0, 1 }
+  animator.setParticleEmitterOffsetRegion("statustext", statusTextRegion)
+  animator.burstParticleEmitter("statustext")
+end
+
+
+function makeAlert()
+        world.spawnProjectile(
+          "teslaboltsmall",
+          mcontroller.position(),
+          entity.id(),
+          directionTo,
+          false,
+          {
+            power = 0,
+            damageTeam = sourceDamageTeam
+          }
+        )
+        animator.playSound("bolt")
+end
 
 function getLight()
   local position = mcontroller.position()
@@ -31,61 +55,88 @@ end
 function update(dt)
       local lightLevel = getLight()
       biomeTimer = biomeTimer - dt
+      
       if biomeTimer <= 0 then
         effect.addStatModifierGroup({{stat = "maxHealth", amount = baseValue* (-1) }})
 		--if lightLevel > 95 then
 		--	biomeDmg = biomeDmg +1
 		--	biomeTimer = 35
- 
+ 		makeAlert()
+ 		  activateVisualEffects()
 		if world.time() <= 0.8 then
 			biomeDmg = biomeDmg +1
 			biomeTimer = 35
+			makeAlert()
+			activateVisualEffects()
 		elseif world.time() <= 0.7 then
  			biomeDmg = biomeDmg +1
  			biomeTimer = 33
+ 			makeAlert()
+ 			activateVisualEffects()
 		elseif world.time() <= 0.6 then
 			biomeDmg = biomeDmg +2
 			biomeTimer = 31
+			makeAlert()
+			activateVisualEffects()
 		elseif world.time() <= 0.5 then
 			biomeDmg = biomeDmg +2
 			biomeTimer = 29
+			makeAlert()
+			activateVisualEffects()
 		elseif world.time() <= 0.4 then
 			biomeDmg = biomeDmg +3
 			biomeTimer = 27
+			makeAlert()
+			activateVisualEffects()
 		elseif world.time() <= 0.3 then
 			biomeDmg = biomeDmg +3
 			biomeTimer = 25
+			makeAlert()
+			activateVisualEffects()
 		elseif world.time() <= 0.2 then
 			biomeDmg = biomeDmg +4
 			biomeTimer = 22
+			makeAlert()
+			activateVisualEffects()
 		elseif world.time() <= 0.1 then
 			biomeDmg = biomeDmg +4
 			biomeTimer = 19
+			makeAlert()
+			activateVisualEffects()
 		elseif world.time() <= 0.05 then
 			biomeDmg = biomeDmg +5
 			biomeTimer = 17
+			makeAlert()
+			activateVisualEffects()
 		else
 			biomeDmg = biomeDmg +5
-			biomeTimer = 15		
+			biomeTimer = 15	
+			makeAlert()
+			activateVisualEffects()
+			
 		end 		
         if world.time() <= 0.5 and biomeDmg <= biomeDay then
           status.applySelfDamageRequest ({
           damageType = "IgnoresDef",
           damage = biomeDmg+5,
-          damageSourceKind = "nitrogenweapon",
+          damageSourceKind = "fire",
           sourceEntityId = entity.id(),  
           biomeTimer = 15	
           })       
+	makeAlert()     
+	activateVisualEffects()
         end
         
         if world.time() > 0.5 and biomeDmg <= biomeNight then
           status.applySelfDamageRequest ({
           damageType = "IgnoresDef",
           damage = biomeDmg,
-          damageSourceKind = "nitrogenweapon",
+          damageSourceKind = "ice",
           sourceEntityId = entity.id(),  
           biomeTimer = 15	
-          })       
+          })   
+	makeAlert()      
+	activateVisualEffects()
         end
       
       
