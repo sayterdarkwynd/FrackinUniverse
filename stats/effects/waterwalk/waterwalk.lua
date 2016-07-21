6c357403e3bf9a23fc55eb20838f5916c0760cf8
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-07-21 10:09:08 -0400

    adjusts

--- stats/effects/waterwalk/waterwalk.lua
+++ stats/effects/waterwalk/waterwalk.lua
@@ -1,13 +1,25 @@
 function init()
   local bounds = mcontroller.boundBox()
   self.liquidMovementParameter = {
-	liquidBuoyancy = 3
+	liquidBuoyancy = 3.2,
+	liquidFriction = 0,
+	runSpeed = 18,
+	minimumLiquidPercentage = 0.2,
+	liquidForce = 100
   }
+  self.groundMovementParameter = {
+	minimumLiquidPercentage = 0.2
+  }  
+   script.setUpdateDelta(5)
 end
 
 function update(dt)
-mcontroller.controlParameters(self.liquidMovementParameter)
-   
+local inLiquid = mcontroller.liquidPercentage() > 0.01
+    if inLiquid then
+      mcontroller.controlParameters(self.liquidMovementParameter)
+    else
+      mcontroller.controlParameters(self.groundMovementParameter)
+    end
 end
 
 function uninit()
