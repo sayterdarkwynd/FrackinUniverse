73e7d1986dbdb3da89474755481643dfa865cf63
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-08-07 00:36:44 -0400

    armor ui changes

--- objects/power/isn_atmosregulator/isn_atmosprotection.lua
+++ objects/power/isn_atmosregulator/isn_atmosprotection.lua
@@ -1,4 +1,6 @@
 function init()
+  status.setPersistentEffects("fu_protoprotection", {{stat = "protoImmunity", amount = 1}}
+  status.setPersistentEffects("isn_airprotection2", {{stat = "waterbreathProtection", amount = 1}})
   status.setPersistentEffects("isn_airprotection", {{stat = "breathProtection", amount = 1}})
   status.setPersistentEffects("isn_radsprotection", {{stat = "biomeradiationImmunity", amount = 1}})
   status.setPersistentEffects("isn_coldprotection", {{stat = "biomecoldImmunity", amount = 1}})
@@ -12,6 +14,7 @@ end
 
 function uninit()
   status.clearPersistentEffects("isn_airprotection")
+  status.clearPersistentEffects("isn_airprotection2")
   status.clearPersistentEffects("isn_radsprotection")
   status.clearPersistentEffects("isn_coldprotection")
   status.clearPersistentEffects("isn_heatprotection")
@@ -19,4 +22,6 @@ function uninit()
   status.clearPersistentEffects("fu_acidprotection")
   status.clearPersistentEffects("fu_coldprotection")
   status.clearPersistentEffects("fu_heatprotection")  
+  status.clearPersistentEffects("fu_protoprotection") 
+  status.clearPersistentEffects("fu_heatprotection") 
 end
