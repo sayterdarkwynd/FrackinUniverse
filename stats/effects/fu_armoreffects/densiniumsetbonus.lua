5ea7d877aeb011667ea6a175adcb4b03b4179808
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-09-12 14:54:08 -0400

    Revert "asffa"
    This reverts commit c28e9389ebdda001a22b619817fa367ff2c9e7bc.

---
+++ stats/effects/fu_armoreffects/densiniumsetbonus.lua
@@ -0,0 +1,19 @@
+require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
+
+function init()
+	setBonusInit("fu_densiniumset", {
+		{stat = "ffextremeheatImmunity", amount = 1},
+		{stat = "biomeheatImmunity", amount = 1},
+		{stat = "protoImmunity", amount = 1},
+		{stat = "fireStatusImmunity", amount = 1},
+		{stat = "extremepressureProtection", amount = 1},
+		{stat = "ffextremecoldImmunity", amount = 1},
+		{stat = "biomecoldImmunity", amount = 1},
+		{stat = "sulphuricImmunity", amount = 1},
+		{stat = "liquidnitrogenImmunity", amount = 1},
+		{stat = "nitrogenfreezeImmunity", amount = 1},
+		{stat = "ffextremeradiationImmunity", amount = 1},
+		{stat = "biomeradiationImmunity", amount = 1},
+		{stat = "sulphuricImmunity", amount = 1}
+	})
+end
