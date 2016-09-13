327b31d832dc580c5aaf96bab137ca2e43fffb95
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-09-13 00:02:20 -0400

    icons

--- stats/effects/fu_armoreffects/brainslug.lua
+++ stats/effects/fu_armoreffects/brainslug.lua
@@ -1,20 +1,21 @@
 function init()
   animator.setParticleEmitterOffsetRegion("slug", mcontroller.boundBox())
   animator.setParticleEmitterActive("slug", true)
+  effect.addStatModifierGroup({{stat = "foodDelta", amount = 0.005}})
+  effect.addStatModifierGroup({{stat = "fallDamageMultiplier", amount = 0.75}})
+  effect.addStatModifierGroup({{stat = "energyRegenBlockTime", amount = 1.35}})
+  effect.addStatModifierGroup({{stat = "energyRegenPercentageRate", amount = 0.08}})  
 end
 
 function update(dt)
   mcontroller.controlParameters({
-        gravityMultiplier = 1.15,
         airForce = 35.0,
         groundForce = 23.5,
-        runSpeed = 17.0,
-      airJumpProfile = {
-        jumpSpeed = 32.5,
-        jumpControlForce = 980.0,
-        jumpInitialPercentage = 0.85
-      },
+        runSpeed = 17.0
     })
+  mcontroller.controlModifiers({
+      speedModifier = 1.40
+    })    
 end
 
 function uninit()
