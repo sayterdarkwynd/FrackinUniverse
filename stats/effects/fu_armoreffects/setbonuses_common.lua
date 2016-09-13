3f9c763a8ce7f656fffb2e804765c57936938092
Not Mr Flibble <NotMrFlibble@users.noreply.github.com>
2016-09-13 16:58:23 +0100

    Add callback support to the armour set bonus code.

--- stats/effects/fu_armoreffects/setbonuses_common.lua
+++ stats/effects/fu_armoreffects/setbonuses_common.lua
@@ -1,11 +1,25 @@
-function setBonusInit(setBonusName, setBonusStats)
+-- For callbacks:
+--   require "/path/to/effect/script.lua"
+--   callbacks = { { init = init, update = update, uninit = uninit } }
+-- repeat as needed:
+--   require "/path/to/another/effect/script.lua"
+--   table.insert(callbacks, { init = init, update = update, uninit = uninit })
+--
+-- Callbacks can be used to implement other scripted bonuses
+--
+-- Pass callbacks to setBonusInit
+-- If animation is needed, reference the animation file in the statuseffect file (copy from the included script's corresponding file)
+-- Limitations: no icons, scripts won't see their own config, only one animation file?
+
+function setBonusInit(setBonusName, setBonusStats, callbacks)
 	self.statGroup = nil
 	self.armourPresent = nil
 	self.setBonusName = setBonusName
 	self.setBonusCheck = { setBonusName .. '_head', setBonusName .. '_chest', setBonusName .. '_legs' }
 	self.setBonusStats = setBonusStats
+	self.callbacks = callbacks or {}
 
-sb.logInfo("init for %s\nchecking for %s", setBonusName, self.setBonusCheck)
+	--sb.logInfo("init for %s\nchecking for %s", setBonusName, self.setBonusCheck)
 end
 
 function update()
@@ -13,14 +27,23 @@ function update()
 
 	local newstate = status.stat(self.setBonusCheck[1]) == 1 and status.stat(self.setBonusCheck[2]) == 1 and status.stat(self.setBonusCheck[3]) == 1
 
-	if self.armourPresent == newstate then return end
+	if self.armourPresent == newstate then
+		for _, callback in pairs(self.callbacks) do
+			if callback.update then callback.update() end
+		end
+		return
+	end
 	self.armourPresent = newstate
 
 	if self.armourPresent then
 		if self.statGroup then effect.removeStatModifierGroup(self.statGroup) end -- shouldn't happen
 
 		self.statGroup = effect.addStatModifierGroup(self.setBonusStats)
-		 
+
+		for _, callback in pairs(self.callbacks) do
+			if callback.init then callback.init() end
+		end
+
 		--effect.setParentDirectives("fade="..config.getParameter("color").."=0.5")
 
 		sb.logInfo("Set bonus active: %s", self.setBonusName)
@@ -33,6 +56,10 @@ function removeSetBonus()
 	if self.statGroup then
 		effect.removeStatModifierGroup(self.statGroup)
 		self.statGroup = nil
+
+		for _, callback in pairs(self.callbacks) do
+			if callback.uninit then callback.uninit() end
+		end
 
 		--effect.setParentDirectives(nil)
 
