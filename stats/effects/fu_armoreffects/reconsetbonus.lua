3b100a8f9e6e6bc4837472e70d5c29885e222b57
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-09-12 21:27:01 -0400

    recon setup

--- stats/effects/fu_armoreffects/reconsetbonus.lua
+++ stats/effects/fu_armoreffects/reconsetbonus.lua
@@ -1,28 +1,13 @@
 require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
 
 function init()
-    self.powerModifier = config.getParameter("powerModifier", 0)
-    setBonusInit("fu_warangelset", {
+
+    setBonusInit("fu_reconset", {
     {stat = "ffextremeradiationImmunity", amount = 1},
     {stat = "biomeradiationImmunity", amount = 1},
-    {stat = "ffextremeheatImmunity", amount = 1},
-    {stat = "biomeheatImmunity", amount = 1},
-    {stat = "ffextremecoldImmunity", amount = 1},
-    {stat = "biomecoldImmunity", amount = 1},
-    {stat = "sulphuricImmunity", amount = 1},
-    {stat = "fireStatusImmunity", amount = 1},
-    {stat = "nitrogenfreezeImmunity", amount = 1},
     {stat = "breathProtection", amount = 1},
-    {stat = "poisonStatusImmunity", amount = 1},
-    {stat = "pressureProtection", amount = 1},
-    {stat = "extremepressureProtection", amount = 1},
-    {stat = "powerMultiplier", baseMultiplier = self.powerModifier},
-    {stat = "grit", amount = 0.75},
-    {stat = "wetImmunity", amount = 1},
-  mcontroller.controlModifiers({
-    groundMovementModifier = 1.22,
-	runModifier = 1.22,
-	jumpModifier = 1.15
-  })
+    {stat = "grit", amount = 0.25},
+    {stat = "wetImmunity", amount = 1}
 	})
+	
 end
