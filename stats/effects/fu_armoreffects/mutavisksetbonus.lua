e1caddf3bf626f96e4066db1f5b6fc8240a25a0f
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-10-04 12:31:06 -0400

    shoggoth dungeon updates
    also mutavisk nerf/set bonus

---
+++ stats/effects/fu_armoreffects/mutavisksetbonus.lua
@@ -0,0 +1,14 @@
+
+
+require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
+
+function init()
+	setBonusInit("fu_mutaviskset", {
+		{stat = "ffextremeradiationImmunity", amount = 1},
+		{stat = "radiationburnImmunity", amount = 1},
+		{stat = "biomeradiationImmunity", amount = 1},
+		{stat = "fallDamageMultiplier", effectiveMultiplier = 0.5}
+	}
+	
+	)
+end
