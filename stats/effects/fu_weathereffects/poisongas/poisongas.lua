39bf4be030da6c7a5b5bff49c0952842dcf36a8f
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-10-16 12:28:07 -0400

    poison gas changes

---
+++ stats/effects/fu_weathereffects/poisongas/poisongas.lua
@@ -0,0 +1,65 @@
+function init()
+  script.setUpdateDelta(5)
+
+  animator.setParticleEmitterOffsetRegion("coldbreath", mcontroller.boundBox())
+  animator.setParticleEmitterActive("coldbreath", true)
+  
+  --config --
+  decreasePower = config.getParameter("decreasePower",0)
+  biomeTimer = 5
+  local bounds = mcontroller.boundBox()
+  effect.setParentDirectives("fade=ffea00=0.027")
+  baseValue = config.getParameter("biomeDmgPerTick")
+  activateVisualEffects()
+end
+
+
+function activateVisualEffects()
+  effect.setParentDirectives("fade=306630=0.8")
+  local statusTextRegion = { 0, 1, 0, 1 }
+  animator.setParticleEmitterOffsetRegion("statustext", statusTextRegion)
+  animator.burstParticleEmitter("statustext")
+end
+
+
+function makeAlert()
+        world.spawnProjectile(
+          "teslaboltsmall",
+          mcontroller.position(),
+          entity.id(),
+          directionTo,
+          false,
+          {
+            power = 0,
+            damageTeam = sourceDamageTeam
+          }
+        )
+        animator.playSound("bolt")
+end
+
+
+  
+function update(dt)
+      biomeTimer = biomeTimer - dt
+      if biomeTimer <= 0 and status.stat("powerMultiplier") >= 1 then
+        effect.addStatModifierGroup({{stat = "maxHealth", amount = baseValue* (-1) }})
+        effect.addStatModifierGroup({{stat = "maxEnergy", amount = baseValue* (-1) }})
+	biomeTimer = 5	
+          status.applySelfDamageRequest ({
+          damageType = "IgnoresDef",
+          damage = 7,
+          damageSourceKind = "poison",
+          sourceEntityId = entity.id(),  
+          biomeTimer = 2	
+          })	
+	makeAlert()
+	activateVisualEffects()
+      end	
+
+       
+
+end
+
+function uninit()
+
+end
--- stats/effects/liquidimmunity/gasimmunity.lua
+++ stats/effects/liquidimmunity/gasimmunity.lua
@@ -1,7 +1,9 @@
 function init()
   effect.addStatModifierGroup({
     {stat = "helium3Immunity", amount = 1},
-    {stat = "shadowgasImmunity", amount = 1}
+    {stat = "shadowgasImmunity", amount = 1},
+    {stat = "metallichydrogengasImmunity", amount = 1},
+    {stat = "poisongasImmunity", amount = 1}
   })
 end
 
