df90aee2552c3aa6ac5df6619fe5eae6a85a6f87
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-10-19 19:06:14 -0400

    shield UI improvements

---
+++ items/buildscripts/buildunrandshield.lua
@@ -0,0 +1,28 @@
+require "/scripts/util.lua"
+
+function build(directory, config, parameters, level, seed)
+  local configParameter = function(keyName, defaultValue)
+    if parameters[keyName] ~= nil then
+      return parameters[keyName]
+    elseif config[keyName] ~= nil then
+      return config[keyName]
+    else
+      return defaultValue
+    end
+  end
+
+  if level and not configParameter("fixedLevel", true) then
+    parameters.level = level
+  end
+
+  -- set price
+  config.price = configParameter("price", 0) * root.evalFunction("itemLevelPriceMultiplier", configParameter("level", 1))
+
+  -- tooltip fields
+  config.tooltipFields = {}
+  config.tooltipFields.healthLabel = util.round(configParameter("baseShieldHealth", 0) * root.evalFunction("shieldLevelMultiplier", configParameter("level", 1)), 0)
+  config.tooltipFields.cooldownLabel = parameters.cooldownTime or config.cooldownTime
+  config.tooltipFields.blockLabel = configParameter("perfectBlockTime") * 100
+  config.tooltipFields.stamLabel = configParameter("shieldStamina") * 100
+  return config, parameters
+end
