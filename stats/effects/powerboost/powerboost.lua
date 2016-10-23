b705643e5de444daa87f3ef4647554015dbe9908
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-10-23 10:27:11 -0400

    iconses

--- stats/effects/powerboost/powerboost.lua
+++ stats/effects/powerboost/powerboost.lua
@@ -5,16 +5,16 @@ function init()
   effect.addStatModifierGroup({{stat = "invulnerable", amount = 1}})
 
   self.powerModifier = config.getParameter("powerModifier", 0)
-  effect.addStatModifierGroup({{stat = "powerMultiplier", effectiveMultiplier = 3}})
+  effect.addStatModifierGroup({{stat = "powerMultiplier", effectiveMultiplier = 2}})
 
   effect.addStatModifierGroup({
       {stat = "energyRegenPercentageRate", amount = -1},
       {stat = "energyRegenBlockTime", effectiveMultiplier = 0}
     })
-
-  script.setUpdateDelta(0)
+  local bounds = mcontroller.boundBox()
+  script.setUpdateDelta(5)
 end
 
 function update(dt)
-  mcontroller.controlModifiers({speedModifier = 0.7})
+  mcontroller.controlModifiers({speedModifier = 0.75})
 end
