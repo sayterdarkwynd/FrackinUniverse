589e74860757e9a180c1ed8c7fece71844c16c30
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-10-27 16:32:06 -0400

    test

--- stats/effects/fu_effects/fleshreaper/fleshreaper.lua
+++ stats/effects/fu_effects/fleshreaper/fleshreaper.lua
@@ -1,24 +1,14 @@
 function init() 
-
+  local bounds = mcontroller.boundBox()
   script.setUpdateDelta(10)
 end
 
 function update(dt)
-
-      local params = copy(self.projectileParameters)
-      params.powerMultiplier = activeItem.ownerPowerMultiplier()
-      params.power = params.power * config.getParameter("damageLevelMultiplier")
-      local actualSlamPosition = world.collisionBlocksAlongLine(lastSlamPosition, newSlamPosition,nil,1)[1]
-    --   sb.logInfo("actualSlamPosition = %s, slam length = %s", actualSlamPosition, world.magnitude(lastSlamPosition,newSlamPosition))
-      world.spawnProjectile(self.projectileType, {actualSlamPosition[1],actualSlamPosition[2]+2}, activeItem.ownerEntityId(), {1,0}, false, params)
+      world.spawnProjectile("bahamutboom", mcontroller.position(), 0, {0, 0}, false, projectileConfig)
 end
 
 
 
-
-function slamPosition()
-  return vec2.add(activeItem.handPosition(animator.partPoint("blade", "groundSlamPoint")), mcontroller.position())
-end
 
 
 function uninit()
