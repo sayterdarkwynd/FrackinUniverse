ef22e4b385918c11b6a92808a29e3b1bee3f9f23
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-10-27 16:24:20 -0400

    script base

--- stats/effects/fu_effects/fleshreaper/fleshreaper.lua
+++ stats/effects/fu_effects/fleshreaper/fleshreaper.lua
@@ -1,14 +1,25 @@
 function init() 
-  local bounds = mcontroller.boundBox()
+
   script.setUpdateDelta(10)
 end
 
 function update(dt)
-    if mcontroller.falling() then
-      mcontroller.controlParameters(config.getParameter("fallingParameters"))
-      mcontroller.setYVelocity(math.max(mcontroller.yVelocity(), config.getParameter("maxFallSpeed")))
-    end
+
+      local params = copy(self.projectileParameters)
+      params.powerMultiplier = activeItem.ownerPowerMultiplier()
+      params.power = params.power * config.getParameter("damageLevelMultiplier")
+      local actualSlamPosition = world.collisionBlocksAlongLine(lastSlamPosition, newSlamPosition,nil,1)[1]
+    --   sb.logInfo("actualSlamPosition = %s, slam length = %s", actualSlamPosition, world.magnitude(lastSlamPosition,newSlamPosition))
+      world.spawnProjectile(self.projectileType, {actualSlamPosition[1],actualSlamPosition[2]+2}, activeItem.ownerEntityId(), {1,0}, false, params)
 end
+
+
+
+
+function slamPosition()
+  return vec2.add(activeItem.handPosition(animator.partPoint("blade", "groundSlamPoint")), mcontroller.position())
+end
+
 
 function uninit()
   
