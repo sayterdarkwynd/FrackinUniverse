b00065add340454996afb5a4dceb7b9792bc0d13
bk3000 <bk3k_spam@yahoo.com>
2016-10-29 05:00:51 -0500

    Update
    Efficiency improvement by avoiding needless recalculating every update() tick

--- objects/fu_watcher/fu_watcher.lua
+++ objects/fu_watcher/fu_watcher.lua
@@ -1,14 +1,22 @@
+--update notes from bk3000 aka bk3k
+--
+--objects aren't mobile so why recalculate every tic of update?  Just a waste of CPU cycles doing that.
+--I did it in init()  It probably only needs to recalculate between session changes
+--since only storage.state persists between sessions.  That being the case, 
+--my edit of line 10 will keep the setting between sessions (which I'd find more desirable by far).
+
 function init()
 	object.setInteractive(true)
-	storage.state = false
+	storage.state = storage.state or false
+  if not storage.watchBox then
+    local myLocation = entity.position()
+    storage.watchBox = { myLocation[1]-20, myLocation[2]-20, myLocation[1]+20, myLocation[2]+20 }
+  end
 end
 
 function update(dt)
-	local myLocation = entity.position()
-	local watchBox = { myLocation[1]-20, myLocation[2]-20, myLocation[1]+20, myLocation[2]+20 }
-	-- world.debugPoly( watchBox, "red" ) -- didn't work, rectf is not usable as poly
 	if storage.state then
-		world.loadRegion( watchBox )
+		world.loadRegion( storage.watchBox )
 	end
 end
 
