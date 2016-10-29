3ba718fe021842d43cc26928dcd3f3d2e848eddd
bk3000 <bk3k_spam@yahoo.com>
2016-10-29 03:56:17 -0500

    Update isn_solarpanel.lua
    efficiency improvement, also request to check that intent of code matches result

--- objects/power/isn_solarpanel/isn_solarpanel.lua
+++ objects/power/isn_solarpanel/isn_solarpanel.lua
@@ -1,48 +1,57 @@
+--edit notes from bk3000 aka bk3k
+--
+--efficiency improvement by not calculating truepos over and over when the location won't change!
+--plus a small improvement in update()
+--and make sure line 34 - 36 is as intended... seems like that might not me and could be done more efficiently by
+--checking against larger values first combined with if, elseif, elseif, elseif to set the actual desired value
+
 function init(virtual)
 	if virtual == true then return end
 	storage.checkticks = 0
-	storage.truepos = isn_getTruePosition()
+	storage.truepos = storage.truepos or {entity.position()[1] + math.random(2,3), entity.position()[2] + 1}
+  --if not nil, no reassignment.  If nil then gets assigned
+  --although I honestly do no understand why the random 'x' offset but I left it intact
 end
 
 function update(dt)
 	storage.checkticks = storage.checkticks + 1
-	if storage.checkticks >= 10 then
+	--if storage.checkticks >= 10 then
+  if storage.checkticks > 9 then --slightly more efficient because >= is equivalent to n > v or n == v
 		storage.checkticks = 0
 		isn_getCurrentPowerOutput()
 	end
 end
 
 function isn_getCurrentPowerOutput(divide)
-	if isn_powerGenerationBlocked == true then
+	if isn_powerGenerationBlocked then
 		animator.setAnimationState("meter", "0")
 		return 0
 	end
 	
 	local generated = 0
-	local genmult = 1
-	local location = isn_getTruePosition()
+	local genmult = 2
 	local light = world.lightLevel(location)
-	-- sb.logInfo("solar panel location is %s", location)
-	-- sb.logInfo("light level is %s", light)
 	if light > 0.1 then generated = generated + 0.25 end
-	if light > 0.3 then generated = generated + 0.25 end
-	if light > 0.5 then generated = generated + 0.25 end
-	if light > 0.7 then generated = generated + 0.25 end
+	if light > 0.3 then generated = generated + 0.35 end
+	if light > 0.5 then generated = generated + 0.45 end
+	if light > 0.7 then generated = generated + 0.55 end
+  --just to check your intention here, the 4 checks are no mutually exclusive so if light > 0.7 then 
+  --generated is going to actually increase 1.6 in total, not 0.55 thanks to a compound increase
 	
-	if location[2] < 500 then genmult = 0.5
-	elseif location[2] > 900 then genmult = 2
-	elseif location[2] > 600 then genmult = 1.5 end
+	if storage.truepos[2] < 500 then genmult = 1
+	elseif storage.truepos[2] > 900 then genmult = 5 
+	elseif storage.truepos[2] > 700 then genmult = 3 end
 	
 	generated = generated * genmult
-	generated = math.min(generated,2)
-
+	generated = math.min(generated,6)
+	
 	local summationForDebug = string.format("P %.2f L %.2f", generated, light)
 	world.debugText(summationForDebug,{location[1]-(string.len(summationForDebug)*0.25),location[2]-3.5},"cyan")
 	
-	if generated >= 2 then animator.setAnimationState("meter", "4")
-	elseif generated >= 1.5  then animator.setAnimationState("meter", "3")
-	elseif generated >= 1 then animator.setAnimationState("meter", "2")
-	elseif generated >= 0.5 then animator.setAnimationState("meter", "1")
+	if generated >= 4 then animator.setAnimationState("meter", "4")
+	elseif generated >= 3  then animator.setAnimationState("meter", "3")
+	elseif generated >= 2 then animator.setAnimationState("meter", "2")
+	elseif generated >= 1 then animator.setAnimationState("meter", "1")
 	else animator.setAnimationState("meter", "0")
 	end
 	
@@ -61,17 +70,16 @@ end
 function isn_powerGenerationBlocked()
 	-- Power generation does not occur if...
 	--if world.info == nil then return true end -- it's on a ship (doesn't work right now)
-	local location = isn_getTruePosition()
-	if world.underground(location) == true then return true end -- it's underground
-	if world.liquidAt(location) == true then return true end -- it's submerged in liquid
-	if world.tileIsOccupied(location,false) == true then return true end -- there's a wall in the way
-	if world.lightLevel(location) < 0.2 then return true end -- not enough light
+	local location = storage.truepos
+	if world.underground(location) or world.liquidAt(location) or world.tileIsOccupied(location,false) or 
+    world.lightLevel(location) < 0.2 then return true 
+  else return false --no false return when the other option is true?!
+  end 
+  --LUA won't call what's to the right of 'or' if it got value beyond false or nil to the left of it
+  --so making so many seperate statements isn't necessary so I combined them.
+  --strickly speaking that probably wasn't necessary to do
 end
 
-function isn_getTruePosition()
-	if storage.truepos ~= nil then return storage.truepos
-	else
-		storage.truepos = {entity.position()[1] + math.random(2,3), entity.position()[2] + 1}
-		return storage.truepos
-	end
+function isn_getTruePosition()  --in case this is called externally, otherwise I'd delete it
+	return storage.truepos
 end
