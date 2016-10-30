8f0958f32fbd2f2da74a88356525f82ff9d4e074
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-10-30 16:02:17 -0400

    new quest typo fixes, text coloring

---
+++ stats/effects/fu_effects/feedpack/feedpack.lua
@@ -0,0 +1,11 @@
+function init() 
+effect.addStatModifierGroup({{stat = "foodDelta", amount = config.getParameter("regenAmount", 0)}})
+end
+
+function update(dt)
+
+end
+
+function uninit()
+  
+end
