38cd60e52806bcf33855372196f2618aa663315c
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-10-31 07:42:40 -0400

    add secondary pressure effect to valyrie set

--- stats/effects/fu_armoreffects/valkyriesetbonus.lua
+++ stats/effects/fu_armoreffects/valkyriesetbonus.lua
@@ -5,6 +5,7 @@ require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
 function init()
 	setBonusInit("fu_valkyrieset", {
 		{stat = "breathImmunity", amount = 1},
+		{stat = "pressureProtection", amount = 1},
 		{stat = "extremepressureProtection", amount = 1},
 		{stat = "fallDamageMultiplier", effectiveMultiplier = 0.1}
 	}
