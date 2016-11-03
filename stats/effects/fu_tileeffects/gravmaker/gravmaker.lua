a6b566116633b0dce4642cc7f29852d6c3ec03ab
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-11-02 20:00:36 -0400

    grav generator in and workin!

--- stats/effects/fu_tileeffects/gravmaker/gravmaker.lua
+++ stats/effects/fu_tileeffects/gravmaker/gravmaker.lua
@@ -1,30 +1,28 @@
 function init()
   self.gravityModifier = config.getParameter("gravityModifier")
   self.movementParams = mcontroller.baseParameters()
-
+  effect.addStatModifierGroup({
+    {stat = "asteroidImmunity", amount = 1}
+  })
   setGravityMultiplier()
-
   activateVisualEffects()
+  
+  script.setUpdateDelta(5)
 end
 
 function setGravityMultiplier()
   local oldGravityMultiplier = self.movementParams.gravityMultiplier or 1
-
   self.newGravityMultiplier = self.gravityModifier * oldGravityMultiplier
 end
 
 function activateVisualEffects()
-  --animator.setParticleEmitterOffsetRegion("embers", mcontroller.boundBox())
-  --animator.setParticleEmitterActive("embers", true)
   local statusTextRegion = { 0, 1, 0, 1 }
   animator.setParticleEmitterOffsetRegion("statustext", statusTextRegion)
   animator.burstParticleEmitter("statustext")
 end
 
 function update(dt)
-  mcontroller.controlParameters({
-     gravityMultiplier = self.newGravityMultiplier
-  })
+  mcontroller.controlParameters( { gravityMultiplier = self.newGravityMultiplier } )
 end
 
 function uninit()
