c8493a3761997b6bb5cb2869e324c571be0d804b
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-11-17 09:28:10 -0500

    gas effects changed
    nitrogen, hydrogen and poison gases cause different movement effects.
    Jump is noticably different, but otherwise you should be able to move
    around fairly well

---
+++ stats/effects/fu_tileeffects/gas/gas.lua
@@ -0,0 +1,42 @@
+function init()
+  self.gravityModifier = config.getParameter("gravityModifier")
+  self.movementParams = mcontroller.baseParameters()
+  activateVisualEffects()
+  self.liquidMovementParameter = {
+    gravityMultiplier = 1.5,
+    groundForce = 100,
+    airForce = 20,
+    airFriction = 0,
+    liquidForce = 100,
+    liquidFriction = 0.0,
+    liquidImpedance = 0.01,
+    liquidBuoyancy = 0.01,
+    liquidJumpProfile = {
+      jumpSpeed = 45.0,
+      jumpControlForce = 900.0,
+      jumpInitialPercentage = 1,
+      jumpHoldTime = 0.0,
+      multiJump = false,
+      autoJump = false,
+      reJumpDelay = 0.5
+    }
+  }  
+end
+
+
+
+function activateVisualEffects()
+  animator.setParticleEmitterOffsetRegion("embers", mcontroller.boundBox())
+  animator.setParticleEmitterActive("embers", true)
+  local statusTextRegion = { 0, 1, 0, 1 }
+  animator.setParticleEmitterOffsetRegion("statustext", statusTextRegion)
+  animator.burstParticleEmitter("statustext")
+end
+
+function update(dt)
+  mcontroller.controlParameters(self.liquidMovementParameter)
+end
+
+function uninit()
+
+end
