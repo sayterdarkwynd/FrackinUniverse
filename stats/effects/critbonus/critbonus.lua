9053586660345607422023c222ce172220192190
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-12-02 16:41:40 -0500

    meta

--- stats/effects/critbonus/critbonus.lua
+++ stats/effects/critbonus/critbonus.lua
@@ -2,7 +2,7 @@ function init()
  --Power
   self.critChanceup = config.getParameter("critChanceMultiplier", 0)
   effect.addStatModifierGroup({{stat = "critChanceMultiplier", baseMultiplier = self.critChanceup}})
-  --status.setStatusProperty("critChanceModifier", 0.66)
+  --status.setStatusProperty("critChanceModifier", 0.2)
   local statusTextRegion = { 0, 1, 0, 1 }
   animator.setParticleEmitterOffsetRegion("statustext", statusTextRegion)
   animator.burstParticleEmitter("statustext")
