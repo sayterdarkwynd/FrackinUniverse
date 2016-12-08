203527dc7d432e0f3ea7deb9cfd09710be76fc45
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-12-07 23:48:40 -0500

    fixes

--- stats/effects/fu_effects/starspawn/starspawntentacle.lua
+++ stats/effects/fu_effects/starspawn/starspawntentacle.lua
@@ -3,11 +3,9 @@ function init()
   
   
   --if mcontroller.onGround() then
-      mcontroller.controlModifiers({airJumpModifier = self.airJumpModifier})
       local projectileConfig = { power = 12 }
       animator.playSound("bombdrop")  
       world.spawnProjectile("tentacleattack", mcontroller.position(), 0, {0, 0}, false, projectileConfig)    
-      status.addPersistentEffect( "rootfu", math.huge)
   --end  
   
 end
