acf6f071a6c014e3809b5d129ff592442ec29e7d
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-12-09 16:25:38 -0500

    added rooting to several objects

--- stats/effects/fu_armoreffects/bonesetbonus.lua
+++ stats/effects/fu_armoreffects/bonesetbonus.lua
@@ -4,7 +4,8 @@ function init()
     if (world.type() == "garden") or (world.type() == "forest") or (world.type() == "desert")  then
 	    setBonusInit("fu_boneset", {
 	      {stat = "maxHealth", baseMultiplier = 1.05},
-	      {stat = "powerMultiplier", baseMultiplier = 1.05}
+	      {stat = "powerMultiplier", baseMultiplier = 1.05},
+	      {stat = "physicalResistance", baseMultiplier = 1.05}
 	    })  
     end  	
 end
