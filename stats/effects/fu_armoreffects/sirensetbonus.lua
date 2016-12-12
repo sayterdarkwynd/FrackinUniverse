c466b1d34454ff4454c68ece019ac6e6617d866b
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-12-11 20:40:18 -0500

    armor/weapon changes
    adjusted Siren armor to not be completely pointless or crappy. It is now
    a useful early-tier water exploration armor.
    
    pyreite dagger element fixed
    
    kelpsteel hatchet relocated to the correct tier

---
+++ stats/effects/fu_armoreffects/sirensetbonus.lua
@@ -0,0 +1,19 @@
+require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
+
+function init()
+    if (world.type() == "ocean") or (world.type() == "tidewater") or (world.type() == "strangesea")  then
+	    setBonusInit("fu_sirenset", {
+	      {stat = "maxEnergy", baseMultiplier = 1.15},
+	      {stat = "shieldStaminaRegen", baseMultiplier = 1.4},
+	      {stat = "maxBreath", baseMultiplier = 5},
+	      {stat = "poisonResistance", baseMultiplier = 1.30}
+	    }) 
+       else
+	    setBonusInit("fu_sirenset", {
+	      {stat = "maxEnergy", baseMultiplier = 1.05},
+	      {stat = "shieldStaminaRegen", baseMultiplier = 1.25},
+	      {stat = "maxBreath", baseMultiplier = 2},
+	      {stat = "poisonResistance", baseMultiplier = 1.15}
+	    })        
+    end  	
+end
