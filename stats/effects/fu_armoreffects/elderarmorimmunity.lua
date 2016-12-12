1e93e16c5baa3111bc902593120421bb0716db51
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-12-12 09:27:14 -0500

    updates to some armor
    also different effect than hitspark for shadow attacks

--- stats/effects/fu_armoreffects/elderarmorimmunity.lua
+++ stats/effects/fu_armoreffects/elderarmorimmunity.lua
@@ -18,8 +18,10 @@ function init()
     {stat = "slushslowImmunity", amount = 1},
     {stat = "snowslowImmunity", amount = 1},
     {stat = "protoImmunity", amount = 1},
+    {stat = "shadowImmunity", amount = 1},
+    {stat = "pressureImmunity", amount = 1},    
     {stat = "shadowResistance", amount = 0.7},
-    {stat = "shadowImmunity", amount = 1}
+    {stat = "physicalResistance", amount = 0.4}
   })
 end
 
