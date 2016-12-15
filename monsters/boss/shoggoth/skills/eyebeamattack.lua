e85207687444342b1f1abd8cca8557f51ef5924e
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-12-14 21:18:34 -0500

    fixed defense augment unlock

--- monsters/boss/shoggoth/skills/eyebeamattack.lua
+++ monsters/boss/shoggoth/skills/eyebeamattack.lua
@@ -8,7 +8,7 @@ function eyeBeamAttack.enter()
 
   return {
     timer = config.getParameter("eyeBeamAttack.skillTime", 2),
-    damagePerSecond = config.getParameter("eyeBeamAttack.damagePerSecond", 1600),
+    damagePerSecond = config.getParameter("eyeBeamAttack.damagePerSecond", 200),
     distanceRange = config.getParameter("eyeBeamAttack.distanceRange"),
     winddownTimer = config.getParameter("eyeBeamAttack.winddownTime"),
     windupTimer = config.getParameter("eyeBeamAttack.windupTime"),
