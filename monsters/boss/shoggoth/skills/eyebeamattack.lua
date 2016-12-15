2af1c1a2d320204724948764859b70608d8fa3ad
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-12-14 21:24:40 -0500

    shoggoth beam adjustment
    the beam attack should now be survivable

--- monsters/boss/shoggoth/skills/eyebeamattack.lua
+++ monsters/boss/shoggoth/skills/eyebeamattack.lua
@@ -8,7 +8,7 @@ function eyeBeamAttack.enter()
 
   return {
     timer = config.getParameter("eyeBeamAttack.skillTime", 2),
-    damagePerSecond = config.getParameter("eyeBeamAttack.damagePerSecond", 200),
+    damagePerSecond = config.getParameter("eyeBeamAttack.damagePerSecond", 2),
     distanceRange = config.getParameter("eyeBeamAttack.distanceRange"),
     winddownTimer = config.getParameter("eyeBeamAttack.winddownTime"),
     windupTimer = config.getParameter("eyeBeamAttack.windupTime"),
