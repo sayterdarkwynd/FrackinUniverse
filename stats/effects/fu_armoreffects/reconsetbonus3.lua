0b36f5997350dd833bbda126c63b3d9a809fd430
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-12-17 23:17:26 -0500

    each tier of recon set now has varying bonuses

---
+++ stats/effects/fu_armoreffects/reconsetbonus3.lua
@@ -0,0 +1,17 @@
+require "/stats/effects/fu_armoreffects/reconstatbonus/reconstatbonus3.lua"
+callbacks = { { init = init, update = update, uninit = uninit } }
+require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
+
+function init()
+
+    setBonusInit("fu_reconset3", {
+    {stat = "ffextremeradiationImmunity", amount = 1},
+    {stat = "biomeradiationImmunity", amount = 1},
+    {stat = "breathProtection", amount = 1},
+    {stat = "grit", amount = 0.25},
+    {stat = "wetImmunity", amount = 1},
+    {stat = "poisonResistance", amount = 0.25},
+    {stat = "electricResistance", amount = 0.15}
+	},callbacks)
+	
+end
