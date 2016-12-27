9789a149d72f82f40cc990c955bc65352507072c
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-12-27 14:07:01 -0500

    fixes and adjustments

---
+++ stats/effects/fu_armoreffects/arcticparkasetbonus.lua
@@ -0,0 +1,21 @@
+require "/stats/effects/slowfall/slowfall.lua"
+callbacks = { { init = init, update = update, uninit = uninit } }
+
+require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
+
+function init()
+	setBonusInit("fu_arcticparkaset", {
+		{stat = "protoImmunity", amount = 1},
+		{stat = "ffextremecoldImmunity", amount = 1},
+		{stat = "biomecoldImmunity", amount = 1},
+		{stat = "shadowImmunity", amount = 1},
+		{stat = "breathProtection", amount = 1},
+		{stat = "waterbreathProtection", amount = 1},
+    		{stat = "slushslowImmunity", amount = 1},
+    		{stat = "iceslipImmunity", amount = 1},
+    		{stat = "snowslowImmunity", amount = 1},		
+                {stat = "liquidnitrogenImmunity", amount = 1},
+                {stat = "nitrogenfreezeImmunity", amount = 1}
+	},
+	callbacks )
+end
