83b45deba23b13d9311219bf905e47e169181652
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-12-27 09:29:43 -0500

    a

--- stats/effects/fu_armoreffects/pyreitesetbonus.lua
+++ stats/effects/fu_armoreffects/pyreitesetbonus.lua
@@ -4,11 +4,11 @@ function init()
 	setBonusInit("fu_pyreiteset", {
             {stat = "ffextremecoldImmunity", amount = 1},
             {stat = "biomecoldImmunity", amount = 1},
+	    {stat = "fireStatusImmunity", amount = 1},
+	    {stat = "shadowImmunity", amount = 1},            
 	    {stat = "physicalResistance", baseMultiplier = 0.15},
 	    {stat = "fireResistance", baseMultiplier = 0.75},
 	    {stat = "shadowResistance", baseMultiplier = 0.15},
-	    {stat = "iceResistance", baseMultiplier = 0.50},
-	    {stat = "fireStatusImmunity", amount = 1},
-	    {stat = "shadowImmunity", amount = 1}
+	    {stat = "iceResistance", baseMultiplier = 0.50}
 	})
 end
