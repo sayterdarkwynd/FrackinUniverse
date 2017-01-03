9a6b4286e39f3ee96e08dd0a57a065db27e074c1
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-01-02 20:13:18 -0500

    bog and spacefarer

--- stats/effects/fu_armoreffects/spacefarersetbonus.lua
+++ stats/effects/fu_armoreffects/spacefarersetbonus.lua
@@ -4,8 +4,9 @@ callbacks = { { init = init, update = update, uninit = uninit } }
 require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
 
 function init()
-  
-    setBonusInit("fu_spacefarerset", {
+    if (world.type() == "asteroids") or (world.type() == "barrenasteroids") or (world.type() == "iceasteroids") or (world.type() == "elderasteroid") or (world.type() == "corruptasteroid") then
+    
+        setBonusInit("fu_spacefarerset", {
 	    {stat = "fireStatusImmunity", amount = 1},
 	    {stat = "maxBreath", baseMultiplier = 50.0 },
 	    {stat = "slushslowImmunity", amount = 1},
@@ -17,5 +18,23 @@ function init()
 	    {stat = "asteroidImmunity", amount = 1},
 	    {stat = "physicalResistance", amount = 0.25}
 	},
-	callbacks)
+	callbacks)    
+    
+    
+    else
+        setBonusInit("fu_spacefarerset", {
+	    {stat = "fireStatusImmunity", amount = 1},
+	    {stat = "maxBreath", baseMultiplier = 50.0 },
+	    {stat = "slushslowImmunity", amount = 1},
+	    {stat = "protoImmunity", amount = 1},
+	    {stat = "liquidnitrogenImmunity", amount = 1},
+	    {stat = "nitrogenfreezeImmunity", amount = 1},
+	    {stat = "iceslipImmunity", amount = 1},
+	    {stat = "extremepressureProtection", amount = 1},
+	    {stat = "asteroidImmunity", amount = 1},
+	    {stat = "physicalResistance", amount = 0.25}
+	})    
+    
+    end
+
 end
