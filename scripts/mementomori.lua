355436e252cbbd0f41ff5848defc0b9a2eb60e91
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-01-06 23:14:10 -0500

    memento mori added

---
+++ scripts/mementomori.lua
@@ -0,0 +1,24 @@
+mementomori = {}
+
+mementomori.deathPositionKey = "mementomori.lastDeathPosition"
+
+function mementomori.interp(a,b,t)
+    return a + ((b-a) * t)
+end
+
+function mementomori.interpRGB(c1,c2,t)
+    return {
+        mementomori.interp(c1[1], c2[1], t),
+        mementomori.interp(c1[2], c2[2], t),
+        mementomori.interp(c1[3], c2[3], t)
+    }
+end
+
+function mementomori.interpRGBA(c1,c2,t)
+    return {
+        mementomori.interp(c1[1], c2[1], t),
+        mementomori.interp(c1[2], c2[2], t),
+        mementomori.interp(c1[3], c2[3], t),
+        mementomori.interp(c1[4], c2[4], t)
+    }
+end
---
+++ scripts/mm_player_primary.lua
@@ -0,0 +1,17 @@
+require "/stats/player_primary.lua"
+require "/scripts/mementomori.lua"
+
+if not _mm_wrapped then
+	_applyDamageRequest = applyDamageRequest
+	_mm_wrapped = true
+end
+
+function applyDamageRequest(damageRequest)
+    local r = _applyDamageRequest(damageRequest)
+    sb.logInfo("r = %s", r)
+    if next(r) ~= nil and r[1].hitType == "kill" then
+        sb.logInfo("mementomori: R.I.P")
+        world.setProperty(mementomori.deathPositionKey, mcontroller.position())
+    end
+    return r
+end
