9917497b96da996d91d75e594e3ef9419ebeb04e
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-01-15 11:19:26 -0500

    fix

--- stats/effects/fu_armoreffects/irradiumsetbonus.lua
+++ stats/effects/fu_armoreffects/irradiumsetbonus.lua
@@ -1,10 +1,10 @@
 require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
 
 function init()
-	setBonusInit("fu_irradiumsetbonus", {
-    {stat = "ffextremecoldImmunity", amount = 1},
-    {stat = "biomecoldImmunity", amount = 1},
-		{stat = "iceResistance", amount = 0.15}
+	setBonusInit("fu_irradiumset", {
+                {stat = "ffextremecoldImmunity", amount = 1},
+                {stat = "biomecoldImmunity", amount = 1},
+		{stat = "iceResistance", amount = 0.15},
 		{stat = "radioactiveResistance", amount = 0.35}
 	})
 end
