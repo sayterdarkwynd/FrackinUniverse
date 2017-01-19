5ccde7b5caadf5ac87cef5d92428c4210ee3666a
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-01-19 10:45:04 -0500

    updates and fixes

--- stats/effects/fu_armoreffects/hellfiresetbonus.lua
+++ stats/effects/fu_armoreffects/hellfiresetbonus.lua
@@ -1,10 +1,11 @@
-require "/stats/effects/liquidimmunity/lavahealing.lua"
-callbacks = { { init = init, update = update, uninit = uninit } }
-
 require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
 
 function init()
-	setBonusInit("fu_hellfireset", {
+    if (world.type() == "infernus") or (world.type() == "infernusdark") or (world.type() == "volcanic") or (world.type() == "magma")  then
+	    setBonusInit("fu_hellfireset", {
+		{stat = "physicalResistance", amount = 0.15},
+		{stat = "maxHealth", baseMultiplier = 1.15},	
+		{stat = "maxEnergy", baseMultiplier = 1.15},	    
 		{stat = "sulphuricacidImmunity", amount = 1},
 		{stat = "sulphuricImmunity", amount = 1},	
 		{stat = "protoImmunity", amount = 1},
@@ -13,8 +14,23 @@ function init()
 		{stat = "ffextremeheatImmunity", amount = 1},
 		{stat = "wetImmunity", amount = 1},
 		{stat = "biomeheatImmunity", amount = 1}
-	},callbacks)
+	    }) 
+       else
+	    setBonusInit("fu_hellfireset", {
+		{stat = "sulphuricacidImmunity", amount = 1},
+		{stat = "sulphuricImmunity", amount = 1},	
+		{stat = "protoImmunity", amount = 1},
+		{stat = "fireStatusImmunity", amount = 1},
+		{stat = "lavaImmunity", amount = 1},
+		{stat = "ffextremeheatImmunity", amount = 1},
+		{stat = "wetImmunity", amount = 1},
+		{stat = "biomeheatImmunity", amount = 1}
+	    })        
+    end  
 	
 end
+
+
+
 
 
--- stats/effects/liquidimmunity/lavahealing.lua
+++ stats/effects/liquidimmunity/lavahealing.lua
@@ -1,18 +1,25 @@
+require "/scripts/vec2.lua"
 function init()
 
   script.setUpdateDelta(5)
 end
 
-
+function isDry()
+local mouthPosition = vec2.add(mcontroller.position(), status.statusProperty("mouthPosition"))
+	if not world.liquidAt(mouthPosition) then
+            status.clearPersistentEffects("lavahealing")
+	    inWater = 0
+	end
+end
 
 function update(dt)
 local mouthPosition = vec2.add(mcontroller.position(), status.statusProperty("mouthPosition"))
 local mouthful = world.liquidAt(mouthposition)
 	if (world.liquidAt(mouthPosition)) and (inWater == 0) and (mcontroller.liquidId()== 2) or (mcontroller.liquidId()== 8)then
             status.setPersistentEffects("lavahealing", {
-              {stat = "physicalResistance", baseMultiplier = 1.20},
-              {stat = "perfectBlockLimit", amount = 2},
-              {stat = "maxHealth", baseMuiltiplier = 1.25}
+              {stat = "physicalResistance", baseMultiplier = 1.25},
+              {stat = "maxEnergy", baseMultiplier = 1.25},
+              {stat = "maxHealth", baseMultiplier = 1.25}
             })
 	    inWater = 1
 	else
