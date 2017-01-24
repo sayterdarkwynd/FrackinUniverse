2c0a20a23155e3b6d41c5c918807ff3a49662a24
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-01-24 13:17:59 -0500

    explorer set fix

--- stats/effects/fu_armoreffects/explorersetbonus.lua
+++ stats/effects/fu_armoreffects/explorersetbonus.lua
@@ -1,8 +1,8 @@
 require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
 
 function init()
-	setBonusInit("fu_bountyhunterset", {
+	setBonusInit("fu_explorerset", {
 		{stat = "iceResistance", amount = 0.15},
-    {stat = "biomecoldImmunity", amount = 1}
+                {stat = "biomecoldImmunity", amount = 1}
 	})
 end
