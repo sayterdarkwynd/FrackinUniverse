2c9672a3f365e4c25e89bd5cc3893eb8f66d42b4
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-02-01 19:13:48 -0500

    well adjustments

--- objects/crafting/farmwell/farmwell.lua
+++ objects/crafting/farmwell/farmwell.lua
@@ -15,29 +15,34 @@ function onInteraction(args)
   if not storage.waterCount then
    storage.waterCount = 0
   end
-  
-  if storage.waterCount < 100 then
-    object.say("Water : "..math.ceil(storage.waterCount).." (Nearly Dry)")
-  elseif storage.waterCount > 600 then
-    object.say("Water : "..math.ceil(storage.waterCount).." (Brimming)")
-  elseif storage.waterCount > 500 then
-    object.say("Water : "..math.ceil(storage.waterCount).." (Full)")
-  elseif storage.waterCount > 400 then
-    object.say("Water : "..math.ceil(storage.waterCount).." (Wet)")
-  elseif storage.waterCount > 300 then
-    object.say("Water : "..math.ceil(storage.waterCount).." (Ample)")
-  elseif storage.waterCount > 200 then
-    object.say("Water : "..math.ceil(storage.waterCount).." (Low)")
-  elseif storage.waterCount > 100 then
-    object.say("Water : "..math.ceil(storage.waterCount).." (Very Low)")
-  end
 
   if storage.waterCount and storage.waterCount > 600 then
     storage.waterCount = 600
   end  
   if storage.waterCount and storage.waterCount > 100 then
     local p = object.position()
-    world.spawnItem("waterbucket", p, 1)
+    world.spawnItem("waterbucket", p, 3)
     storage.waterCount = storage.waterCount - 100
+
+	  if storage.waterCount < 100 then
+	    object.say("Water : "..math.ceil(storage.waterCount).." (Nearly Dry)")
+	  elseif storage.waterCount > 600 then
+	    object.say("Water : "..math.ceil(storage.waterCount).." (Brimming)")
+	  elseif storage.waterCount > 500 then
+	    object.say("Water : "..math.ceil(storage.waterCount).." (Full)")
+	  elseif storage.waterCount > 400 then
+	    object.say("Water : "..math.ceil(storage.waterCount).." (Wet)")
+	  elseif storage.waterCount > 300 then
+	    object.say("Water : "..math.ceil(storage.waterCount).." (Ample)")
+	  elseif storage.waterCount > 200 then
+	    object.say("Water : "..math.ceil(storage.waterCount).." (Low)")
+	  elseif storage.waterCount > 100 then
+	    object.say("Water : "..math.ceil(storage.waterCount).." (Very Low)")
+	  end
+      if randMath >= 99 then
+        world.spawnItem("methanol", p, 5)
+      elseif randMath >=85 then
+        world.spawnItem("algaegreen", p, 5)
+      end
   end
 end
--- objects/crafting/liquidpumpwell/liquidpumpwell.lua
+++ objects/crafting/liquidpumpwell/liquidpumpwell.lua
@@ -38,8 +38,25 @@ function onInteraction(args)
     world.spawnItem("liquidwater", p, 50)
     storage.waterCount = storage.waterCount - 50
     local randMath = math.random(100)
-    if randMath > 99 then
-      world.spawnItem("methanol", p, 5)
-    end
+	  if storage.waterCount < 100 then
+	    object.say("Water : "..math.ceil(storage.waterCount).." (Nearly Dry)")
+	  elseif storage.waterCount > 600 then
+	    object.say("Water : "..math.ceil(storage.waterCount).." (Brimming)")
+	  elseif storage.waterCount > 500 then
+	    object.say("Water : "..math.ceil(storage.waterCount).." (Full)")
+	  elseif storage.waterCount > 400 then
+	    object.say("Water : "..math.ceil(storage.waterCount).." (Wet)")
+	  elseif storage.waterCount > 300 then
+	    object.say("Water : "..math.ceil(storage.waterCount).." (Ample)")
+	  elseif storage.waterCount > 200 then
+	    object.say("Water : "..math.ceil(storage.waterCount).." (Low)")
+	  elseif storage.waterCount > 100 then
+	    object.say("Water : "..math.ceil(storage.waterCount).." (Very Low)")
+	  end    
+      if randMath >= 99 then
+        world.spawnItem("methanol", p, 5)
+      elseif randMath >=85 then
+        world.spawnItem("algaegreen", p, 5)
+      end
   end
 end
