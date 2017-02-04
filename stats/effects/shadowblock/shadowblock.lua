ea1ec2e9eb1531096e11620c6ad2eb2c1aaaae5a
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-02-04 00:00:31 -0500

    new effects

---
+++ stats/effects/shadowblock/shadowblock.lua
@@ -0,0 +1,13 @@
+function init()
+   effect.addStatModifierGroup({{stat = "shadowResistance", amount = 0.25}, {stat = "shadowImmunity", amount = 1}})
+
+   script.setUpdateDelta(0)
+end
+
+function update(dt)
+
+end
+
+function uninit()
+  
+end
