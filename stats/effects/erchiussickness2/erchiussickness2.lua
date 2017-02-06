25639cfcbd9fafcb89e00bb5552e49dbff8d7c9e
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-02-06 08:09:55 -0500

    adjustments

--- stats/effects/erchiussickness2/erchiussickness2.lua
+++ stats/effects/erchiussickness2/erchiussickness2.lua
@@ -26,6 +26,7 @@ function update(dt)
   local erchiusCount = 0
   erchiusCount = erchiusCount + (world.entityHasCountOfItem(entity.id(), "effigiumore") or 0)
   erchiusCount = erchiusCount + (world.entityHasCountOfItem(entity.id(), "effigiumbar") or 0)
+  erchiusCount = erchiusCount + (world.entityHasCountOfItem(entity.id(), "solidfuel") or 0)
   local erchiusRatio = math.sqrt(math.min(1.0, erchiusCount / self.effectMaxErchius))
   if erchiusCount > 0 and self.spawnTimer > 0 then
     self.spawnTimer = self.spawnTimer - dt
