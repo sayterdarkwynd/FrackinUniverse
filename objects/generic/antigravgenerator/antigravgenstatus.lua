bf37424128f59b770485bd196611759b3e0291b3
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-02-09 20:58:47 -0500

    adjustments

--- objects/generic/antigravgenerator/antigravgenstatus.lua
+++ objects/generic/antigravgenerator/antigravgenstatus.lua
@@ -7,7 +7,7 @@ function update(dt)
 	if self.params.collisionEnabled then
 		if self.params.gravityEnabled then
 			--mcontroller.addMomentum({0,world.gravity(entity.position())*dt*self.params.gravityMultiplier})
-			self.newParams={gravityEnabled = false,airForce=0,flySpeed=0}
+			self.newParams={gravityEnabled = false,airForce=5,flySpeed=5,airFriction=3}
 		end
 		mcontroller.controlParameters(self.newParams)
 	end
