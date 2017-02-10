aa3c4e421986bbb7eb3c1a2769780d8da25872be
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-02-10 08:08:18 -0500

    slime armor to set bonuses

---
+++ stats/effects/fu_armoreffects/slimesetbonus3.lua
@@ -0,0 +1,14 @@
+require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
+
+function init()
+	setBonusInit("fu_slimeset3", {
+	    {stat = "protoImmunity", amount = 1},
+	    {stat = "insanityImmunity", amount = 1},
+	    {stat = "biooozeImmunity", amount = 1},
+	    {stat = "slimestickImmunity", amount = 1},
+	    {stat = "slimefrictionImmunity", amount = 1},
+	    {stat = "wetImmunity", amount = 1},
+	    {stat = "slimeImmunity", amount = 1},
+	    {stat = "poisonResistance", baseMultiplier = 0.35}
+	})
+end
