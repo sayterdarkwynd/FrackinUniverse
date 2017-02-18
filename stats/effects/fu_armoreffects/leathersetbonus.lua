151280f8dce7cde7835d5be82ccfd4d7787fdfe8
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-02-18 14:32:17 -0500

    adjustments for t1 armors

--- stats/effects/fu_armoreffects/leathersetbonus.lua
+++ stats/effects/fu_armoreffects/leathersetbonus.lua
@@ -1,9 +1,20 @@
+require "/stats/effects/runboost/runboostleatherarmor.lua"
+callbacks = { { init = init, update = update, uninit = uninit } }
+
 require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
 
 function init()
-	    setBonusInit("fu_boneset", {
-	      {stat = "powerMultiplier", baseMultiplier = 1.1},
-	      {stat = "fireResistance", baseMultiplier = 1.2},
-              {stat = "grit", baseMultiplier = 1.25}
-	    })        
+    if (world.type() == "garden") or (world.type() == "desert")  then
+	    setBonusInit("fu_leatherset", {
+	      {stat = "maxEnergy", baseMultiplier = 1.05},
+	      {stat = "critChance", baseMultiplier = 1.05},
+              {stat = "grit", baseMultiplier = 0.2}
+	    },callbacks) 
+       else
+	    setBonusInit("fu_leatherset", {
+	      {stat = "maxEnergy", baseMultiplier = 1.0},
+	      {stat = "critChance", baseMultiplier = 1.05},
+              {stat = "grit", baseMultiplier = 0.1}
+	    },callbacks)        
+    end  	
 end
