f79c9b2c524a07bd93b37691cb5425c59bae99ea
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-02-18 20:27:51 -0500

    updates

---
+++ stats/effects/fu_armoreffects/new/wolfsetbonus.lua
@@ -0,0 +1,16 @@
+require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
+
+function init()
+    if (world.type() == "garden") or (world.type() == "forest") then
+	    setBonusInit("fu_boneset", {
+	      {stat = "maxHealth", baseMultiplier = 1.05},
+	      {stat = "powerMultiplier", baseMultiplier = 1.05},
+	      {stat = "physicalResistance", baseMultiplier = 1.05},
+              {stat = "fallDamageMultiplier", baseMultiplier = 0.25}
+	    }) 
+       else
+	    setBonusInit("fu_boneset", {
+              {stat = "fallDamageMultiplier", baseMultiplier = 0.25}
+	    })        
+    end  	
+end
