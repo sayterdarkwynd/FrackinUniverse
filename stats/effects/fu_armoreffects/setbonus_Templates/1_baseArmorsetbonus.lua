9370a3c9b65705c090d2188c29ad7a2ab4d3397d
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-02-24 17:46:52 -0500

    adjustments

--- stats/effects/fu_armoreffects/setbonus_Templates/1_baseArmorsetbonus.lua
+++ stats/effects/fu_armoreffects/setbonus_Templates/1_baseArmorsetbonus.lua
@@ -1,10 +1,10 @@
 setName="fu_bearset"
 
-weaponEffect={
-    {stat = "critChance", amount = 25}
+weaponEffect={ -- if it is to have stats from weapons wielded, enter them here
+    {stat = "critChance", amount = 25} 
   }
   
-armorBonus={
+armorBonus={  -- whatever bonuses the armor provides go here
   {stat = "iceResistance", amount = 0.15},
   {stat = "coldimmunity", amount = 1}
   }
@@ -22,12 +22,12 @@ function update()
 	if not checkSetWorn(self.setBonusCheck) then
 		effect.expire()
 	else
-		bearPawCheck()
+		myFunction()  -- we can run custom functions here to make all sorts of neat shit possible
 	end
 end
 
-function bearPawCheck()
-	if weaponCheck("both",{"axe","hammer"},false) then
+function myFunction()
+	if weaponCheck("both",{"axe","hammer"},false) then   -- weaponCheck is run in setbonuses core file)
 		effect.setStatModifierGroup(handler,weaponEffect)
 	else
 		effect.setStatModifierGroup(handler,{})
