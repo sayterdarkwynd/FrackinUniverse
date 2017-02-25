6f246891e37f2b2e9badc708e944a382e077d9d8
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-02-25 18:07:00 -0500

    new armor sets

---
+++ stats/effects/fu_armoreffects/footmansetbonuseffect.lua
@@ -0,0 +1,34 @@
+setName="fu_footmanset"
+
+weaponEffect={
+    {stat = "powerMultiplier", baseMultiplier = 1.15}
+  }
+  
+armorBonus={
+    {stat = "iceResistance", amount = 0.15}
+}
+
+require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
+
+function init()
+	setSEBonusInit(setName)
+	weaponHandle=effect.addStatModifierGroup({})
+	daggerCheck()
+	armorHandle=effect.addStatModifierGroup(armorBonus)
+end
+
+function update()
+	if not checkSetWorn(self.setBonusCheck) then
+		effect.expire()
+	else
+		daggerCheck()
+	end	
+end
+
+function daggerCheck()
+	if weaponCheck("both",{"shortsword","broadsword"},false) then
+		effect.setStatModifierGroup(weaponHandle,weaponEffect)
+	else
+		effect.setStatModifierGroup(weaponHandle,{})
+	end
+end
