c38a1f3536ef3b275b10e5a960aeb84489f8d273
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-02-26 00:33:38 -0500

    set bonuses!

--- stats/effects/fu_armoreffects/swashbucklersetbonus.lua
+++ stats/effects/fu_armoreffects/swashbucklersetbonus.lua
@@ -1,11 +1,14 @@
-require "/stats/effects/jumpboost/jumpboostswashbuckler.lua"
-callbacks = { { init = init, update = update, uninit = uninit } }
+setName="fu_swashbucklerset"
+setStatEffects={"swashbucklersetbonuseffect"}
 
 require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
 
 function init()
-  setBonusInit("fu_swashbucklerset", {
-    {stat = "foodDelta", baseMultiplier = 0.85},
-    {stat = "iceResistance", baseMultiplier = 0.2}
-  },callbacks)  
+	setSEBonusInit(setName,setStatEffects)
+end
+
+function update()
+	if checkSetWorn(self.setBonusCheck) then
+		applySetEffects()
+	end
 end
--- stats/effects/fu_armoreffects/wolfsetbonus.lua
+++ stats/effects/fu_armoreffects/wolfsetbonus.lua
@@ -1,12 +1,14 @@
+setName="fu_wolfset"
+setStatEffects={"wolfsetbonuseffect"}
+
 require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
 
 function init()
-  setBonusInit("fu_wolfset", {
-    {stat = "biomecoldImmunity", amount = 1},
-    {stat = "slushslowImmunity", amount = 1},
-    {stat = "iceslipImmunity", amount = 1},
-    {stat = "snowslowImmunity", amount = 1},
-    {stat = "iceResistance", amount = 0.5},
-    {stat = "iceStatusImmunity", amount = 1}
-	})
+	setSEBonusInit(setName,setStatEffects)
+end
+
+function update()
+	if checkSetWorn(self.setBonusCheck) then
+		applySetEffects()
+	end
 end
