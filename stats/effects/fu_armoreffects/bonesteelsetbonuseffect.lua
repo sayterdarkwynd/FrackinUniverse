eb1382aeffc2925410bf90051ac6d82cb7f5d393
Kherae <cwaldridge@live.com>
2017-02-28 13:58:44 -0800

    bone and bonesteel update

--- stats/effects/fu_armoreffects/bonesteelsetbonuseffect.lua
+++ stats/effects/fu_armoreffects/bonesteelsetbonuseffect.lua
@@ -4,8 +4,17 @@ weaponEffect={
     {stat = "powerMultiplier", baseMultiplier = 1.15}
   }
   
+armorBonus2={
+	{stat = "maxHealth", baseMultiplier = 1.10},
+	{stat = "maxEnergy", baseMultiplier = 1.05},
+	{stat = "physicalResistance", amount = 0.20},
+	{stat = "fallDamageMultiplier", baseMultiplier = 0.25}
+}
+
 armorBonus={
-    {stat = "physicalResistance", amount = 0.15}
+	{stat = "physicalResistance", amount = 0.15},
+	{stat = "maxHealth", baseMultiplier = 1.05},
+	{stat = "fallDamageMultiplier", baseMultiplier = 0.25}
 }
 
 require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
@@ -21,6 +30,11 @@ function update(dt)
 	if not checkSetWorn(self.setBonusCheck) then
 		effect.expire()
 	else
+		if (world.type() == "garden") or (world.type() == "forest") then
+			effect.setStatModifierGroup(armorHandle,armorBonus2)
+		else
+			effect.setStatModifierGroup(armorHandle,armorBonus)
+		end
 		checkWeapons()
 	end	
 end
