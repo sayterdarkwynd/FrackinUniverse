39c945eb7b7bfbf5c0181bbdef2616f31c52f3a5
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-03-06 19:33:21 -0500

    update

--- stats/effects/1_new_environmental/protoworldnew/coldweathernew.lua
+++ stats/effects/1_new_environmental/protoworldnew/coldweathernew.lua
@@ -20,23 +20,32 @@ self.baseMod = config.getParameter("baseMod",0)
 self.biomeMod = ( ( self.baseTemp ) * ( 1 +self.baseMod ) )
 
 if (world.type() == "snow") then
-
+  self.baseTemp = math.random(10)
+  self.baseMod = 3
 elseif (world.type() == "arctic") then
-self.baseTemp = config.getParameter("baseTemp",0)
-elseif (world.type() == "icewaste") then
-self.baseTemp = config.getParameter("baseTemp",0)
-elseif (world.type() == "frozenvolcanic") then
-self.baseTemp = config.getParameter("baseTemp",0)
-elseif (world.type() == "icemoon") then
-self.baseTemp = config.getParameter("baseTemp",0)
-elseif (world.type() == "icewastedark") then
-self.baseTemp = config.getParameter("baseTemp",0)
-elseif (world.type() == "nitrogensea") then
-self.baseTemp = config.getParameter("baseTemp",0)
-elseif (world.type() == "nitrogenseafloor") then
-self.baseTemp = config.getParameter("baseTemp",0)
+  self.baseTemp = math.random(20)
+  self.baseMod = 3
 elseif (world.type() == "arcticoceanfloor") then
-self.baseTemp = config.getParameter("baseTemp",0)
+  self.baseTemp = math.random(30)
+  self.baseMod = 3
+elseif (world.type() == "icewaste") then
+  self.baseTemp = math.random(30)
+  self.baseMod = 5
+elseif (world.type() == "icewastedark") then
+  self.baseTemp = math.random(40)
+  self.baseMod = 5
+elseif (world.type() == "frozenvolcanic") then
+  self.baseTemp = math.random(20)
+  self.baseMod = 2
+elseif (world.type() == "icemoon") then
+  self.baseTemp = math.random(20)
+  self.baseMod = 1.5
+elseif (world.type() == "nitrogensea") then
+  self.baseTemp = math.random(35)
+  self.baseMod = 4
+elseif (world.type() == "nitrogenseafloor") then
+  self.baseTemp = math.random(40)
+  self.baseMod = 4
 end
 
 
@@ -44,23 +53,23 @@ end
 
 -- check resist level and apply modifier for effects
   if status.stat("iceResistance") <= 0.99 then
-    self.icehitmod = 2 * 1+ (status.stat("iceResistance",0) * 4)
+    self.icehitmod = 2.2 * 1+ (status.stat("iceResistance",0) * 4)
   elseif status.stat("iceResistance") <= 0.80 then
-    self.icehitmod = 2 * 1+ (status.stat("iceResistance",0) * 3.5)    
+    self.icehitmod = 1.9 * 1+ (status.stat("iceResistance",0) * 3.5)    
   elseif status.stat("iceResistance") <= 0.75 then
-    self.icehitmod = 2 * 1+ (status.stat("iceResistance",0) * 3) 
+    self.icehitmod = 1.5 * 1+ (status.stat("iceResistance",0) * 3) 
   elseif status.stat("iceResistance") <= 0.65 then
-    self.icehitmod = 2 * 1+ (status.stat("iceResistance",0) * 2.5)    
+    self.icehitmod = 1.2 * 1+ (status.stat("iceResistance",0) * 2.5)    
   elseif status.stat("iceResistance") <= 0.50 then
-    self.icehitmod = 2 * 1+ (status.stat("iceResistance",0) * 2)
+    self.icehitmod = 0.8 * 1+ (status.stat("iceResistance",0) * 2)
   elseif status.stat("iceResistance") <= 0.40 then
-    self.icehitmod = 1 * 1+ (status.stat("iceResistance",0) * 1.5)    
+    self.icehitmod = 0.5 * 1+ (status.stat("iceResistance",0) * 1.5)    
   elseif status.stat("iceResistance") <= 0.30 then
-    self.icehitmod = 0.6
+    self.icehitmod = 0.1
   elseif status.stat("iceResistance") <= 0.10 then
-    self.icehitmod = 0.2
+    self.icehitmod = 0.05
   elseif status.stat("iceResistance") <= 0.05 then
-    self.icehitmod = 0.05       
+    self.icehitmod = 0.01       
   end 
   
   self.icehitTimer = self.icehitmod +1
@@ -85,11 +94,11 @@ end
 
 
 function update(dt)
-    
       self.biomeTimer = self.biomeTimer - dt
       self.debuffApply = self.baseDebuff * (-self.icehitmod)
       self.damageApply = self.baseDmg * (self.icehitmod) 
-      
+   
+  if status.stat("iceResistance") <= 0.99 then
       if self.biomeTimer <= 0 and status.stat("iceResistance") < 1.0 then
          -- damage application per tick
           status.applySelfDamageRequest ({
@@ -99,59 +108,27 @@ function update(dt)
             sourceEntityId = entity.id()	
           })   
 
+    -- speed penalty is equal to your resistance level. higher is better. 0 is motionless and frozen.
+    mcontroller.controlModifiers({
+        speedModifier = status.stat("iceResistance")
+    }) 
+
           effect.addStatModifierGroup({
-            {stat = "maxHealth", amount = self.debuffApply },
-            {stat = "critChance", amount = 0 }
+            {stat = "maxEnergy", amount = self.debuffApply },
+            -- your HP is dropped in half
+            {stat = "protection", amount = (stat.status("protection")/2) }
           })
 
-
-  if status.stat("iceResistance") <= 0.99 then
-    mcontroller.controlModifiers({
-        speedModifier = 0.98
-    }) 
-  elseif status.stat("iceResistance") <= 0.80 then
-    mcontroller.controlModifiers({
-        speedModifier = 0.90
-    })    
-  elseif status.stat("iceResistance") <= 0.75 then
-    mcontroller.controlModifiers({
-        speedModifier = 0.80
-    }) 
-  elseif status.stat("iceResistance") <= 0.65 then
-    mcontroller.controlModifiers({
-        speedModifier = 0.70
-    })  
-  elseif status.stat("iceResistance") <= 0.50 then
-    mcontroller.controlModifiers({
-        speedModifier = 0.50
-    }) 
-  elseif status.stat("iceResistance") <= 0.40 then
-    mcontroller.controlModifiers({
-        speedModifier = 0.40
-    })   
-  elseif status.stat("iceResistance") <= 0.30 then
-    mcontroller.controlModifiers({
-        speedModifier = 0.30
-    }) 
-  elseif status.stat("iceResistance") <= 0.10 then
-    mcontroller.controlModifiers({
-        speedModifier = 0.20
-    }) 
-  elseif status.stat("iceResistance") <= 0.05 then
-    mcontroller.controlModifiers({
-        speedModifier = 0.15
-    })     
   end 
   
   
-          -- activate visuals and check stats
-	  makeAlert()
-	  activateVisualEffects()
+  -- activate visuals and check stats
+  makeAlert()
+  activateVisualEffects()
 	  
-	  -- set the timer
-          self.biomeTimer = self.icehitTimer
-
-      end 	
+  -- set the timer
+  self.biomeTimer = self.icehitTimer
+	
 end       
 
 function uninit()
