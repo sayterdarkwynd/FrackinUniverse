f9da68fdc17778ab1bf69dc23c0897115bced799
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-03-18 09:54:41 -0400

    icon additions

--- stats/effects/fu_armoreffects/set_bonuses/chitinsetbonuseffect.lua
+++ stats/effects/fu_armoreffects/set_bonuses/chitinsetbonuseffect.lua
@@ -1,8 +1,4 @@
-setName="fu_chitinset"
-
-armorEffect={
-	{stat = "sulphuricImmunity", amount = 1}
-}
+require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
 
 armorBonus={
 	{stat = "maxHealth", baseMultiplier = 1.12},
@@ -12,23 +8,31 @@ armorBonus={
 	{stat = "physicalResistance", amount = 0.15}
 }
 
+armorEffect={
+	{stat = "sulphuricImmunity", amount = 1}
+}
 
-require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
+setName="fu_chitinset"
 
 function init()
 	setSEBonusInit(setName)
-        armorEffectHandle=effect.addStatModifierGroup(armorBonus)
 	armorEffectHandle=effect.addStatModifierGroup(armorEffect)
+	armorBonusHandle=effect.addStatModifierGroup({})
+	checkArmor()
 end
 
 function update(dt)
 	if not checkSetWorn(self.setBonusCheck) then
 		effect.expire()
 	else
-		if (world.type() == "sulphuric") or (world.type() == "sulphuricdark") or (world.type() == "sulphuricocean") then
-			effect.setStatModifierGroup(armorBonusHandle,armorBonus)
-		else
-			effect.setStatModifierGroup(armorBonusHandle,armorEffect)
-		end
+		checkArmor()
+	end
+end
+
+function checkArmor()
+	if (world.type() == "sulphuric") or (world.type() == "sulphuricdark") or (world.type() == "sulphuricocean") then
+		effect.setStatModifierGroup(armorBonusHandle,armorBonus)
+	else
+		effect.setStatModifierGroup(armorBonusHandle,{})
 	end
 end
