3bfdd30354f7aacb984c72bf92970b7be053ed35
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-03-20 08:26:41 -0400

    armor cult set gets eye

--- stats/effects/fu_armoreffects/set_bonuses/corruptsetbonuseffect.lua
+++ stats/effects/fu_armoreffects/set_bonuses/corruptsetbonuseffect.lua
@@ -1,4 +1,5 @@
 require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
+require "/scripts/vec2.lua"
 
 armorBonus={
 	{stat = "maxHealth", baseMultiplier = 1.25},
@@ -21,15 +22,46 @@ function init()
 	checkArmor()
 end
 
+function getLight()
+  local position = mcontroller.position()
+  position[1] = math.floor(position[1])
+  position[2] = math.floor(position[2])
+  local lightLevel = world.lightLevel(position)
+  self.lightLevel = math.floor(lightLevel * 100)
+  return lightLevel
+end
+
+-- ***********************************************************************************************************
+-- FR SPECIALS  Functions for projectile spawning
+-- ***********************************************************************************************************
+function firePosition()
+   return vec2.add(mcontroller.position(), entity.position())
+end
+
+function aimVector()  -- fires straight
+  local aimVector = vec2.rotate({1, 0}, mcontroller.facingDirection() )
+  aimVector[1] = aimVector[1] * mcontroller.facingDirection()
+  return aimVector
+end
+
+function aimVectorRand() -- fires wherever it wants
+  local aimVector = vec2.rotate({1, 0},  mcontroller.facingDirection() + sb.nrand(inaccuracy, 0))
+  aimVector[1] = aimVector[1] * mcontroller.facingDirection()
+  return aimVector
+end
+
+
 function update(dt)
 	if not checkSetWorn(self.setBonusCheck) then
 		effect.expire()
 	else
+	        getLight()
+	        
 		checkArmor()
-		randValue = math.random(100)	
-		if (randValue < 5) and (lightLevel < 26) then  -- spawn a projectile
+		self.randValue = math.random(100)	
+		if (self.randValue < 95) then  -- spawn a projectile
 		  params = { power = 5, damageKind = "shadow" }			
-		  projectileId = world.spawnProjectile("scouteye",self:firePosition(),activeItem.ownerEntityId(),self:aimVector(),false,params)
+		  projectileId = world.spawnProjectile("scouteye",mcontroller.position(),entity.id(), aimVectorRand(),false,params)
 		end			
 	end
 end
