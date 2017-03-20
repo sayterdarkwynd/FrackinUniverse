ef93fb2503b738a7cfd434867e63ffe2d69a04ba
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-03-20 07:56:18 -0400

    fix for armored cultist armor

--- stats/effects/fu_armoreffects/set_bonuses/corruptsetbonuseffect.lua
+++ stats/effects/fu_armoreffects/set_bonuses/corruptsetbonuseffect.lua
@@ -26,11 +26,16 @@ function update(dt)
 		effect.expire()
 	else
 		checkArmor()
+		randValue = math.random(100)	
+		if (randValue < 5) and (lightLevel < 26) then  -- spawn a projectile
+		  params = { power = 5, damageKind = "shadow" }			
+		  projectileId = world.spawnProjectile("scouteye",self:firePosition(),activeItem.ownerEntityId(),self:aimVector(),false,params)
+		end			
 	end
 end
 
 function checkArmor()
-	if (world.type() == "lightless") or (world.type() == "penumbra") or (world.type() == "aethersea") or (world.type() == "moon_shadow") or (world.type() == "shadow") or or (world.type() == "midnight") then
+	if (world.type() == "lightless") or (world.type() == "penumbra") or (world.type() == "aethersea") or (world.type() == "moon_shadow") or (world.type() == "shadow") or (world.type() == "midnight") then
 		effect.setStatModifierGroup(
 		armorBonusHandle,armorBonus)
 	else
