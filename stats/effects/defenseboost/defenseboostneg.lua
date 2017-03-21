2ed2341c9824553c9d5ab0dc4107d524cdba6d53
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-03-20 21:36:21 -0400

    updates

--- stats/effects/defenseboost/defenseboostneg.lua
+++ stats/effects/defenseboost/defenseboostneg.lua
@@ -2,7 +2,10 @@ function init()
   animator.setParticleEmitterOffsetRegion("snow", mcontroller.boundBox())
   animator.setParticleEmitterActive("snow", true)
   effect.setParentDirectives("fade=6f6f6f=0.55")
-   effect.addStatModifierGroup({{stat = "protection", amount = config.getParameter("defenseAmount", 0)}})
+   effect.addStatModifierGroup({
+   {stat = "protection", amount = config.getParameter("defenseAmount", 0)},
+   {stat = "physicalResistance", amount = config.getParameter("defenseAmount2", 0)}
+   })
 
    script.setUpdateDelta(0)
 end
