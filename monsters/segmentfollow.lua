dc34b25f2c86e1690e6d488085e89883154522aa
myothernameisreal <robertrdrane@gmail.com>
2017-04-23 21:03:16 -0500

    Experimental start to worm monsters

---
+++ monsters/segmentfollow.lua
@@ -0,0 +1,12 @@
+local monsterUpdate = update
+
+function update(dt)
+	monsterUpdate(dt)
+	self.parent = config.getParameter("parent")
+	distance = entity.distanceToEntity(self.parent)
+	if math.abs(distance[1] + distance[2]) > 4 then
+		newPosition = {(world.entityPosition(self.parent)[1]+mcontroller.position()[1])/2,(world.entityPosition(self.parent)[2]+mcontroller.position()[2])/2}
+		mcontroller.setPosition(newPosition)
+	end
+
+end
