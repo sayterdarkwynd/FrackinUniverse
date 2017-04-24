be7497e7fbf08e7cec82ea75ef60da6b479a83f7
myothernameisreal <robertrdrane@gmail.com>
2017-04-24 13:14:21 -0500

    More work on worm like monsters

--- monsters/segmentfollow.lua
+++ monsters/segmentfollow.lua
@@ -2,7 +2,7 @@ local monsterUpdate = update
 
 function update(dt)
 	monsterUpdate(dt)
-	self.parent = config.getParameter("parent")
+	self.parent = self.parent and self.parent or config.getParameter("parent")
 	distance = entity.distanceToEntity(self.parent)
 	if math.abs(distance[1] + distance[2]) > 4 then
 		newPosition = {(world.entityPosition(self.parent)[1]+mcontroller.position()[1])/2,(world.entityPosition(self.parent)[2]+mcontroller.position()[2])/2}
