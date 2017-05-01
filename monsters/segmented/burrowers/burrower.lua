1d26a2fa901c30792ea76fde6c81f2614c7a7ff6
myothernameisreal <robertrdrane@gmail.com>
2017-05-01 12:56:04 -0500

    removed commented out code

--- monsters/segmented/burrowers/burrower.lua
+++ monsters/segmented/burrowers/burrower.lua
@@ -2,25 +2,6 @@ require "/scripts/vec2.lua"
 require "/scripts/actions/animator.lua"
 
 local monsterUpdate = update
---[[
-function updateAngularVelocity(dt)
-  local positionDiff = world.distance(self.lastPosition or mcontroller.position(), mcontroller.position())
-  self.angularVelocity = -vec2.mag(positionDiff) / dt / self.ballRadius
-
-  if positionDiff[1] > 0 then
-    self.angularVelocity = -self.angularVelocity
-  end
-end
-
-function updateRotationFrame(dt)
-  self.angle = math.fmod(math.pi * 2 + self.angle + self.angularVelocity * dt, math.pi * 2)
-  local rotationFrame = math.floor(self.angle / math.pi * self.ballFrames) % self.ballFrames
-  if mcontroller.facingDirection() == -1 then
-    rotationFrame = self.ballFrames - ( 1 + rotationFrame)
-  end
-  animator.setGlobalTag("rotationFrame", rotationFrame + 1)
-end
-]]
 
 function burrowEffect(dt)
 
@@ -50,15 +31,7 @@ end
 function update(dt)
 
 	self.parent = self.parent and self.parent or config.getParameter("parent")
-	--[[
-	if not self.angularVelocity then
-	  self.angularVelocity = 0
-	  self.angle = 0
-	  self.rotationFrame = 0
-	  self.ballFrames = config.getParameter("frames")
-	  self.ballRadius = config.getParameter("radius")
-	end
-	]]
+
 	if not self.burrowTimer then
 	  self.burrowProjectile = config.getParameter("burrowProjectile", "burrow")
 	  self.burrowBurstProjectile = config.getParameter("burrowBurstProjectile", "burrowburst")
@@ -92,10 +65,7 @@ function update(dt)
 		  mcontroller.setVelocity(vec2.approach(distance,world.entityPosition(self.parent),self.parentRadius))
 		end
 	end
-	--[[
-  	updateAngularVelocity(dt)
-  	updateRotationFrame(dt)
-  	self.lastPosition = mcontroller.position()
-	]]
+
 	burrowEffect(dt)
+
 end
