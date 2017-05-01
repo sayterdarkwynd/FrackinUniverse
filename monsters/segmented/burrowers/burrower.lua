535f57b08a5200304149862d7204b56ecf93ae41
myothernameisreal <robertrdrane@gmail.com>
2017-04-30 19:03:57 -0500

    Removed health bars for segments (which never lose health)

--- monsters/segmented/burrowers/burrower.lua
+++ monsters/segmented/burrowers/burrower.lua
@@ -66,6 +66,7 @@ function update(dt)
 	  self.burrowTick = self.burrowTimer
 	end
 	if not self.followRadius then
+	  monster.setDamageBar("none")
 	  self.parentRadius =  config.getParameter("parentRadius",1)
 	  local segmentRadius = config.getParameter("radius",1)
 	  self.followRadius =  segmentRadius + self.parentRadius
