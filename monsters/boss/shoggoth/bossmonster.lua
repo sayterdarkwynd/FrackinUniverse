bb187de6893914a476f9c2deed7b7733024f5e60
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-05-11 13:52:17 -0400

    shoggoth

--- monsters/boss/shoggoth/bossmonster.lua
+++ monsters/boss/shoggoth/bossmonster.lua
@@ -52,7 +52,7 @@ end
 
 function update(dt)
   self.tookDamage = false
-  monster.say("I love dick")
+  --monster.say("I love you")
   world.spawnProjectile("pushzone2",mcontroller.position(),entity.id(),{0,-35},false,params)
   trackTargets(self.keepTargetInSight, self.queryTargetDistance, self.trackTargetDistance, self.switchTargetDistance)
 
