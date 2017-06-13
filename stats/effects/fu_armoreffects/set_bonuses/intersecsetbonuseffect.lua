78ee374b7ee5f2cf94042998e7b7331266304cfb
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-06-12 23:24:18 -0400

    stuff

--- stats/effects/fu_armoreffects/set_bonuses/intersecsetbonuseffect.lua
+++ stats/effects/fu_armoreffects/set_bonuses/intersecsetbonuseffect.lua
@@ -3,7 +3,7 @@ require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
 weaponBonus={
 	{stat = "critChance", amount = 5},
 	{stat = "critBonus", baseMultiplier = 1.25},
-	{stat = "shipMass", baseMultiplier = 0.75}
+	{stat = "shipMass", baseMultiplier = 0.85}
 }
 
 armorBonus={}
