4a89198f5d59e429a21e5846d7739d2accdcbc98
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-07-16 16:46:05 -0400

    a

--- items/active/weapons/weapon.lua
+++ items/active/weapons/weapon.lua
@@ -69,14 +69,6 @@ function setCritDamage(damage)
       elseif root.itemHasTag(heldItem, "rapier") then
         self.critChance = 0 + weaponModifier      
       elseif root.itemHasTag(heldItem, "whip") then
-        self.critChance = 0 + weaponModifier   
-      elseif root.itemHasTag(heldItem, "longsword") then
-        self.critChance = 0 + weaponModifier 
-      elseif root.itemHasTag(heldItem, "rapier") then
-        self.critChance = 0 + weaponModifier  
-      elseif root.itemHasTag(heldItem, "scythe") then
-        self.critChance = 0 + weaponModifier  
-      elseif root.itemHasTag(heldItem, "mace") then
         self.critChance = 0 + weaponModifier           
       end
 	end
@@ -97,7 +89,7 @@ function setCritDamage(damage)
         --              weapon specific crit abilities!
         -- *****************************************************************
         self.stunChance = math.random(100) + status.stat("stunChance",0) + config.getParameter("stunChance",0)
-        if (self.stunChance) >= 100 and root.itemHasTag(heldItem, "hammer") or root.itemHasTag(heldItem, "greataxe") or root.itemHasTag(heldItem, "quarterstaff") or root.itemHasTag(heldItem, "mace") then -- Stun!!!!
+        if (self.stunChance) >= 100 and root.itemHasTag(heldItem, "hammer") or root.itemHasTag(heldItem, "greataxe") or root.itemHasTag(heldItem, "quarterstaff") then -- Stun!!!!
 		params = { speed=30, power = 1, damageKind = "default"} 
 		world.spawnProjectile("shieldBashStunProjectile",mcontroller.position(),activeItem.ownerEntityId(),{0.2,0},false,params)   
         end
