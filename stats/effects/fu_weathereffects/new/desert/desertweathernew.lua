502c160236878deb2a543efde549c2e021fd67b1
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-08-13 18:04:12 -0400

    batch of new
    new partytimecollar effect
    new relocator additions
    new sound
    some edits to weather messages
    can no longer produce liquid on space stations or ships using a liquid
    condensor

--- stats/effects/fu_weathereffects/new/desert/desertweathernew.lua
+++ stats/effects/fu_weathereffects/new/desert/desertweathernew.lua
@@ -17,7 +17,7 @@ function init()
   self.biomeTimer2 = (self.baseRate * (1 + status.stat("fireResistance",0)) *10)
   
   --conditionals
-
+  self.usedIntro = 0
   self.windLevel =  world.windLevel(mcontroller.position())        -- is there wind? we note that too
   self.biomeThreshold = config.getParameter("biomeThreshold",0)    -- base Modifier (tier)
   self.biomeNight = config.getParameter("biomeNight",0)            -- is this effect worse at night? how much?
@@ -31,6 +31,7 @@ end
 
 --******* check effect and cancel ************
 function checkEffectValid()
+  
   if world.entityType(entity.id()) ~= "player" then
     deactivateVisualEffects()
     effect.expire()
@@ -40,7 +41,7 @@ function checkEffectValid()
 	  effect.expire()
 	else
 	  if (status.stat("fireResistance") <= self.effectCutoffValue) then
-	    if not self.usedIntro and self.timerRadioMessage == 0 then
+	    if self.usedIntro>0 and self.timerRadioMessage == 0 then
 	      -- activate visuals and check stats
 	      world.sendEntityMessage(entity.id(), "queueRadioMessage", "ffbiomedesert", 1.0) -- send player a warning
 	      self.usedIntro = 1
