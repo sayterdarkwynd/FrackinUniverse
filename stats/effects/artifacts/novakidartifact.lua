67f13c121821aaa91bd7d95ae245f8c60dee2b60
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-08-18 18:53:42 -0400

    artifact augments

---
+++ stats/effects/artifacts/novakidartifact.lua
@@ -0,0 +1,17 @@
+function init()
+  effect.addStatModifierGroup({
+    {stat = "radioactiveResistance", amount = config.getParameter("resist1")},
+    {stat = "fireResistance", amount = config.getParameter("resist2")},
+    {stat = "powerMultiplier", amount = config.getParameter("damageBoost")}
+  })
+  
+  script.setUpdateDelta(10)
+end
+
+function update(dt)
+		
+end
+
+function uninit()
+
+end
