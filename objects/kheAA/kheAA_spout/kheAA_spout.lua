94a44a9b7cde4de86bd5126451e1eef6270cfc54
Kherae <cwaldridge@live.com>
2017-09-12 14:13:05 -0700

    spout fix
    no more bridges needed!

--- objects/kheAA/kheAA_spout/kheAA_spout.lua
+++ objects/kheAA/kheAA_spout/kheAA_spout.lua
@@ -2,12 +2,15 @@ require "/scripts/kheAA/liquidLib.lua"
 require "/scripts/kheAA/transferUtil.lua"
 
 function init()
+	powerNode=0
+	receiveLiquid=true
 	transferUtil.init()
 	liquidLib.init()
-	receiveLiquid=true
+	transferUtil.loadSelfContainer()
 end
 
 function update(dt)
+	transferUtil.loadSelfContainer()
 	deltatime = (deltatime or 0) + dt;
 	if deltatime < 0.05 then
 		return
