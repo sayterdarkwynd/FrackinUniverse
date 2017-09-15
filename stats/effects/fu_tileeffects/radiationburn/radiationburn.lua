452fde1d3732b10fe277392181a41adb8e676a52
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-09-15 09:58:24 -0400

    adds pies
    adds pies
    reduces mace combo damage, adjusts knockback
    war angel helm improvement
    changed some turrets on lethia fleet
    added scented giant apiary

--- stats/effects/fu_tileeffects/radiationburn/radiationburn.lua
+++ stats/effects/fu_tileeffects/radiationburn/radiationburn.lua
@@ -3,7 +3,7 @@ function init()
   animator.setParticleEmitterActive("flames", true)
   effect.setParentDirectives("fade=00FF33=0.15")
   script.setUpdateDelta(5)
-  self.tickDamagePercentage = 0.032
+  self.tickDamagePercentage = 0.05
   self.tickTime = 1.0
   self.tickTimer = self.tickTime
 end
@@ -20,7 +20,7 @@ function update(dt)
     self.tickTimer = self.tickTime
     status.applySelfDamageRequest({
         damageType = "IgnoresDef",
-        damage = math.floor(status.resourceMax("health") * self.tickDamagePercentage) + 1,
+        damage = math.floor(status.resourceMax("health") * self.tickDamagePercentage) + 1.5,
         damageSourceKind = "radioactive",
         sourceEntityId = entity.id()
       })
