8908758ef7b7f0faf6ee1a3f549f9ed12f349daa
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-09-18 13:46:23 -0400

    precursor armor no longere causes deadly falls

--- stats/effects/fu_armoreffects/set_bonuses/precursorsetbonuseffect.lua
+++ stats/effects/fu_armoreffects/set_bonuses/precursorsetbonuseffect.lua
@@ -28,7 +28,7 @@ function update(dt)
 	if not checkSetWorn(self.setBonusCheck) then
 		effect.expire()
 	else
-	        status.addEphemeralEffect("gravgenfieldarmor2",5)
+	        --status.addEphemeralEffect("gravgenfieldarmor2",5)
 		checkWeapons()
 	end	
 
