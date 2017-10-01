7c2421b4758e357a97ebf3edbcaea7b25513d209
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-10-01 19:36:00 -0400

    fixed broken ass berserker augment

--- stats/effects/augments/berserker.lua
+++ stats/effects/augments/berserker.lua
@@ -1,9 +1,9 @@
 function init()
   local bounds = mcontroller.boundBox()
   
-    status.setPersistentEffects("floranpower1", {
-      {stat = "maxHealth", amount = status.stat("maxHealth")*0.8 },
-      {stat = "powerMultiplier", amount = status.stat("powerMultiplier")*1.2 }
+    status.setPersistentEffects("berserk", {
+      {stat = "maxHealth", baseMultiplier = status.stat("maxHealth")*0.8 },
+      {stat = "powerMultiplier", baseMultiplier = status.stat("powerMultiplier")*1.2 }
     })  
 end
 
