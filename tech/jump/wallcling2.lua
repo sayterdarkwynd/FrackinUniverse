073d42c5a1eff6e8660083e3851b14d7db90d122
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-10-30 15:43:03 -0400

    magnet grip magnet jumps cost energy

--- tech/jump/wallcling2.lua
+++ tech/jump/wallcling2.lua
@@ -3,6 +3,7 @@ require "/scripts/vec2.lua"
 require "/scripts/poly.lua"
 
 function init()
+  self.energyCost = config.getParameter("energyCost")
   self.multiJumpCount = config.getParameter("multiJumpCount")
   self.multiJumpModifier = config.getParameter("multiJumpModifier")
 
@@ -62,7 +63,7 @@ function update(args)
 
     if not checkWall(self.wall) or status.statPositive("activeMovementAbilities") then
       releaseWall()
-    elseif jumpActivated then
+    elseif jumpActivated and status.overConsumeResource("energy", self.energyCost) then
       doWallJump()
     else
       if lrInput and lrInput ~= self.wall then
