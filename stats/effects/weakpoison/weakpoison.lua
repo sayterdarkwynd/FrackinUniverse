53bac8e34a8e3a52080f0d1caed99d143dd30395
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-11-21 14:55:10 -0500

    proper weakpoison resistance

--- stats/effects/weakpoison/weakpoison.lua
+++ stats/effects/weakpoison/weakpoison.lua
@@ -1,11 +1,13 @@
 function init()
-  animator.setParticleEmitterOffsetRegion("drips", mcontroller.boundBox())
-  animator.setParticleEmitterActive("drips", true)
-
+  if (status.stat("poisonResistance",0) <= 0.45) then
+    animator.setParticleEmitterOffsetRegion("drips", mcontroller.boundBox())
+    animator.setParticleEmitterActive("drips", true)
+  end
+  
   script.setUpdateDelta(5)
 
   self.tickDamagePercentageP = 0.025
-  self.tickTime = 1.0
+  self.tickTime = 1.2
   self.tickTimer = self.tickTime
 end
 
@@ -15,20 +17,22 @@ function setEffectDamage()
 end
 
 function update(dt)
-  self.tickDamagePercentage = setEffectDamage()
-  
-  self.tickTimer = self.tickTimer - dt
-  if self.tickTimer <= 0 then
-    self.tickTimer = self.tickTime
-    status.applySelfDamageRequest({
-        damageType = "IgnoresDef",
-        damage = math.floor(status.resourceMax("health") * self.tickDamagePercentageP) + 1,
-        damageSourceKind = "poison",
-        sourceEntityId = entity.id()
-      })
+  if (status.stat("poisonResistance",0) <= 0.45) then
+	  self.tickDamagePercentage = setEffectDamage()
+	  self.tickTimer = self.tickTimer - dt
+	  if self.tickTimer <= 0 then
+	    self.tickTimer = self.tickTime
+	    status.applySelfDamageRequest({
+		damageType = "IgnoresDef",
+		damage = math.floor(status.resourceMax("health") * self.tickDamagePercentageP) + 1,
+		damageSourceKind = "poison",
+		sourceEntityId = entity.id()
+	      })
+	  end
+	  effect.setParentDirectives(string.format("fade=00AA00=%.1f", self.tickTimer * 0.4))
+  else
+  effect.expire()
   end
-
-  effect.setParentDirectives(string.format("fade=00AA00=%.1f", self.tickTimer * 0.4))
 end
 
 function uninit()
