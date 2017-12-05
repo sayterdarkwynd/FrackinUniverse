9d6d3a24cd6d6eabc9dce436e8e89e67d360e98e
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-12-05 12:27:58 -0500

    adds mech configuration to tech tablet Shift-click

--- items/active/unsorted/techtablet/techtablet.lua
+++ items/active/unsorted/techtablet/techtablet.lua
@@ -1,11 +1,12 @@
-function activate(fireMode)
-    if fireMode == "primary" then
+function activate(fireMode, shiftHeld)
+    if shiftHeld then
+        activeItem.interact("ScriptPane", "/interface/scripted/mechassembly/mechassemblygui.config")
+    elseif fireMode == "primary" then
 		activeItem.interact("ScriptPane", "/interface/scripted/techshop/techshop.config")
-		animator.playSound("activate")
 	elseif fireMode == "alt" then
 		activeItem.interact("ScriptPane", "/interface/scripted/techupgrade/techupgradegui.config")
-		animator.playSound("activate")
 	end
+    animator.playSound("activate")
 end
 
 function update()
@@ -15,3 +16,9 @@ function update()
 		activeItem.setArmAngle(-0.5)
 	end
 end
+
+
+
+
+
+
