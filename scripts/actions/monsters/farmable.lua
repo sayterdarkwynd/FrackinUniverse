489a9fdac32b5b2fecd15ff06f5fab1d27db933d
sayterdarkwynd <sayterdarkwynd@gmail.com>
2018-01-10 14:41:00 -0500

    added patch notes

--- scripts/actions/monsters/farmable.lua
+++ scripts/actions/monsters/farmable.lua
@@ -132,8 +132,8 @@ function checkPoop()
         self.foodMod = storage.food/20 * config.getParameter('hungerTime',20)
   	self.randPoop = math.random(500) - self.foodMod
   	if self.randPoop < 1 then self.randPoop = 1 end
-  	sb.logInfo("poop roll = "..self.randPoop)
-  	sb.logInfo("mod = "..self.foodMod)
+  	--sb.logInfo("poop roll = "..self.randPoop)
+  	--sb.logInfo("mod = "..self.foodMod)
   	if self.randPoop <= 1.14 then
   	  animator.playSound("deathPuff")
   	  world.spawnItem("poop", mcontroller.position(), 1)
