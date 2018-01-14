e3a8550feb7bf7b8f733dba9d0e32721b082f344
ProfoundDarkness <prodark77@gmail.com>
2018-01-13 22:45:40 -0700

    Not ready for merge IMO, more interested in feedback...
    overview of changes
    -ONLY changes the lua for fu_growingtray and not (yet) isn_hydroponicstray
    -Instead of FU having to know the seed output, look up the treasurePool from the seed.
    -Base the grow time on the actual seed.
    -Instead of one item type from the output yield multiple types based on the treasure pool.
    -Currently does multiple iterations of the treasure pool (fertilizer).
    -Only uses 1 unit of water per growth stage.
    
    Details:
    -init-
    insert seed
    if seed passes checks (original plus a check if it is a farmable type)
    -copy information from seed .object definition, specifically "stages" into seedData.stages
    -Also fetch the seed treasure pool ("harvestPool") and if the seed is perennial/annual
    -generate growthData from seedData.
    --Basically how long to grow as well as how long (of total grow time) each stage would be.
    *might need to insert a flag to consome one water as currently that might be free for the first stage.
    -check fertilizer state and set yield count
    
    -growth-
    Check if we are in an apt state to update...
    Calculate which graphic (from 0 to 3) to show based on how far the plant has grown.
    Check if a new growth stage was entered...
    -If so then try to use some water.
     *pretty sure that the first phase is free for water
    --If the water is used, keep growing and increment the internal stage to track when the next water happens.
    --If not then growth is haulted until water is present.
    Update growth progress (considering fertilizer)
    Check if growth complete...
    -If growth complete then for every yield count...
    --generate contents of treasure pool (a list of item descriptors)
    ---for each of those add them to the container (same way as before).
    --determine if the plant is perennial or annual...
    ---if perennial then simply reset (regenerate) our growth data to fit the reduced number of stages and resume...
    ---if annual then much like the old mechanic of consuming another seed.
    
    Open Questions:
    -The right way to handle 'water'?  Should it constantly use like before or use per growth.
    -Should the last growth stage be used?  I have a strong impression that the final stage is the old
     rot stage and should be ignored... It's usually a VERY large number (thousands) where other stages are hundreds or tens.
    -Should there be something to speed up the growth more?
    --Higher growth values per tick?
    --Division of growth stage (ie if total growth is 2000 then divide that by 5 or something).
    -(yield)Could generate multiples of a single treasure pool instead of multiple treasure pool iterations.
    
    TODO:
    Besides filling in some blanks and testing... really need to clean up this mess as there was a LOT of learning going on...
    Verify fertilizer mechanics are still working.

--- objects/crafting/fu_growingtray/fu_growingtray.lua
+++ objects/crafting/fu_growingtray/fu_growingtray.lua
@@ -4,20 +4,49 @@ local deltaTime=0
 function init()
 	transferUtil.init()
 	object.setInteractive(true)
-
+	
 	if storage.growth == nil then storage.growth = 0 end
 	if storage.water == nil then storage.water = 0 end
-	if storage.fertSpeed == nil then storage.fertSpeed = 0 end
-	if storage.fertYield == nil then storage.fertYield = 0 end
-        if storage.liquidMod == nil then storage.liquidMod = 2 end
-        if storage.seedMod == nil then storage.seedMod = 3 end
-	if storage.yield == nil then storage.yield = 0 end
-	if storage.growthcap == nil then storage.growthcap = config.getParameter("isn_growthCap") end
+	if storage.fertSpeed == nil then storage.fertSpeed = false end
+	if storage.fertYield == nil then storage.fertYield = false end
+	if storage.fertSpeed2 == nil then storage.fertSpeed2 = false end
+	if storage.fertYield2 == nil then storage.fertYield2 = false end
+	if storage.fertSpeed3 == nil then storage.fertSpeed3 = false end
+	if storage.fertYield3 == nil then storage.fertYield3 = false end
+	if storage.harvestPool == nil then storage.harvestPool = "" end
+	-- note: Need to go through and either have good place holder values or code that can handle bad vals.
+	-- Also reformat the structure some...
+	if storage.seedData == nil then	storage.seedData = {} end
+	if storage.seedData.stages == nil then storage.seedData.stages = -1 end
+	if storage.seedData.stage == nil then storage.seedData.stage = {} end
+	if storage.seedData.resetStage == nil then storage.seedData.resetStage = -1 end
+	if storage.growthData == nil then storage.growthData = {} end
+	if storage.growthData.cap == nil then storage.growthData.cap = config.getParameter("isn_growthCap") end
+	if storage.growthData.current == nil then storage.growthData.current = 0 end
+	if storage.growthData.stage == nil then storage.growthData.stage = {} end
+	if storage.yield == nil then storage.yield = 0 end	-- note: yield is now number of picks from pool
+	--if storage.growthcap == nil then storage.growthcap = 100 end	-- placeholder, is set per stage.
+	--if storage.growthcap == nil then storage.growthcap = config.getParameter("isn_growthCap") end
+	
 	if storage.activeConsumption == nil then storage.activeConsumption = false end
-
 	storage.seedslot = 1
 	storage.waterslot = 2
-	storage.fertslot = 3	
+	storage.fertslot = 3
+end
+
+function isn_genGrowthData(initStage)
+	if initStage == nil then initStage = 0 end
+	storage.growthData.current = initStage
+	storage.growthData.cap = 0
+	for index = 0, storage.seedData.stages do
+		local duration = storage.seedData.stage[tostring(index)]
+	    if index >= initStage then
+			--lua's random generator is inclusive to the given values and sticks to the type it was run with.
+			storage.growthData.cap = storage.growthData.cap + math.random(duration["min"],duration["max"])
+			storage.growthData.stage[tostring(index)] = storage.growthData.cap
+		end
+	end
+	return
 end
 
 function update(dt)
@@ -27,14 +56,16 @@ function update(dt)
 	else
 		deltaTime=deltaTime+dt
 	end
-
+	
 	storage.activeConsumption = false
 	
-	if storage.currentseed == nil or storage.currentcrop == nil then
+	--if storage.currentseed == nil or storage.currentcrop == nil then
+	if storage.currentseed == nil or storage.seedData.pool == nil then
 		if isn_doSeedIntake() ~= true then return end
 	end
 	
-	local growthperc = isn_getXPercentageOfY(storage.growth,storage.growthcap)
+	--local growthperc = isn_getXPercentageOfY(storage.growth,storage.growthcap)
+	local growthperc = isn_getXPercentageOfY(storage.growth, storage.growthData.cap)
 	if growthperc >= 75 then
 		animator.setAnimationState("growth", "3")
 	elseif growthperc >= 50 then
@@ -45,20 +76,55 @@ function update(dt)
 		animator.setAnimationState("growth", "0")
 	end
 	
-	if storage.water <= 0 and isn_doWaterIntake() ~= true then return end
-	storage.water = storage.water - 1
+	-- instead of water every tick we only check/use water every growth period.
+	--if storage.water <= 0 and isn_doWaterIntake() ~= true then return end
+	if storage.growth >= storage.growthData.stage[tostring(storage.growthData.current)] then
+		if storage.water <= 0 and isn_doWaterIntake() ~= true then return end
+		-- water situation is OK...
+		storage.growthData.current = storage.growthData.current + 1
+		if storage.growthData.current <= storage.seedData.stages then
+			storage.water = storage.water - 1
+		end
+	end
 	storage.activeConsumption = true
 	storage.growth = storage.growth + 1
-	
-	if storage.fertSpeed then
-		storage.growth = storage.growth + storage.fertSpeed + 1
+	if storage.fertSpeed == true then
+		storage.growth = storage.growth + 1
 	end
-	
-	if storage.growth >= storage.growthcap then
-		world.containerAddItems(entity.id(), {name = storage.currentseed, count = math.random(1,3), data={}})
+	if storage.fertSpeed2 == true then
+	        storage.growth = storage.growth + 2
+	end
+	if storage.fertSpeed3 == true then
+	        storage.growth = storage.growth + 3
+	end
+	if storage.growth >= storage.growthData.cap then
+		-- spawn items from the treasurepool or silently fail...
+		if root.isTreasurePool(storage.seedData.pool) then
+			for i = 1, storage.yield do
+				local itemDescriptors = root.createTreasure(storage.seedData.pool, world.threatLevel())
+				for index, itemDescriptor in pairs(itemDescriptors) do
+					fu_sendOrStoreItems(0, itemDescriptor)
+					world.containerAddItems(entity.id(), itemDescriptor)
+				end
+			end
+		end
+		-- use up some fertilizer for the next planting...
+		isn_doFertIntake()
+		-- conditionally reset the growth of the current seed or use a seed and start up again.
+		if storage.seedData.resetStage > 0 then
+			storage.growth = 0
+			isn_genGrowthData(storage.seedData.resetStage)
+		else
+			isn_doSeedIntake()
+		end
+		--[[
+		-- if connected to an object receiver, try to send the crop, else store loally
+		-- Wired Industry's item router is one such device
 		fu_sendOrStoreItems(0, {name = storage.currentcrop, count = storage.yield}, {0, 1, 2})
+		world.containerAddItems(entity.id(), {name = storage.currentseed, count = math.random(1,2), data={}})
 		isn_doFertIntake()
 		isn_doSeedIntake()
+		]]--
 	end
 end
 
@@ -70,7 +136,7 @@ function isn_doWaterIntake()
 	for key, value in pairs(config.getParameter("isn_waterInputs")) do
 		if water.name == key then
 			storage.water = value
-			world.containerConsume(entity.id(), {name = water.name, count = storage.liquidMod, data={}})
+			world.containerConsume(entity.id(), {name = water.name, count = 1, data={}})
 			return true
 		end
 	end
@@ -80,91 +146,89 @@ end
 function isn_doSeedIntake()
 	storage.growth = 0
 	storage.currentseed = nil
-	storage.currentcrop = nil
+	--storage.currentcrop = nil
 	local contents = world.containerItems(entity.id())
 	local seed = contents[storage.seedslot]
-	if seed == nil then return false end
 	
+	if seed == nil then return false end
+	local seedConfig = root.itemConfig(seed).config
+	if seedConfig["objectType"] ~= "farmable" then return false end
+	--store key properties of the seed.
+	for key, value in pairs(seedConfig.stages) do
+		if value.duration ~= nil then
+			storage.seedData.stages = storage.seedData.stages+1
+			storage.seedData.stage[tostring(storage.seedData.stages)] = {}
+			storage.seedData.stage[tostring(storage.seedData.stages)].min = value.duration[1]
+			storage.seedData.stage[tostring(storage.seedData.stages)].max = value.duration[2]
+		else
+			if value.harvestPool ~= nil then storage.seedData.pool = value.harvestPool end
+			if value.resetToStage ~= nil then storage.seedData.resetStage = value.resetToStage end
+		end
+	end
+	--set how many picks of the pool we get.
+	storage.yield = 1
+	--modify the number of picks by fertilizer in use.
+	if storage.fertYield == true then storage.yield = storage.yield * 2 end
+	if storage.fertYield2 == true then storage.yield = storage.yield * 3 end
+	if storage.fertYield3 == true then storage.yield = storage.yield * 4 end
+	--randomize our time to the next growth stage
+	isn_genGrowthData()
+	--storage.growthcap = randomduration(storage.seedData.stage[0])
+	--consume a seed.
+	world.containerConsume(entity.id(), {name = seed.name, count = 1, data={}})
+	--note the seed...
+	storage.currentseed = seed
+	return true
+	
+	--[[
 	for key, value in pairs(config.getParameter("isn_seedOutputs")) do
 		if seed.name == key then
 			storage.currentseed = key
 			storage.currentcrop = value
 			storage.yield = math.random(3,5)
-			if storage.fertYield == 1 then storage.yield = storage.yield * 2 end
-			if storage.fertYield == 2 then storage.yield = storage.yield * 3 end
-			if storage.fertYield == 3 then storage.yield = storage.yield * 4 end
-			
-			world.containerConsume(entity.id(), {name = seed.name, count = storage.seedMod, data={}})
 			return true
 		end
 	end
 	return false
+	]]--
 end
 
 function isn_doFertIntake()
-	storage.fertSpeed = 0
-	storage.fertYield = 0
-
+	storage.fertSpeed = false
+	storage.fertYield = false
+	storage.fertSpeed2 = false
+	storage.fertYield2 = false
+	storage.fertSpeed3 = false
+	storage.fertYield3 = false
 	local contents = world.containerItems(entity.id())
 	local fert = contents[storage.fertslot]
 	if fert == nil then return false end
 	
 	for key, value in pairs(config.getParameter("isn_fertInputs")) do
 		if fert.name == key then
-			if value == 1 then --basic speed
-				storage.fertSpeed = 1
-				storage.liquidMod = 2
-				storage.seedMod = 3
+			if value == 1 then
+				storage.fertSpeed = true
 				world.containerConsume(entity.id(), {name = fert.name, count = 1, data={}})
 				return true
-			elseif value == 2 then -- basic yield
-				storage.fertYield = 1
-				storage.liquidMod = 2
-				storage.seedMod = 3
+			elseif value == 2 then
+				storage.fertYield = true
 				world.containerConsume(entity.id(), {name = fert.name, count = 1, data={}})
 				return true
-			elseif value == 3 then --basic speed, good yield
-				storage.fertSpeed = 1
-				storage.fertYield = 2
-				storage.liquidMod = 2
-				storage.seedMod = 3
+			elseif value == 3 then
+				storage.fertSpeed = true
+				storage.fertYield = true
 				world.containerConsume(entity.id(), {name = fert.name, count = 1, data={}})
 				return true
-			elseif value == 4 then -- good speed, basic yield
-				storage.fertSpeed = 2
-				storage.fertYield = 1
-				storage.liquidMod = 2
-				storage.seedMod = 3
+			elseif value == 4 then
+				storage.fertSpeed2 = true
+				storage.fertYield2 = true
 				world.containerConsume(entity.id(), {name = fert.name, count = 1, data={}})
 				return true
-			elseif value == 5 then -- great speed,good yield
-				storage.fertSpeed = 3
-				storage.fertYield = 2
-				storage.liquidMod = 2
-				storage.seedMod = 2
-				world.containerConsume(entity.id(), {name = fert.name, count = 1, data={}})
-				return true
-			elseif value == 6 then -- great speed,great yield
-				storage.fertSpeed = 2
-				storage.fertYield = 3	
-				storage.liquidMod = 2
-				storage.seedMod = 1
-				world.containerConsume(entity.id(), {name = fert.name, count = 1, data={}})
-				return true	
-			elseif value == 7 then -- low liquid consumption
-				storage.liquidMod = 1	
-				storage.fertSpeed = 1
-				storage.fertYield = 1
-				storage.seedMod = 2
-				world.containerConsume(entity.id(), {name = fert.name, count = 1, data={}})
-				return true	
 			else
-				storage.liquidMod = 2	
-				storage.fertSpeed = 0
-				storage.fertYield = 0
-				storage.seedMod = 3
+				storage.fertSpeed3 = true
+				storage.fertYield3 = true				
 				world.containerConsume(entity.id(), {name = fert.name, count = 1, data={}})
-				return true			
+				return true
 			end
 		end
 	end
