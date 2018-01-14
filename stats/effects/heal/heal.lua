e05b0bcc808f0b756a074278b21e75712bbffa6e
sayterdarkwynd <sayterdarkwynd@gmail.com>
2018-01-14 15:05:04 -0500

    a further edit

--- stats/effects/heal/heal.lua
+++ stats/effects/heal/heal.lua
@@ -5,15 +5,11 @@ function init()
 
   script.setUpdateDelta(5)
   self.penaltyRate = config.getParameter("penaltyRate",0)
-  self.healingRate = (config.getParameter("healAmount", 5) - self.penaltyRate) / effect.duration()
+  self.healingRate = config.getParameter("healAmount", 5) / effect.duration()
 end
 
 function update(dt)
-  if not status.stat("healingStatusImmunity") then
-    status.modifyResource("health", self.healingRate * dt)
-  else
-    effect.expire()
-  end
+    status.modifyResource("health", (self.healingRate - self.penaltyRate) * dt)
 end
 
 function uninit()
