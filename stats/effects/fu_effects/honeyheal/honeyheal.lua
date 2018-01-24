b6ac71a5aff0b9b510677e1feac5442ca22dcd5f
sayterdarkwynd <sayterdarkwynd@gmail.com>
2018-01-24 14:43:39 -0500

    recipe adjusts

--- stats/effects/fu_effects/honeyheal/honeyheal.lua
+++ stats/effects/fu_effects/honeyheal/honeyheal.lua
@@ -4,7 +4,7 @@ function init()
   animator.setParticleEmitterActive("healing", true)
 
   script.setUpdateDelta(5)
-
+  self.penaltyRate = config.getParameter("penaltyRate",0)
   self.healingRate = config.getParameter("healAmount", 1.5) / effect.duration()
 end
 
