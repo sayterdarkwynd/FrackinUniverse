4eb47ec9480d9b3a582fb4536a3d984b83c7aab8
sayterdarkwynd <sayterdarkwynd@gmail.com>
2018-02-08 11:32:20 -0500

    set bonus for hacker striker use

--- stats/effects/fu_armoreffects/set_bonuses/hackersetbonuseffect.lua
+++ stats/effects/fu_armoreffects/set_bonuses/hackersetbonuseffect.lua
@@ -1,6 +1,8 @@
 setName="fu_hackerset"
 
-weaponBonus={}
+weaponBonus={
+  {stat = "critChance", amount = 1}
+}
 
 armorBonus={
 	{stat = "energyRegenPercentageRate", baseMultiplier = 1.25},
@@ -11,12 +13,26 @@ require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
 
 function init()
 	setSEBonusInit(setName)
-
+	weaponBonusHandle=effect.addStatModifierGroup({})
+	checkWeapons()
 	armorBonusHandle=effect.addStatModifierGroup(armorBonus)
 end
 
 function update(dt)
-if not checkSetWorn(self.setBonusCheck) then
-	effect.expire()
+	if not checkSetWorn(self.setBonusCheck) then
+		effect.expire()
+	end
 end
+
+function checkWeapons()
+	local weapons=weaponCheck({"flail"})
+	if weapons["either"] then
+		effect.setStatModifierGroup(weaponBonusHandle,weaponBonus)
+	else
+		effect.setStatModifierGroup(weaponBonusHandle,{})
+	end
 end
+
+
+
+
