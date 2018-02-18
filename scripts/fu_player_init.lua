5103165533d02a6adc4c27943aaaa01aa1457885
zimberzimber <tral2@walla.com>
2018-02-18 06:04:36 +0200

    lootbox stuff

--- scripts/fu_player_init.lua
+++ scripts/fu_player_init.lua
@@ -1,5 +1,6 @@
 
 local origInit = init
+local origUninit = uninit
 
 function init()
 	origInit()
@@ -31,4 +32,51 @@ function init()
 	--]]
 	
 	sb.logInfo("Frackin' Universe version %s", data.version)
+end
+
+function uninit()
+	local untieredLootboxes = player.hasCountOfItem({name = "fu_lootbox", parameters = {}}, true)
+	if untieredLootboxes and untieredLootboxes > 0 then
+		local threatLevel = roundNum(world.threatLevel())
+		for i = 1, untieredLootboxes do
+			player.consumeItem({name = "fu_lootbox", parameters = {}}, true, true)
+			player.giveItem({name = "fu_lootbox", parameters = {level = threatLevel}})
+		end
+	end
+	
+	sb.logError("")
+	sb.logError("Uninit found %s untiered lootboxes.", untieredLootboxes)
+	sb.logError("")
+	
+	--[[
+	local goods = {"foodgoods", "medicalgoods", "electronicgoods", "militarygoods"}
+	for _, g in ipairs(goods) do
+		local amount = player.hasCountOfItem({name = g})
+		if amount > 0 then
+			player.consumeItem({name = g, count = amount})
+			player.addCurrency("fu"..g, amount)
+			
+			sb.logInfo("converted %s '%s' into '%s' currency", amount, g, "fu"..g)
+		end
+	end
+	--]]
+end
+
+function roundNum(num)
+	local low = math.floor(num)
+	local high = math.ceil(num)
+	
+	if math.abs(num - low) < math.abs(num - high) then
+		if num < 0 then
+			return low * -1
+		else
+			return low
+		end
+	else
+		if num < 0 then
+			return high * -1
+		else
+			return high
+		end
+	end
 end
