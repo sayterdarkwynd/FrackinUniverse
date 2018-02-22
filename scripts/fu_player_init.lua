e95be4c783d7a8724e9b87bba3f66505a77b7c8b
zimberzimber <tral2@walla.com>
2018-02-22 09:04:14 +0200

    stuph

--- scripts/fu_player_init.lua
+++ scripts/fu_player_init.lua
@@ -49,7 +49,7 @@ end
 function uninit()
 	local untieredLootboxes = player.hasCountOfItem({name = "fu_lootbox", parameters = {}}, true)
 	if untieredLootboxes and untieredLootboxes > 0 then
-		local threatLevel = roundNum(world.threatLevel())
+		local threatLevel = math.floor(world.threatLevel() + 0.5)
 		for i = 1, untieredLootboxes do
 			player.consumeItem({name = "fu_lootbox", parameters = {}}, true, true)
 			player.giveItem({name = "fu_lootbox", parameters = {level = threatLevel}})
@@ -72,23 +72,4 @@ function uninit()
 		end
 	end
 	--]]
-end
-
-function roundNum(num)
-	local low = math.floor(num)
-	local high = math.ceil(num)
-	
-	if math.abs(num - low) < math.abs(num - high) then
-		if num < 0 then
-			return low * -1
-		else
-			return low
-		end
-	else
-		if num < 0 then
-			return high * -1
-		else
-			return high
-		end
-	end
 end
