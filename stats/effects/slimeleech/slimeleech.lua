d570ddc78f8478eb79893d0edc4c46788781371a
sayterdarkwynd <sayterdarkwynd@gmail.com>
2018-04-02 17:33:55 -0400

    slime stuff

---
+++ stats/effects/slimeleech/slimeleech.lua
@@ -0,0 +1,53 @@
+require "/scripts/util.lua"
+
+function init()
+  animator.setParticleEmitterOffsetRegion("sparks", mcontroller.boundBox())
+  animator.setParticleEmitterActive("sparks", false)
+  effect.setParentDirectives("fade=7733AA=0.25")
+
+  script.setUpdateDelta(5)
+
+  self.damageClampRange = config.getParameter("damageClampRange")
+
+  self.tickTime = config.getParameter("boltInterval", 1.0)
+  self.tickTimer = self.tickTime
+end
+
+function update(dt)
+  self.tickTimer = self.tickTimer - dt
+  local boltPower = util.clamp(status.resourceMax("health") * config.getParameter("healthDamageFactor", 1.0), self.damageClampRange[1], self.damageClampRange[2])
+  if self.tickTimer <= 0 then
+    self.tickTimer = self.tickTime
+    local targetIds = world.entityQuery(mcontroller.position(), config.getParameter("jumpDistance", 80), {
+      withoutEntityId = entity.id(),
+      includedTypes = {"creature"}
+    })
+
+    shuffle(targetIds)
+
+    for i,id in ipairs(targetIds) do
+      local sourceEntityId = entity.id("crew", "player")
+      if world.entityCanDamage(sourceEntityId, id) and not world.lineTileCollision(mcontroller.position(), world.entityPosition(id)) then
+        local sourceDamageTeam = world.entityDamageTeam(sourceEntityId)
+        local directionTo = world.distance(world.entityPosition(id), mcontroller.position())
+        world.spawnProjectile(
+          "slimedaggerleech",
+          mcontroller.position(),
+          entity.id(),
+          directionTo,
+          false,
+          {
+            power = 0,
+            damageTeam = sourceDamageTeam
+          }
+        )
+        animator.playSound("bolt")
+        return
+      end
+    end
+  end
+end
+
+function uninit()
+
+end
