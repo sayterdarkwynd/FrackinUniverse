ffeb7d5f95a553eac6ed9915de630d6fa2fe9dc8
sayterdarkwynd <sayterdarkwynd@gmail.com>
2018-04-14 23:18:40 -0400

    effect organisation

--- stats/effects/freeze/freeze.lua
+++ stats/effects/freeze/freeze.lua
@@ -1,17 +1,17 @@
 function init()
-  animator.setParticleEmitterOffsetRegion("snow", mcontroller.boundBox())
-  animator.setParticleEmitterActive("snow", true)
-  effect.setParentDirectives("fade=DDDDFF=0.5")
-
+  if config.getParameter("isFreeze",0)==1 then  
+    animator.setParticleEmitterOffsetRegion("snow", mcontroller.boundBox())
+    animator.setParticleEmitterActive("snow", true)
+    effect.setParentDirectives("fade=DDDDFF=0.5")
+  end
+  
   local stuns = status.statusProperty("stuns", {})
   stuns["freeze"] = true
   status.setStatusProperty("stuns", stuns)
 end
 
 function update(dt)
-  mcontroller.controlModifiers({
-      movementSuppressed = true
-    })
+  mcontroller.controlModifiers({movementSuppressed = true})
 end
 
 function uninit()
