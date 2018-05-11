fab82395a9b62058f0199875887ee81312b58666
Kherae <cwaldridge@live.com>
2018-05-11 05:58:22 -0700

    slow update

--- stats/effects/fu_tileeffects/mobility_effects/slow.lua
+++ stats/effects/fu_tileeffects/mobility_effects/slow.lua
@@ -1,43 +1,27 @@
 function init()
-  animator.setParticleEmitterOffsetRegion("drips", mcontroller.boundBox())
-  animator.setParticleEmitterActive("drips", true)
-  
-  --check types
-  if config.getParameter("isHoney",0)==1 then
-    activateVisualEffectsHoney()
-  end
-  if config.getParameter("isTar",0)==1 then
-    activateVisualEffectsTar()
-  end 
-  
+	animator.setParticleEmitterOffsetRegion("drips", mcontroller.boundBox())
+	animator.setParticleEmitterActive("drips", true)
+
+	--check types
+	if config.getParameter("isHoney",0)==1 then
+		activateVisualEffects("fade=edcd5c=0.2")
+	end
+	if config.getParameter("isTar",0)==1 then
+		activateVisualEffects("fade=300030=0.8")
+	end 
+
 end
 
 function update(dt)
-  mcontroller.controlModifiers({
-      groundMovementModifier = config.getParameter("moveMod",1),
-      speedModifier = config.getParameter("speedMod",1),
-      airJumpModifier = config.getParameter("jumpMod",1)     
-    })
+	mcontroller.controlModifiers({
+		groundMovementModifier = config.getParameter("moveMod",1),
+		speedModifier = config.getParameter("speedMod",1),
+		airJumpModifier = config.getParameter("jumpMod",1)     
+	})
 end
 
-function activateVisualEffectsHoney()
-  animator.setParticleEmitterOffsetRegion("drips", mcontroller.boundBox())
-  animator.setParticleEmitterActive("drips", true)
-  effect.setParentDirectives("fade=edcd5c=0.2")
-  local statusTextRegion = { 0, 1, 0, 1 }
-  animator.setParticleEmitterOffsetRegion("statustext", statusTextRegion)
-  animator.burstParticleEmitter("statustext")
-end
-
-function activateVisualEffectsTar()
-  animator.setParticleEmitterOffsetRegion("drips", mcontroller.boundBox())
-  animator.setParticleEmitterActive("drips", true)
-  effect.setParentDirectives("fade=300030=0.8")
-  local statusTextRegion = { 0, 1, 0, 1 }
-  animator.setParticleEmitterOffsetRegion("statustext", statusTextRegion)
-  animator.burstParticleEmitter("statustext")
-end
-
-function uninit()
-
+function activateVisualEffects(directives)
+	effect.setParentDirectives(directives)
+	animator.setParticleEmitterOffsetRegion("statustext", { 0, 1, 0, 1 })
+	animator.burstParticleEmitter("statustext")
 end
