d29c89114dce47f497b51621efeaf4ebc58d125a
Kherae <cwaldridge@live.com>
2018-05-16 08:16:21 -0700

    fixy stupid

--- stats/effects/fu_genericLightDirection.lua
+++ stats/effects/fu_genericLightDirection.lua
@@ -1,6 +1,6 @@
-oldUpdate=update
+oldUpdateLightDirection=update
 
 function update(dt)
-  animator.setFlipped(mcontroller.facingDirection() == -1)
-  if oldUpdate then oldUpdate(dt) end
+	animator.setFlipped(mcontroller.facingDirection() == -1)
+	if oldUpdateLightDirection then oldUpdateLightDirection(dt) end
 end
