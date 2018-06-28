54b0118fcd5b49d38cf9ad8e4cfe9324c1b6d4f1
sayterdarkwynd <sayterdarkwynd@gmail.com>
2018-06-28 11:37:18 -0400

    moar utilities

--- objects/minibiome/elder/elderaltar/elderaltar.lua
+++ objects/minibiome/elder/elderaltar/elderaltar.lua
@@ -25,7 +25,9 @@ local recipes =
 {inputs = { nocxiumbar=5, elderrelic8=1, elderrelic14=1 }, outputs = { cultarmorpants=1 }, time = 15.0 },
 {inputs = { cthulureward=1, elderrelic3=2, elderrelic14=1 }, outputs = { eldermace=1 }, time = 15.0 },
 {inputs = { elderrelic12=5, elderrelic4=1, elderrelic8=1 }, outputs = { eldershotgun=1 }, time = 15.0 },
-{inputs = { cthulureward=1, elderrelic1=3, elderrelic11=2 }, outputs = { eldergrenadelauncher=1 }, time = 15.0 }
+{inputs = { cthulureward=1, elderrelic1=3, elderrelic11=2 }, outputs = { eldergrenadelauncher=1 }, time = 15.0 },
+{inputs = { shoggothessence=5}, outputs = { shoggothflesh=1 }, time = 1.0 },
+{inputs = { shoggothflesh=1}, outputs = { essence=1000,fuscienceresource=1000 }, time = 1.0 }
 }
 
 function init()
