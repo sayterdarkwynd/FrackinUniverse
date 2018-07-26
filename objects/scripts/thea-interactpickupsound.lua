42f55f7bad7a49a57b473731bd3e69e3f736bfd2
sayterdarkwynd <sayterdarkwynd@gmail.com>
2018-07-26 09:34:25 -0400

    conversion

--- objects/scripts/thea-interactpickupsound.lua
+++ objects/scripts/thea-interactpickupsound.lua
@@ -1,32 +1,35 @@
 function init()
-  if storage.itemHasSpawned == false or storage.itemHasSpawned == nil then
-    object.setInteractive(true)
-    animator.setAnimationState("interactiveObject", "filled")
-	storage.itemHasSpawned = false
-  else
-    animator.setAnimationState("interactiveObject", "empty")
-	object.setInteractive(false)
-  end
-  self.spawnableItem = config.getParameter("spawnableItem")
-  world.spawnProjectile("soundeffect_pickup", object.position(),entity.id(), {0,0})
+	if storage.itemHasSpawned == false or storage.itemHasSpawned == nil then
+		object.setInteractive(true)
+		animator.setAnimationState("interactiveObject", "filled")
+		storage.itemHasSpawned = false
+	else
+		animator.setAnimationState("interactiveObject", "empty")
+		object.setInteractive(false)
+	end
+	
+	self.spawnableItem = config.getParameter("spawnableItem")
 end
 
 function open()
-  animator.setAnimationState("interactiveObject", "empty")
-  world.spawnItem(self.spawnableItem, entity.position(), 1)
-  storage.itemHasSpawned = true
-  object.setInteractive(false)
-   
-  --Make sure that, if the object is broken after having been collected, nothing drops
-  object.setConfigParameter("breakDropPool", "empty")
+	animator.setAnimationState("interactiveObject", "empty")
+	world.spawnItem(self.spawnableItem, entity.position(), 1)
+	if animator.hasSound("pickup") then
+		animator.playSound("pickup")
+	end
+	storage.itemHasSpawned = true
+	object.setInteractive(false)
+	
+	--Make sure that, if the object is broken after having been collected, nothing drops
+	object.setConfigParameter("breakDropPool", "empty")
 end
 
 function onInteraction(args)
-  if storage.itemHasSpawned == false then
-    open()
-  end
+	if storage.itemHasSpawned == false then
+		open()
+	end
 end
 
 function update(dt) 
-  
+	
 end
