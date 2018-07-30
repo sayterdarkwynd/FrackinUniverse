6cbe2a1fafa134396291a5332f3e4f2cec4ea0b5
Kherae <cwaldridge@live.com>
2018-07-30 13:27:09 -0700

    another idiot fix

--- stats/effects/biomeairless/biomeairless.lua
+++ stats/effects/biomeairless/biomeairless.lua
@@ -11,11 +11,11 @@ function sendMessage(force)
 	if not world.breathable(entity.position()) then
 		if status.isResource(warningResource) then
 			if not status.resourcePositive(warningResource) then
-				world.sendEntitysendMessage(entity.id(), "queueRadioMessage", "biomeairless", 1.0)
+				world.sendEntityMessage(entity.id(), "queueRadioMessage", "biomeairless", 1.0)
 			end
 			status.setResourcePercentage(warningResource,1.0)
 		elseif force then
-			world.sendEntitysendMessage(entity.id(), "queueRadioMessage", "biomeairless", 30.0)
+			world.sendEntityMessage(entity.id(), "queueRadioMessage", "biomeairless", 30.0)
 		end
 	end
 end
