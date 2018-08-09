4f2076de695b7ad58012f8b2f920b59f0cd3ea2c
sayterdarkwynd <sayterdarkwynd@gmail.com>
2018-08-09 00:11:08 -0400

    trap

--- scripts/lobstertrap.lua
+++ scripts/lobstertrap.lua
@@ -5,18 +5,27 @@ function init()
 end
 
 function update(dt)
+    self.depth = world.oceanLevel(entity.position())
+    --sb.logInfo("depth is = "..self.depth)
+     
+    if not world.liquidAt(entity.position()) or self.depth < 50 then  --cannot work unless in ocean liquid
+      return 
+    end   
 
-  if world.type() == "ocean" and world.oceanLevel(entity.position()) then
-  
+    if world.type() == "ocean" or world.type() == "arctic" or world.type() == "tidewater" then
+
 	  transferUtil.loadSelfContainer()
 	  storage.waterCount = math.min((storage.waterCount or 0) + dt,100)
+	  
 	  for i=2,#config.getParameter('wellslots') do
 	    if world.containerItemAt(entity.id(),i-1) and world.containerItemAt(entity.id(),i-1).name ~= config.getParameter('wellslots')[i].name then
 	      world.containerConsumeAt(entity.id(),i-1,world.containerItemAt(entity.id(),i-1).count)
 	      world.spawnItem(world.containerItemAt(entity.id(),i-1),entity.position())
 	    end
 	  end
+	  
 	  local item = world.containerItemAt(entity.id(),0) or {name=config.getParameter('wellslots')[1].name,count=0}
+	  
 	  if item.name ~= config.getParameter('wellslots')[1].name then
 	    world.spawnItem(item,entity.position())
 	    world.containerConsumeAt(entity.id(),0,item.count)
@@ -28,9 +37,11 @@ function update(dt)
 	    world.containerConsumeAt(entity.id(),0,dropitem.count)
 	    item.count = config.getParameter('wellslots')[1].max
 	  end
+	  
 	  local amount = math.min(math.floor(storage.waterCount/config.getParameter('wellslots')[1].ratio),config.getParameter('wellslots')[1].max - item.count)
 	  world.containerPutItemsAt(entity.id(),{name=config.getParameter('wellslots')[1].name,count=amount},0)
 	  storage.waterCount = storage.waterCount - amount * config.getParameter('wellslots')[1].ratio
+	  
 	  if amount > 0 and #config.getParameter('wellslots') > 1 then
 	    for i=(storage.count or 0)+1,(storage.count or 0)+amount do
 	      for j=2,#config.getParameter('wellslots') do
