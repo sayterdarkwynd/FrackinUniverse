35c47a9ed0fd044e09ea8047d52c5cd988093404
Kherae <cwaldridge@live.com>
2018-08-21 18:23:01 -0700

    derp. fix!

--- stats/effects/biomeairless/biomeairless.lua
+++ stats/effects/biomeairless/biomeairless.lua
@@ -2,7 +2,7 @@ require "/stats/effects/fu_weathereffects/fuWeatherLib.lua"
 
 function update(dt)
 	if not world.breathable(world.entityMouthPosition(entity.id())) then
-		warningTimer=(warningTimer or script.updateDt()*2)-dt
+		warningTimer=(warningTimer or script.updateDt()*4)-dt
 		if warningTimer<=0 then
 			warningTimer=fuWeatherLib.warn("biomeairlesswarning","biomeairless") and 1 or math.huge
 		end
