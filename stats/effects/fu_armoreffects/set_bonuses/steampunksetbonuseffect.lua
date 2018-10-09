bfdc2c84ba73dee9f602495b8c001f253fde39c4
sayterdarkwynd <sayterdarkwynd@gmail.com>
2018-10-09 08:07:44 -0400

    steampunk adjust

--- stats/effects/fu_armoreffects/set_bonuses/steampunksetbonuseffect.lua
+++ stats/effects/fu_armoreffects/set_bonuses/steampunksetbonuseffect.lua
@@ -37,7 +37,7 @@ end
 
 function 
 	checkWeapons()
-	local weapons=weaponCheck({"tesla","electric"})
+	local weapons=weaponCheck({"tesla","electric","energy"})
 	if weapons["either"] then
 		effect.setStatModifierGroup(effectHandlerList.weaponBonusHandle,weaponBonus)
 	else
