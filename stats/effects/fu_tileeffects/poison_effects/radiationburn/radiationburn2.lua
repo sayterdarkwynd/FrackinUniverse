231cf0fd613d2de020f017eb711a6695e4de4ebf
sayterdarkwynd <sayterdarkwynd@gmail.com>
2019-01-02 22:00:11 -0500

    v

--- stats/effects/fu_tileeffects/poison_effects/radiationburn/radiationburn2.lua
+++ stats/effects/fu_tileeffects/poison_effects/radiationburn/radiationburn2.lua
@@ -4,30 +4,35 @@ function init()
   animator.setParticleEmitterActive("fx", true)
   effect.setParentDirectives("fade=00FF33=0.15")
   script.setUpdateDelta(5)
-  self.tickDamagePercentage = 0.2
-  self.tickTime = 0.05
+  self.tickDamagePercentage = 0.03
+  self.tickTime = 0.1
   self.tickTimer = self.tickTime
+  self.statusValue = status.stat("physicalResistance",0)
 end
 
-
-
 function update(dt)
-	if (status.stat("radioactiveResistance",0)  >= 2) then
-	  effect.expire()
-	end
-	
+  if (status.stat("radioactiveResistance",0)  >= 0.4) then
+    effect.expire()
+  end
+  
+  
   self.tickTimer = self.tickTimer - dt
+  self.statusValue = self.statusValue - dt
   if self.tickTimer <= 0 then
-    self.tickTimer = self.tickTime
-    status.applySelfDamageRequest({
-        damageType = "IgnoresDef",
-        damage = math.floor(status.resourceMax("health") * self.tickDamagePercentage) + 1.5,
-        damageSourceKind = "radioactive",
-        sourceEntityId = entity.id()
-      })
+      	self.tickTimer = self.tickTime
+	if self.statusValue > 0.05 then
+	  status.setPersistentEffects("aciddust", { {stat = "physicalResistance", amount = self.statusValue } })
+        else 
+	    status.applySelfDamageRequest({
+		damageType = "IgnoresDef",
+		damage = math.floor(status.resourceMax("health") * self.tickDamagePercentage),
+		damageSourceKind = "radioactive",
+		sourceEntityId = entity.id()
+	    })          
+	end  
   end
 end
 
 function uninit()
-
+  status.clearPersistentEffects("aciddust")
 end
