52c64b194cca764e52df996db267b6d9fdad5e01
sayterdarkwynd <sayterdarkwynd@gmail.com>
2019-01-28 08:07:32 -0500

    armor descriptions

--- stats/effects/fu_armoreffects/set_bonuses/slimesetbonuseffect.lua
+++ stats/effects/fu_armoreffects/set_bonuses/slimesetbonuseffect.lua
@@ -1,7 +1,7 @@
 require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
 
 weaponBonus={
-	{stat = "critChance", amount = 6},
+	{stat = "critChance", amount = 5},
 	{stat = "powerMultiplier", effectiveMultiplier = 1.10}
 }
 
