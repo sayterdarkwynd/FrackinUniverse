54be24942341147c70dbfb3c3a5f8e576b471834
wannas16 <wein.lau@gmail.com>
2019-02-10 15:10:30 +1100

    [fu_armor_set_bonus_revamp] Added extra checks in SetBonusManager.lua so that it doesn't loop through the armour set config unless the player's equipped armour changes.

--- stats/effects/fu_armoreffects/new/SetBonusManager.lua
+++ stats/effects/fu_armoreffects/new/SetBonusManager.lua
@@ -4,6 +4,9 @@ function init()
   -- Read armor set config.
   self.armorSets = root.assetJson("/stats/effects/fu_armoreffects/new/armorsets.config")
   self.activeSet = nil
+  self.currentHead = nil
+  self.currentChest = nil
+  self.currentLegs = nil
   script.setUpdateDelta(5)
 end
 
@@ -11,20 +14,33 @@ function uninit()
 end
 
 function update()
-  local newSet = nil
-  for setStat, setEffect in pairs(self.armorSets) do
-    if (status.stat(setStat) == 3) then
-      newSet = setEffect
-      break
+  -- Check if player's equipped armour.
+  local newHead = player.equippedItem("head")
+  local newChest = player.equippedItem("chest")
+  local newLegs = player.equippedItem("legs")
+  -- Only check set bonuses if armour has changed!
+  if ((self.currentHead ~= newHead) or
+      (self.currentChest ~= newChest) or
+      (self.currentLegs ~= newLegs)) then
+    self.currentHead = newHead
+    self.currentChest = newChest
+    self.currentLegs = newLegs
+    -- Loop through armour sets and check for a complete set (count = 3).
+    local newSet = nil
+    for setStat, setEffect in pairs(self.armorSets) do
+      if (status.stat(setStat) == 3) then
+        newSet = setEffect
+        break
+      end
     end
-  end
-  if (newSet ~= self.activeSet) then
-    if (self.activeSet ~= nil) then
-      status.clearPersistentEffects("setbonus")
+    if (newSet ~= self.activeSet) then
+      if (self.activeSet ~= nil) then
+        status.clearPersistentEffects("setbonus")
+      end
+      if (newSet ~= nil) then
+        status.addPersistentEffects("setbonus", {newSet})
+      end
+      self.activeSet = newSet
     end
-    if (newSet ~= nil) then
-      status.addPersistentEffects("setbonus", {newSet})
-    end
-    self.activeSet = newSet
   end
 end
