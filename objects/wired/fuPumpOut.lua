290bda6a8010640f7f994ed1b46f2c29206407a1
sayterdarkwynd <sayterdarkwynd@gmail.com>
2019-02-15 17:32:45 -0500

    edits

--- objects/wired/fuPumpOut.lua
+++ objects/wired/fuPumpOut.lua
@@ -1,22 +1,23 @@
 function init()
   storage.currentState = storage.currentState or false
-  output(storage.state)
+  output(storage.currentState)
 end
 
---ternary operator
 function toggleState(condition,iftrue,iffalse)
-    if condition then return iftrue else return iffalse end
+    if condition then 
+      return iftrue 
+    else 
+      return iffalse 
+    end
 end
 
--- Change Animation
 function output(state)
   if stateCurrent ~= storage.currentState then
     storage.currentState = stateCurrent
-    animator.setAnimationState("pumpoutState",toggleState(stateCurrent,"on","off"))
+    animator.setAnimationState("outputState",toggleState(stateCurrent,"on","off"))
   end
 end
 
---update animation and outputwire
 function update(dt)
     local isConnected = object.isOutputNodeConnected(0) and object.getInputNodeLevel(0)
     output(isConnected)
--- objects/wired/fuPumpOutPressurized.lua
+++ objects/wired/fuPumpOutPressurized.lua
@@ -3,22 +3,23 @@ function init()
   output(storage.stateCurrent)
 end
 
---ternary operator
 function toggleState(condition,iftrue,iffalse)
-    if condition then return iftrue else return iffalse end
+    if condition then 
+      return iftrue 
+    else 
+      return iffalse 
+    end
 end
 
--- Change Animation
 function output(state)
   if stateCurrent ~= storage.currentState then
     storage.currentState = stateCurrent
-    animator.setAnimationState("pumpoutpressureState",toggleState(stateCurrent,"on","off"))
+    animator.setAnimationState("outputPressurizedState",toggleState(stateCurrent,"on","off"))
   end
 end
 
---update animation and outputwire
 function update(dt)
-    local con = object.isOutputNodeConnected(0) and object.getInputNodeLevel(0)
-    output(con)
-    object.setAllOutputNodes(con)
+    local isConnected = object.isOutputNodeConnected(0) and object.getInputNodeLevel(0)
+    output(isConnected)
+    object.setAllOutputNodes(isConnected)
 end
