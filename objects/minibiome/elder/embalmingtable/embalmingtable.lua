77812ab331de36e361e19bf61018cbb19daeb455
sayterdarkwynd <sayterdarkwynd@gmail.com>
2019-04-23 22:01:05 -0400

    taste test

--- objects/minibiome/elder/embalmingtable/embalmingtable.lua
+++ objects/minibiome/elder/embalmingtable/embalmingtable.lua
@@ -1,10 +1,25 @@
 local recipes =
 {
-{inputs = { wrappedbody=1 }, outputs = { brain=1,fuscienceresource=120,bone=8,leather=1,alienmeat=4,faceskin=1,rawribmeat=2,liquidblood=40 }, time = 14.0},
-{inputs = { wrappedbodyputrid=1 }, outputs = { brain=1,fumadnessresource=5,bone=8,leather=1,rottingfleshmaterial=4,slew2=12,biospore=2,liquidbioooze=40 }, time = 14.0},
-{inputs = { wrappedbodyalien=1 }, outputs = { brain=1,fuprecursorresource=5,bone=8,leather=1,slew4=12,rawribmeat=2,cellmateria=4,liquidalienjuice=40 }, time = 14.0},
-{inputs = { greghead=1 }, outputs = { inferiorbrain=1,bone=4,slew2=3}, time = 14.0}
+--combos
+{inputs = { wrappedbody=1,elderrelic2=1 }, outputs = { inferiorbrain=1,fuscienceresource=520,bone=8,leather=1,alienmeat=4,faceskin=1,rawribmeat=2,liquidelderfluid=120  }, time = 14.0},
+{inputs = { wrappedbody=1,elderrelic10=1 }, outputs = { inferiorbrain=1,fumadnessresource=520,bone=8,leather=1,alienmeat=12,rawribmeat=2,liquidelderfluid=80  }, time = 14.0},
 
+--mains
+{inputs = { wrappedbody=1 }, outputs = { inferiorbrain=1,fuscienceresource=120,bone=8,leather=1,alienmeat=4,faceskin=1,rawribmeat=2,liquidblood=40 }, time = 14.0},
+{inputs = { wrappedbodyputrid=1 }, outputs = { inferiorbrain=1,fumadnessresource=5,bone=8,leather=1,rottingfleshmaterial=4,slew2=12,biospore=2,liquidbioooze=40 }, time = 14.0},
+{inputs = { wrappedbodyalien=1 }, outputs = { brain=1,fuprecursorresource=5,bone=8,leather=1,slew4=12,rawribmeat=2,cellmateria=4,liquidalienjuice=40 }, time = 14.0},
+{inputs = { greghead=1 }, outputs = { fumadnessresource=1,inferiorbrain=1,bone=4,slew2=3}, time = 9.0},
+{inputs = { faceskin=1 }, outputs = { fumadnessresource=1,liquidblood=2, slew2=1}, time = 7.0},
+{inputs = { leather=1 }, outputs = { slew2=1}, time = 7.0},
+{inputs = { hardenedcarapace=1 }, outputs = { slew2=1}, time = 7.0},
+{inputs = { agaranichor=1 }, outputs = { fuscienceresource=5}, time = 7.0},
+{inputs = { biospore=1 }, outputs = { cellmateria=1}, time = 7.0},
+{inputs = { blobbushjelly=1 }, outputs = { fumadnessresource=1,cellmateria=1}, time = 7.0},
+{inputs = { brain=1 }, outputs = { fumadnessresource=1,cellmateria=2}, time = 7.0},
+{inputs = { inferiorbrain=1 }, outputs = { fumadnessresource=1,cellmateria=1}, time = 7.0},
+{inputs = { bone=10 }, outputs = { bonemealmaterial=50}, time = 7.0},
+{inputs = { crunchychick=1 }, outputs = { fumadnessresource=1,bonemealmaterial=10,alienmeat=1}, time = 5.0},
+{inputs = { meatpickle=1 }, outputs = { fumadnessresource=1,alienmeat=1}, time = 2.0}
 }
 
 function init()
@@ -115,7 +130,10 @@ function update(dt)
 	    animator.playSound("bodydown")
         end
         if not self.crafting and self.timer <= 0 then --make sure we didn't just finish crafting
-            if not startCrafting(getValidRecipes(getInputContents())) then self.timer = self.mintick end --set timeout if there were no recipes
+            if not startCrafting(getValidRecipes(getInputContents())) then 
+              self.timer = self.mintick --set timeout if there were no recipes
+              animator.stopAllSounds("cutting")
+            end 
         end
     else
       if self.crafting then
@@ -125,6 +143,7 @@ function update(dt)
 		animator.playSound("cutting")
 	    end 
       end
+      
     end
 end
 
@@ -145,4 +164,8 @@ function startCrafting(result)
         animator.setAnimationState("samplingarrayanim", "working")
         return true
     end
+end
+
+function uninit()
+animator.stopAllSounds("cutting")
 end
