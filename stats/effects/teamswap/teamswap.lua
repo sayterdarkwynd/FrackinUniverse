bb29a87dbe85d5eafdf075f35868edcf10e70fc5
sayterdarkwynd <sayterdarkwynd@gmail.com>
2019-04-26 14:40:15 -0400

    madness bomb

--- stats/effects/teamswap/teamswap.lua
+++ stats/effects/teamswap/teamswap.lua
@@ -1,15 +1,11 @@
 function init()
-   --effect.addStatModifierGroup({{stat = "nude", amount = 1}})
+   effect.addStatModifierGroup({{stat = "isInsane", amount = 1}})
 
-   script.setUpdateDelta(0)
+   script.setUpdateDelta(5)
 end
 
 function update(dt)
- local sourceEntityId = effect.sourceEntity() or entity.id()
- local sourceDamageTeam = world.entityDamageTeam(sourceEntityId)
- 
- self.damageTeam = config.getParameter("damageTeam")
- sb.logInfo(self.damageTeam)
+
 end
 
 function uninit()
