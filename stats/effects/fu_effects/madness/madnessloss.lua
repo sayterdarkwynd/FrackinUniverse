b91a210dad3b4fe6766466f88090d8730040d9e9
sayterdarkwynd <sayterdarkwynd@gmail.com>
2019-04-27 16:42:27 -0400

    stuff and things

---
+++ stats/effects/fu_effects/madness/madnessloss.lua
@@ -0,0 +1,17 @@
+function init()
+  script.setUpdateDelta(3)
+  
+  effect.addStatModifierGroup({
+    {stat = "mentalProtection", amount = 1}
+  })  
+  
+end
+
+function update(dt)
+
+ 
+end
+
+function uninit()
+  
+end
