867db63bc55ae92c96a0943fa4fee88dd94dd312
sayterdarkwynd <sayterdarkwynd@gmail.com>
2019-04-28 10:26:18 -0400

    madness branches

--- stats/effects/fu_effects/madness/madnessgain.lua
+++ stats/effects/fu_effects/madness/madnessgain.lua
@@ -1,9 +1,10 @@
 function init()
   script.setUpdateDelta(3)
+  self.valBonus = config.getParameter("valBonus")
 end
 
 function update(dt)
-  self.randVal = math.random(1,12)
+  self.randVal = math.random(1,12) * self.valBonus
   world.spawnItem("fumadnessresource",entity.position(),self.randVal)
   effect.expire()
 end
