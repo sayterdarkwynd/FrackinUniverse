17b566c06882db80a4d7d72ce2e51cbc602a53f9
sayterdarkwynd <sayterdarkwynd@gmail.com>
2019-05-11 10:29:26 -0400

    sarcophagi

---
+++ objects/noisy/quiet.lua
@@ -0,0 +1,25 @@
+function init()
+  self.count = 0
+  self.sounds = config.getParameter("sounds", {})
+  animator.setSoundPool("noise", self.sounds)
+  object.setInteractive(true)
+end
+
+function onInteraction()
+  if self.count < 1 then
+    self.random = math.random(1,2)
+    if self.random == 1 then
+      world.spawnItem("wrappedbodybirb", object.position(), 1)  
+      self.count = 1
+    else
+      self.count = 1
+    end
+  end
+end
+
+function onNpcPlay(npcId)
+  local interact = config.getParameter("npcToy.interactOnNpcPlayStart")
+  if interact == nil or interact ~= false then
+    onInteraction()
+  end
+end
