abf824bf18839ff47f9d2c258c82e69fe36179f1
Kherae <cwaldridge@live.com>
2019-06-01 23:29:41 -0700

    ting

--- stats/effects/fu_effects/sourcekill/sourcekill.lua
+++ stats/effects/fu_effects/sourcekill/sourcekill.lua
@@ -1,11 +1,9 @@
 function init()
 	source={}
 	source.id=effect.sourceEntity()
-	source.hp=world.callScriptedEntity(source.id,"status.resourceMax","health")
 	world.callScriptedEntity(source.id,"status.applySelfDamageRequest",{
 			damageType = "IgnoresDef",
-			damage = source.health,
-			sourceEntityId = source.id
+			damage = math.huge
 		}
 	)
 end
--- stats/effects/fu_effects/volatilemonsters/volatilemonsterbomb.lua
+++ stats/effects/fu_effects/volatilemonsters/volatilemonsterbomb.lua
@@ -1,31 +1,52 @@
+require "/scripts/poly.lua"
+require "/scripts/rect.lua"
+
 function init()
-	if entity.type()=="monster" then
-		script.setUpdateRate(0.1)
+	if status.isResource("timeToLive") then
+		script.setUpdateDelta(1)
 	else
-		script.setUpdateRate(0.0)
+		script.setUpdateDelta(0.0)
 	end
+	self.health=status.resourceMax("health") or self.health
 end
 
 function update(dt)
+	--[[
+	if mcontroller then
+		local poly=mcontroller.collisionPoly()
+		local bound=poly.boundBox(poly)
+		local center=poly.center(poly)
+		local size=rect.size(bound)
+		size=vec2.mag(size)
+		
+		sb.logInfo("%s",boundBox)
+		if world.entityQuery(center,size,{withoutEntityId=entity.id(),includedTypes={"creature"}}) then
+			sb.logInfo("colliding!")
+		end
+	end]]
+
 	effect.modifyDuration(dt)
-	if not status.resourcePositive("timer") then
-		--die()
-	end
-	if not status.resourcePositive("health") then
-		boom()
+	if not dying then
+		if not status.resourcePositive("timeToLive") then
+			die()
+			boom()
+		elseif not status.resourcePositive("health") then
+			dying=true
+			boom()
+		end
 	end
 end
 
 function die()
+	dying=true
 	status.applySelfDamageRequest({
 			damageType = "IgnoresDef",
-			damage = status.resource("health"),
-			sourceEntityId = entity.id()
+			damage = math.huge
 		}
 	)
 end
 
 function boom()
 	sb.logInfo("inksplat!")
-
+	
 end
