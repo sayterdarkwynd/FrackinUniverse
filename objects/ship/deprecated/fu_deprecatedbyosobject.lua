e7b8efd298f3b10b2b9e17dcd331304adbcd57b0
Kherae <cwaldridge@live.com>
2019-08-20 06:56:50 -0700

    deprecation

---
+++ objects/ship/deprecated/fu_deprecatedbyosobject.lua
@@ -0,0 +1,22 @@
+local oldInit=init
+
+function init()
+	if oldInit then oldInit() end
+	object.setInteractive(true)
+end
+
+function onInteraction(args)
+	object.setConfigParameter("breakDropPool", "empty")
+	object.setInteractive(false)
+	
+	local dummy=root.recipesForItem(object.name())
+	for _,recipe in pairs(dummy) do
+		
+		for _,item in pairs(recipe.input) do
+			world.spawnItem(item,object.position())
+		end
+		break
+	end
+	
+	object.smash(true)
+end
