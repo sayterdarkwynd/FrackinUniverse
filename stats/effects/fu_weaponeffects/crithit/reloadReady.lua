b131f9edd81e01c68aa1e4d07ee0caf9f4334e18
sayterdarkwynd <sayterdarkwynd@gmail.com>
2019-10-15 23:44:48 -0400

    some reload data

--- stats/effects/fu_weaponeffects/crithit/reloadReady.lua
+++ stats/effects/fu_weaponeffects/crithit/reloadReady.lua
@@ -1,28 +1,22 @@
 function init()
   activateVisualEffects() 
-  self.timer = 1
+  self.timer = 0.5
   script.setUpdateDelta(5)
 end
 
 
 function activateVisualEffects()
- if status.stat("isCharged") == 1 then
   local statusTextRegion = { 0, 1, 0, 1 }
   animator.setParticleEmitterOffsetRegion("reloadText", statusTextRegion)
   animator.burstParticleEmitter("reloadText")
   effect.setParentDirectives("fade=0055aa=0.2")
- end
-
+  animator.burstParticleEmitter("smoke")
 end
 
 
 
 function update(dt)
---  if self.timer > 0 then
---    self.timer = self.timer - dt
---  else
---    effect.die()
---  end
+
 end
 
 
