b48a6e5542306c84fbdfd3777e123f8afb642c71
sayterdarkwynd <sayterdarkwynd@gmail.com>
2019-10-23 18:10:33 -0400

    fix for nocturn array

--- objects/power/isn_nocturnpanel.lua
+++ objects/power/isn_nocturnpanel.lua
@@ -17,18 +17,18 @@ function update(dt)
       local light = getLight(location) or 0
       local genmult = 0
 
-      if light >= 0.50 then
-        genmult = 1
-      elseif light >= 0.35 then
-        genmult = 2
-      elseif light >= 0.15 then
-        genmult = 3       
-      elseif light <= 0.01 then
+      if light <= 0.01 then
         genmult = 4
+      elseif light <= 0.15 then
+        genmult = 3
+      elseif light <= 0.35 then
+        genmult = 2       
+      elseif light <= 0.50 then
+        genmult = 1
       else 
         genmult = 0
       end
-
+      
       if world.liquidAt(location)then genmult = genmult * 0.05 end -- water significantly reduces the output
 
       local generated = math.min(self.powerLevel * genmult,4) -- max at 4 just in case.
