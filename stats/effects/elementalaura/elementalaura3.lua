6d108d5bbc1d722b126b7ee57964d95c928dca7c
Myou <alexa.granger@gmx.de>
2019-10-27 16:07:32 +0100

    Delete elementalaura3.lua

--- stats/effects/elementalaura/elementalaura3.lua
+++
@@ -1,5 +0,0 @@
-function init()
-  animator.setAnimationState("aura", "windup")
-  self.resistance = config.getParameter("resistType")
-  effect.addStatModifierGroup({{stat = self.resistance, baseMultiplier = 0.6 }})
-end
