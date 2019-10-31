1b30fd5242e92f295c45c54a98a6a1707398070b
sayterdarkwynd <sayterdarkwynd@gmail.com>
2019-10-31 08:32:41 -0400

    arctic set change

--- stats/effects/fu_armoreffects/set_bonuses/arcticsetbonuseffect.lua
+++ stats/effects/fu_armoreffects/set_bonuses/arcticsetbonuseffect.lua
@@ -9,7 +9,7 @@ armorEffect={
 	{stat = "slushslowImmunity", amount = 1},
 	{stat = "ffextremecoldImmunity", amount = 1},
 	{stat = "biomecoldImmunity", amount = 1},
-	{stat = "lethalcoldImmunity", amount = 1}
+	{stat = "lethalColdImmunity", amount = 1}
 }
 
 setName="fu_arcticset"
