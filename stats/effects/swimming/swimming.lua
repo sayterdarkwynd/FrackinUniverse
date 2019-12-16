a34c2a84d73b42fe87c8790a3abdb3264ac8999e
sayterdarkwynd <sayterdarkwynd@gmail.com>
2019-12-16 11:19:28 -0500

    fixes

--- stats/effects/swimming/swimming.lua
+++ stats/effects/swimming/swimming.lua
@@ -4,7 +4,6 @@ function init()
   self.mouthPosition = status.statusProperty("mouthPosition") or {0,0}  --mouth position
   self.mouthBounds = {self.mouthPosition[1], self.mouthPosition[2], self.mouthPosition[1], self.mouthPosition[2]}
   self.setWet = false -- doesnt start wet
-  
   animator.setParticleEmitterOffsetRegion("bubbles", self.mouthBounds)
 end
 
@@ -57,9 +56,6 @@ function update(dt)
 		liquidForce = 100 * self.finalValue  -- get more swim force the better your boost is?
 	    }
 	)
-
-
-
     else
 	effect.expire() -- we could do effect.expire here, but its probably pointless
     end    
@@ -73,38 +69,56 @@ function onExpire()
 end
 
 function setMonsterAbilities()
-	if (entity.id("atropusfish")) or (entity.id("veilendrex")) or (entity.id("dolphin1")) then
-	    mcontroller.controlModifiers( 
-	      {speedModifier = 5.735}    -- slower than players in water
-	    )   
-	    mcontroller.controlParameters({  --sets monster movement to be floaty but otherwise identical to player on land
-		gravityMultiplier = 1,
+	if status.stat("isWaterCreature") then
+	  if (mcontroller.liquidPercentage() >= 0.80) then
+		if (entity.id("atropusfish")) or (entity.id("veilendrex")) or (entity.id("dolphin1")) then
+		    mcontroller.controlModifiers( 
+		      {speedModifier = 4.735}    
+		    )   
+		    mcontroller.controlParameters({
+			gravityMultiplier = 1,
+			liquidImpedance = 0.5,
+			liquidForce = 80.0
+		    })	
+		elseif entity.id("atropuseye") then
+		    mcontroller.controlModifiers( 
+		      {speedModifier = 2.735}   
+		    )   
+		    mcontroller.controlParameters({  
+			gravityMultiplier = 1,
+			liquidImpedance = 0.5,
+			liquidForce = 30.0
+		    })		    
+		elseif entity.id("deathjelly") then
+		    mcontroller.controlModifiers( 
+		      {speedModifier = 1.735}   
+		    )   
+		    mcontroller.controlParameters({  
+			gravityMultiplier = 0,
+			liquidImpedance = 0.2,
+			liquidForce = 80.0
+		    })	
+		end
+	  else
+
+	    if not mcontroller.baseParameters().gravityEnabled then
+	     mcontroller.setYVelocity(-5)
+	    end	  
+	    mcontroller.controlModifiers({speedModifier = 0})   
+	    mcontroller.controlParameters({ 
+		gravityMultiplier = 1.5,
 		liquidImpedance = 0.5,
-		liquidForce = 80.0
-	    })	
-	elseif entity.id("atropuseye") then
-	    mcontroller.controlModifiers( 
-	      {speedModifier = 2.735}    -- slower than players in water
-	    )   
-	    mcontroller.controlParameters({  --sets monster movement to be floaty but otherwise identical to player on land
-		gravityMultiplier = 0,
-		liquidImpedance = 0.5,
-		liquidForce = 30.0
-	    })		    
-	elseif entity.id("deathjelly") then
-	    mcontroller.controlModifiers( 
-	      {speedModifier = 2.735}    -- slower than players in water
-	    )   
-	    mcontroller.controlParameters({  --sets monster movement to be floaty but otherwise identical to player on land
-		gravityMultiplier = 0,
-		liquidImpedance = 0.5,
-		liquidForce = 80.0
-	    })	
+		liquidForce = 80.0,
+		airFriction = 0,
+		airForce = 0
+	    })  
+	  end
+
 	else
 	    mcontroller.controlModifiers( 
-	      {speedModifier = 2.735}    -- slower than players in water
+	      {speedModifier = 2.735}    
 	    )   
-	    mcontroller.controlParameters({  --sets monster movement to be floaty but otherwise identical to player on land
+	    mcontroller.controlParameters({  
 		gravityMultiplier = 0.6,
 		liquidImpedance = 0.5,
 		liquidForce = 80.0
