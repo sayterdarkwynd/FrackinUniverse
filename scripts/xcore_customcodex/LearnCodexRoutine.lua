e3103901338cfcfcc8385a3c3ecb3da4478ed3b5
Xan <subzeroxan@gmail.com>
2020-02-18 20:46:17 -0600

    Remove some garbage, tweak code to use warn instead of error

--- scripts/xcore_customcodex/LearnCodexRoutine.lua
+++ scripts/xcore_customcodex/LearnCodexRoutine.lua
@@ -4,16 +4,9 @@ require("/scripts/xcore_customcodex/LoggingOverride.lua") -- Very nice script!
 local HasLoggingOverride = false
 local print, warn, error, assertwarn, assert, tostring;
 
-local REMINDER_DONT_REPORT_INSIGNIFICANT_ERRORS = "\n\t >>>> THIS ERROR MESSAGE IS FOR LOGGING PURPOSES ONLY. THIS IS ***NOT*** A FATAL ERROR.\n\t >>>> If you wish to report it anyway, go to Xan the Dragon#1760 as this is his script.\n"
-
 local function GetLoggingOverridesIfNecessary()
 	if HasLoggingOverride then return end
-	print, warn, new_error, assertwarn, assert, tostring = CreateLoggingOverride("[Codex Learning Routine]")
-	
-	-- Hacky, but it ensures every error message has this without the need to paste it after every message.
-	error = function (...)
-		new_error(..., REMINDER_DONT_REPORT_INSIGNIFICANT_ERRORS)
-	end
+	print, warn, error, assertwarn, assert, tostring = CreateLoggingOverride("[XCustomCodex Systems] ")
 	
 	HasLoggingOverride = true
 end
@@ -35,15 +28,15 @@ function LearnCodex(itemName)
 	
 	-- First sanity check: Item name OK?
 	if type(itemName) ~= "string" then
-		error("Something called LearnCodex with an item name that wasn't a string. Aborting the learning procedure. (itemName is nil? " .. tostring(itemName == nil):upper() .. ").")
+		warn("Something called LearnCodex with an item name that wasn't a string. Aborting the learning procedure. (itemName is nil? " .. tostring(itemName == nil):upper() .. ").")
 		return 2
 	end
 	if not itemName == nil or #itemName <= 6 then
-		error("Something called LearnCodex with an item name that was either nil or had a length less than or equal to 6 (and by extension, it would be impossible for its name to end in -codex and still have content beforehand as a result). Aborting the learning procedure. The item name is: " .. tostring(itemName))
+		warn("Something called LearnCodex with an item name that was either nil or had a length less than or equal to 6 (and by extension, it would be impossible for its name to end in -codex and still have content beforehand as a result). Aborting the learning procedure. The item name is: " .. tostring(itemName))
 		return 2
 	end
 	if itemName:sub(-6) ~= "-codex" then
-		error("Player attempted to learn codex, but held item name did not end in -codex! Aborting the learning procedure. The item name is: " .. tostring(itemName))
+		warn("Player attempted to learn codex, but held item name did not end in -codex! Aborting the learning procedure. The item name is: " .. tostring(itemName))
 		return 2
 	end
 	------------------------------------
@@ -51,7 +44,7 @@ function LearnCodex(itemName)
 	-- Second sanity check: Item exists?
 	local data = root.itemConfig(itemName) -- Kind of counterintuitive but oh well.
 	if data == nil or data.directory == nil then
-		error("Player attempted to learn a codex from the item [" .. tostring(itemName) .. "], but root.itemConfig() returned nil data on this item! Aborting the learning procedure.")
+		warn("Player attempted to learn a codex from the item [" .. tostring(itemName) .. "], but root.itemConfig() returned nil data on this item! Aborting the learning procedure.")
 		return 2
 	end
 	
@@ -60,7 +53,7 @@ function LearnCodex(itemName)
 	
 	local foundCodexFile = pcall(root.assetJson, data.directory .. itemName .. ".codex")
 	if not foundCodexFile then
-		error("An item's ID ended in -codex, but it was not located as a codex in the game data files -- this item is violating naming conventions! Aborting the learning procedure. (Attempted to locate the file [" .. data.directory .. itemName .. ".codex], which doesn't exist.")
+		warn("An item's ID ended in -codex, but it was not located as a codex in the game data files -- this item is violating naming conventions! Aborting the learning procedure. (Attempted to locate the file [" .. data.directory .. itemName .. ".codex], which doesn't exist.")
 		return 2
 	end
 	------------------------------------
@@ -75,10 +68,10 @@ function LearnCodex(itemName)
 	if not TableAlreadyContains(existingKnownEntries, codexCache) then
 		table.insert(existingKnownEntries, codexCache)
 		player.setProperty("xcodex.knownCodexEntries", existingKnownEntries)
-		if sb then sb.logInfo("Player has learned codex " .. table.concat(codexCache, ", ") .. ".") end
+		print("Player has learned codex " .. table.concat(codexCache, ", ") .. ".")
 		return 0
 	else
-		if sb then sb.logInfo("Player attempted to learn codex " .. table.concat(codexCache, ", ") .. " but they already know it, so we don't need to learn it again.") end
+		print("Player attempted to learn codex " .. table.concat(codexCache, ", ") .. " but they already know it, so we don't need to learn it again.")
 		return 1
 	end
 end
