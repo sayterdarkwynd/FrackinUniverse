04042ef1ec4ea0314836a03542b718808ce180f6
Kherae <cwaldridge@live.com>
2020-04-05 02:53:54 -0700

    warped capture

---
+++ stats/effects/fu_effects/capturebot/warpedcapture.lua
@@ -0,0 +1,33 @@
+require "/scripts/util.lua"
+require "/scripts/companions/util.lua"
+require "/scripts/messageutil.lua"
+
+function init()
+  effect.setParentDirectives(config.getParameter("directives", ""))
+end
+
+
+function update(dt)
+  hit()
+end
+
+function hit()
+	if self.hit then return end
+	if world.isMonster(entity.id()) then
+		-- If a monster doesn't implement pet.attemptCapture or its response is nil
+		-- then it isn't caught.
+		podData=world.callScriptedEntity(entity.id(), "capturable.attemptCapture", entity.id())
+		
+		if podData then
+			spawnFilledPod(podData)
+			self.hit=true
+			world.callScriptedEntity(entity.id(),"monster.setDropPool",nil)
+			status.setResourcePercentage("health",0)
+		end
+	end
+end
+
+function spawnFilledPod(pet)
+  local pod = createFilledPod(pet)
+  world.spawnItem(pod.name, mcontroller.position(), pod.count, pod.parameters)
+end
