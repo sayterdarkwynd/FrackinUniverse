1801b8ca0f22158a6f7f4902a33d3a011c20833a
Kherae <cwaldridge@live.com>
2020-04-29 02:28:31 -0700

    Update biomeairless.lua

--- stats/effects/biomeairless/biomeairless.lua
+++ stats/effects/biomeairless/biomeairless.lua
@@ -1,8 +1,10 @@
 function update(dt)
 	if not world.breathable(world.entityMouthPosition(entity.id())) then
-    if (self.warningUsed ~= true) then
-      world.sendEntityMessage(entity.id(), "queueRadioMessage", "biomeairless", 1.0)
-      self.warningUsed = true
-    end
+		if not status.isResource("biomeairlesswarning") or not status.resourcePositive("biomeairlesswarning") then
+			world.sendEntityMessage(entity.id(), "queueRadioMessage", "biomeairless", 1.0)
+		end
+		if status.isResource("biomeairlesswarning") then
+			status.setResourcePercentage("biomeairlesswarning",1) 
+		end
 	end
 end
