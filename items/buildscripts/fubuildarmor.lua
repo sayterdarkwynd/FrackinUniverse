c2d7a4a82bf2599f2e0348251406121d5300972b
Kherae <cwaldridge@live.com>
2020-05-02 23:16:11 -0700

    yay

--- items/buildscripts/fubuildarmor.lua
+++ items/buildscripts/fubuildarmor.lua
@@ -2,7 +2,16 @@ require "/scripts/util.lua"
 require "/scripts/vec2.lua"
 require "/scripts/versioningutils.lua"
 
-local resistances={physicalResistance="physicalResLabel",fireResistance="fireResLabel",iceResistance="iceResLabel",poisonResistance="poisonResLabel",electricResistance="electricResLabel",cosmicResistance="cosmicResLabel",shadowResistance="shadowResLabel",radioactiveResistance="radiationResLabel"}
+local resistances={
+	physicalResistance={label="physicalResLabel",friendly="Phys. Resist: "},
+	fireResistance={label="fireResLabel",friendly="Fire Resist: "},
+	iceResistance={label="iceResLabel",friendly="Ice Resist: "},
+	poisonResistance={label="poisonResLabel",friendly="Poison Resist: "},
+	electricResistance={label="electricResLabel",friendly="Elec. Resist: "},
+	cosmicResistance={label="cosmicResLabel",friendly="Cosmic Resist: "},
+	shadowResistance={label="shadowResLabel",friendly="Shadow Resist: "},
+	radioactiveResistance={label="radiationResLabel",friendly="Rad. Resist: "}
+}
 
 function build(directory, config, parameters, level, seed)
 	local configParameter = function(keyName, defaultValue)
@@ -19,9 +28,9 @@ function build(directory, config, parameters, level, seed)
 		parameters.level = level
 	end
 
-		config.description = configParameter("description",0)
-		config.shortdescription = configParameter("shortdescription",0)
-		config.inventoryIcon = configParameter("inventoryIcon",0)
+	config.description = configParameter("description",0)
+	config.shortdescription = configParameter("shortdescription",0)
+	config.inventoryIcon = configParameter("inventoryIcon",0)
 
 
 	if config.level < 3 then
@@ -36,18 +45,23 @@ function build(directory, config, parameters, level, seed)
 		config.rarity = "essential"
 	end
 
-		config.price = (config.price or 0) * root.evalFunction("itemLevelPriceMultiplier", configParameter("level", 1))
-		config.tooltipFields = {}
+	config.price = (config.price or 0) * root.evalFunction("itemLevelPriceMultiplier", configParameter("level", 1))
+	config.tooltipFields = {}
+	
+	config.tooltipFields.monkeyLabel=config.description
 
-		config.tooltipFields.levelLabel = util.round(configParameter("level", 1), 1)
+	config.tooltipFields.levelLabel = util.round(configParameter("level", 1), 1)
 
 	if configParameter("upgrades") == 1 then
 		config.tooltipFields.upgradeLabel = "^cyan;Upgradeable^reset;"
 	end
 
-	for _,label in pairs(resistances) do
-		config.tooltipFields[label]="0%"
+	for _,data in pairs(resistances) do
+		config.tooltipFields[data.label]="0%"
 	end
+
+	local resistanceInfo={}
+	local resistanceText=""
 
 	if config.leveledStatusEffects then 
 		config.tooltipFields.priceLabel = config.price
@@ -65,21 +79,31 @@ function build(directory, config, parameters, level, seed)
 		
 		for _,v in pairs(config.leveledStatusEffects) do
 			if resistances[v.stat] then
-				config.tooltipFields[resistances[v.stat]]=(util.round((v.amount or 0)*root.evalFunction(v.levelFunction,configParameter("level", 1)),3)*100).."%"
-				config.tooltipFields[resistances[v.stat]]=string.gsub(config.tooltipFields[resistances[v.stat]],"%.0%%","%%")
+				local label=resistances[v.stat].label
+				local friendly=resistances[v.stat].friendly
+				config.tooltipFields[label]=(util.round((v.amount or 0)*root.evalFunction(v.levelFunction,configParameter("level", 1)),3)*100).."%"
+				config.tooltipFields[label]=string.gsub(config.tooltipFields[label],"%.0%%","%%")
+				table.insert(resistanceInfo,friendly..config.tooltipFields[label])
 			end
 		end
 	end
 	
-	--debug thing
-	--[[for _,label in pairs(resistances) do
-		config.tooltipFields[label]=(100.0).."%"
-	end]]
-
-	if config.statusEffects then
-	-- this area should display Immunities, Resistances
+	local doOnce=false
+	for i = #resistanceInfo, 1, -1 do
+		if not doOnce then
+			resistanceText="\n^green;^reset; " 
+			doOnce=true
+		end
+		resistanceText=resistanceText..resistanceInfo[i]
+		if i > 1 then
+			resistanceText=resistanceText..", "
+		end
 	end
-
+	if string.len(resistanceText) then
+		config.description=config.description..resistanceText
+		--sb.logInfo("data\n%s",config.description)
+	end
+	
 	return config, parameters
 end
 
