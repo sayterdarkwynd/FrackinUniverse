f94d728d6cebf76d6fec7ccc614b8057e775dc4b
Kherae <cwaldridge@live.com>
2020-05-21 19:58:22 -0700

    extra resists

--- monsters/fu_bossExtraResistsHandler.lua
+++ monsters/fu_bossExtraResistsHandler.lua
@@ -2,19 +2,37 @@ require "/scripts/util.lua"
 
 local bossExtraResistsOldInit=init
 local bossExtraResistsValue=100
+local bossExtraResistsOverride=false
 
 function init()
 	if bossExtraResistsOldInit then bossExtraResistsOldInit() end
 	
-	local baseParameters=root.monsterParameters(monster.type())
-	local overrideParameters=monster.uniqueParameters()
-	overrideParameters=util.mergeTable(baseParameters,overrideParameters)
-	if overrideParameters.bossExtraResistsValue then
-		bossExtraResistsValue=overrideParameters.bossExtraResistsValue
-	end
 	local elementaltypes=root.assetJson("/damage/elementaltypes.config")
 	local buffer={}
 	local resists={}
+
+	local baseParameters=root.monsterParameters(monster.type())
+	local overrideParameters=monster.uniqueParameters()
+	
+	overrideParameters=util.mergeTable(baseParameters,overrideParameters)
+	baseParameters=overrideParameters.statusSettings.stats
+	
+	if overrideParameters.bossExtraResistsValue then
+		bossExtraResistsValue=overrideParameters.bossExtraResistsValue
+	end
+	if overrideParameters.bossExtraResistsOverride then
+		bossExtraResistsOverride=overrideParameters.bossExtraResistsOverride
+	end
+	
+	if not bossExtraResistsOverride then
+		for stat,value in pairs(baseParameters) do
+			if value.amount then
+				buffer[stat]=value.amount
+			end
+		end
+		overrideParameters=buffer
+	end
+	buffer={}
 	
 	for element,data in pairs(elementaltypes) do
 		if data.resistanceStat then
@@ -23,7 +41,7 @@ function init()
 	end
 	
 	for resist,_ in pairs(buffer) do
-		if status.stat(resist) == 0.0 then
+		if bossExtraResistsOverride or not overrideParameters[resist] then
 			table.insert(resists,{stat = resist, amount = bossExtraResistsValue })
 		end
 	end
