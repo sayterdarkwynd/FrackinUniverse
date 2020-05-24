fb1659b9f2bf3b5eb8f3bf15af55e3c0f4d6b29c
Kherae <cwaldridge@live.com>
2020-05-24 00:19:49 -0700

    implementation switch

--- stats/effects/fu_effects/infection/infection.lua
+++ stats/effects/fu_effects/infection/infection.lua
@@ -1,3 +1,5 @@
+require "/scripts/effectUtil.lua"
+
 function init()
   animator.setParticleEmitterOffsetRegion("drips", mcontroller.boundBox())
   animator.setParticleEmitterActive("drips", true)
@@ -6,13 +8,15 @@ function init()
 end
 
 function update(dt)
-
+	effectUtil.effectAllInRange("weakpoison",4)
+	effectUtil.effectAllInRange("darkvision",4)
+--[[
 local distanceCheck = world.entityQuery(mcontroller.position(),4)
 
   for key, value in pairs(distanceCheck) do
    world.sendEntityMessage(value,"applyStatusEffect","weakpoison")
    world.sendEntityMessage(value,"applyStatusEffect","darkvision")
-  end
+  end]]
 end
 
 function uninit()
