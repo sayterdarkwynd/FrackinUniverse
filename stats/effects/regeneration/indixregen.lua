6eefc6e16770986f847d59f61b41b25147616702
Kherae <cwaldridge@live.com>
2020-06-10 20:53:23 -0700

    indix regen

--- stats/effects/regeneration/indixregen.lua
+++ stats/effects/regeneration/indixregen.lua
@@ -1,24 +1,19 @@
-require "/scripts/vec2.lua"
-require "/scripts/util.lua"
-require "/scripts/interp.lua"
-
 function init()
-	self.healingRate = 1.01 / config.getParameter("healTime", 180)
-	script.setUpdateDelta(5)
+	--self.healingRate = 1.01 / config.getParameter("healTime", 180)
+	self.healingRate = 1.0 / config.getParameter("healTime", 180)
+	script.setUpdateDelta(10)
 	bonusHandler=effect.addStatModifierGroup({})
 end
 
 
 function update(dt)
 	--sb.logInfo("indixregen")
-    if status.resource("energy") >= status.stat("maxEnergy")/2 then
-		self.healingRate = 1.0009 / config.getParameter("healTime", 180)
+    if status.resourcePercentage("energy") >= 0.5 then
 		effect.setStatModifierGroup(bonusHandler,{{stat="healthRegen",amount=status.stat("maxHealth")*self.healingRate}})
 		--status.modifyResourcePercentage("health", self.healingRate * dt)  
 	else
 		effect.setStatModifierGroup(bonusHandler,{})
     end
-
 end
 
 function uninit()
