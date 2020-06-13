a97e38f53a496501be159341ae58c1865a22056e
Kherae <cwaldridge@live.com>
2020-06-13 01:57:16 -0700

    it is local

--- stats/effects/fu_tileeffects/specialty_effects/insanityblur/insanityblurstat.lua
+++ stats/effects/fu_tileeffects/specialty_effects/insanityblur/insanityblurstat.lua
@@ -1,27 +1,37 @@
 function init()
-  script.setUpdateDelta(30)
+	--script.setUpdateDelta(30)
+	setParticleConfig()
+	script.setUpdateDelta(1)
 end
 
 
-function activateVisualEffects()
-if world.entityType(entity.id()) ~= "player" then
-  effect.expire()
-end
-	    animator.setParticleEmitterOffsetRegion("smoke", mcontroller.boundBox())
-	    animator.setParticleEmitterActive("smoke", true)
-end
+--[[function activateVisualEffects()
+	if world.entityType(entity.id()) ~= "player" then
+		effect.expire()
+	end
+	--animator.setParticleEmitterOffsetRegion("smoke", mcontroller.boundBox())
+	--animator.setParticleEmitterActive("smoke", true)
+end]]
 
 
+function setParticleConfig()
+	particleConfig={type = "textured",image = "/animations/insanity/insanityfx.png",velocity = {0, 0},approach = {0, 0},destructionAction = "fade",size = 1,layer = "front",variance = {initialVelocity = {0, 0}}}
+	dt=script.updateDt()
+	particleConfig.timeToLive = dt*10.0
+	particleConfig.destructionTime = dt*5.0
+end
 
 
 function update(dt)
-if world.entityType(entity.id()) ~= "player" then
-  effect.expire()
-end
-    animator.setParticleEmitterActive("smoke", true)
+	if world.entityType(entity.id()) ~= "player" then
+		effect.expire()
+	end
+	particleConfig.position=entity.position()
+	world.sendEntityMessage(entity.id(),"fu_specialAnimator.spawnParticle",particleConfig)
+	--animator.setParticleEmitterActive("smoke", true)
 end
 
 
 function uninit()
-  animator.setParticleEmitterActive("smoke", false)
+	--animator.setParticleEmitterActive("smoke", false)
 end
