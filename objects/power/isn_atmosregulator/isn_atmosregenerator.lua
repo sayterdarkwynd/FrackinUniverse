c1faf899bd8d715fc3418753d135deaa9193cbf6
Kherae <cwaldridge@live.com>
2020-06-20 20:50:23 -0700

    atmos move

--- objects/power/isn_atmosregulator/isn_atmosregenerator.lua
+++ objects/power/isn_atmosregulator/isn_atmosregenerator.lua
@@ -4,7 +4,7 @@ require "/scripts/effectUtil.lua"
 function update(dt)
   if power.consume(config.getParameter('isn_requiredPower')) then
     animator.setAnimationState("switchState", "on")
-    effectUtil.effectAllInRange("isn_atmosregen",500)
+    effectUtil.effectAllInRange("regenerationAtmosRegen",500)
   else
 	animator.setAnimationState("switchState", "off")
   end
