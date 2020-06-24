d8033206f89d9b02475ee65706e058fed0d95985
Bruce Reif (Buswolley) <btreif@gmail.com>
2020-06-24 11:41:15 -0700

    rework Advanced Liquid Sensor

--- objects/wired/liquidsensor/swtjc_wp_advancedliquidsensor.lua
+++ objects/wired/liquidsensor/swtjc_wp_advancedliquidsensor.lua
@@ -1,40 +1,62 @@
+require "/scripts/kheAA/transferUtil.lua"
+require "/scripts/kheAA/liquidLib.lua"
+local deltaTime = 0
+
 function init()
+  transferUtil.init()
+  object.setInteractive(true)
+
   self.defaultOnAnimation = config.getParameter("defaultOnAnimation","other")
   self.defaultOffAnimation = config.getParameter("defaultOffAnimation","off")
-  self.liquidCount = config.getParameter("liquidCount",0)
-  if self.liquidCount > 0 then
-    self.liquidArrayNodes = config.getParameter("liquidArrayNodes")
-    self.liquidArrayIDs = config.getParameter("liquidArrayIDs")
-    self.liquidArrayAnimations = config.getParameter("liquidArrayAnimations")
-  end
-  
-
 end
 
-function getSample()
+function sampleLiquid()
   return world.liquidAt(object.position())
 end
 
-function update(dt)
-  local sample = getSample()
+-- Compares the stored object's 'liquid' field against the sampled liquid's name.
+function isOn()
+  local predicateId = predicateLiquidId()
+  local sampleId = sampleLiquidId()
+  local result = sampleId and predicateId == sampleId
+  return result
+end
 
-  object.setAllOutputNodes(false)
-  local i = 0
-  if not sample then
-    animator.setAnimationState("sensorState", self.defaultOffAnimation)
-  else
-    while i < self.liquidCount+2 do
-      if i == self.liquidCount+1 then
-        object.setOutputNodeLevel(0, true)
-        animator.setAnimationState("sensorState", self.defaultOnAnimation)
-        i = i + 1
-      elseif sample[1] == self.liquidArrayIDs[i] then
-        object.setOutputNodeLevel(self.liquidArrayNodes[i], true)
-        animator.setAnimationState("sensorState", self.liquidArrayAnimations[i])
-        i = self.liquidCount + 2
-      else
-        i = i + 1
-      end
-    end
+function sampleLiquidId()
+  local sample = sampleLiquid()
+  if (sample == nil) then
+    sb.logInfo("sample nil")
+    return nil 
   end
+  return sample[1]
+end
+
+function predicateLiquidId()
+  local id = entity.id()
+  local predicate = world.containerItemAt(id, 0)
+  if (predicate == nil) then 
+    sb.logInfo("predicate nil")
+    return nil 
+  end
+  return liquidLib.itemToLiquidId(predicate)
+end
+
+function update(dt)
+  deltaTime = deltaTime + dt
+  if deltaTime > 1 then
+    deltaTime = 0
+    transferUtil.loadSelfContainer()
+  end
+
+
+  local on = isOn()
+
+  if on then 
+    object.setOutputNodeLevel(0, true)
+    animator.setAnimationState("sensorState", self.defaultOnAnimation)
+  else 
+    object.setOutputNodeLevel(0, false)
+    animator.setAnimationState("sensorState", self.defaultOffAnimation)
+  end
+
 end
