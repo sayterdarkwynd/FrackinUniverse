de17635ca09a6b96b114ad554240854b1895d0ad
sayterdarkwynd <sayterdarkwynd@gmail.com>
2020-07-04 15:11:30 -0400

    shield

--- items/active/shields/shield.lua
+++ items/active/shields/shield.lua
@@ -2,6 +2,12 @@ require "/scripts/util.lua"
 require "/scripts/status.lua"
 require "/scripts/FRHelper.lua"
 
+
+-- to-to : move stats for always-on effects to an array
+-- and do the same for raised-only stats/effect
+-- investigate bash chance math
+-- current system really doesn't do a good job of handling stats that exist only on the item and stats that exist globally on the player
+
 function init()
 	self.debug = false
 	if self.debug then sb.logInfo("(FR) shield.lua init() for %s", activeItem.hand()) end
@@ -84,11 +90,8 @@ function init()
 	stunChance = config.getParameter("stunChance", 0)
 	shieldBash = config.getParameter("shieldBash",0)
 	shieldBashPush = config.getParameter("shieldBashPush",0)
-
- 	        --shieldBonusApply()
+ 	--shieldBonusApply()
 	-- end FU special effects
-        
-
 
 	animator.setGlobalTag("directives", "")
 	animator.setAnimationState("shield", "idle")
@@ -163,19 +166,17 @@ function isShield(name) -- detect if they have a shield equipped for racial tag 
 end
 
 function update(dt, fireMode, shiftHeld)
-shieldBonusApplyPartial()
+
+    shieldBonusApplyPartial()
+
     if not self.species:succeeded() then init() end
     if self.helper then self.helper:runScripts("shield-update", self, dt, fireMode, shiftHeld) end
 
-    --**************************************
-
 	self.cooldownTimer = math.max(0, self.cooldownTimer - dt)
 
-	--**************************************	setting default
 	if self.blockCountShield == nil then
         self.blockCountShield = 0
 	end
-	--**************************************
 
 	if not self.active and fireMode == "primary" and self.cooldownTimer == 0 and status.resourcePositive("shieldStamina") then
         raiseShield()
@@ -185,13 +186,10 @@ shieldBonusApplyPartial()
         self.activeTimer = self.activeTimer + dt
 
         self.damageListener:update()
-
 	
         -- ************************************** FU SPECIALS **************************************
         status.modifyResourcePercentage("health", self.shieldHealthRegen * dt)
         -- *****************************************************************************************
-
-
 
         if status.resourcePositive("perfectBlock") then
             animator.setGlobalTag("directives", self.perfectBlockDirectives)
@@ -261,7 +259,9 @@ function raiseShield()
 	status.setPersistentEffects(activeItem.hand().."Shield", {{stat = "shieldHealth", amount = shieldHealth()}})
 	local shieldPoly = animator.partPoly("shield", "shieldPoly")
 	activeItem.setItemShieldPolys({shieldPoly})
-        shieldBonusApply()
+    
+    shieldBonusApply()
+    
 	if self.knockback > 0 then
         local knockbackDamageSource = {
             poly = shieldPoly,
@@ -304,9 +304,9 @@ function raiseShield()
 						animator.burstParticleEmitter("perfectBlock")
 					end
 
-
                     -- *******************************************************
                     self.blockCountShield = self.blockCountShield + 1
+
                     -- *******************************************************
                     -- Begin racial Perfect Blocking scripts
                     -- *******************************************************
