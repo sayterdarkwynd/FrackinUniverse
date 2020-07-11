f26a1d80fd44b9ad3f56813ca1d1e8b3a2ce867a
sayterdarkwynd <sayterdarkwynd@gmail.com>
2020-07-11 19:28:31 -0400

    derp

--- scripts/fr_weaponscripts/aviancombo.lua
+++ scripts/fr_weaponscripts/aviancombo.lua
@@ -6,7 +6,7 @@ function FRHelper:call(args, main, ...)
         local randValue = math.random(100)
         if (randValue <= 40) and energyValue >= 50 then	-- spawn a projectile if rand is good and they have enough energy
             params = { power = energyValue/24, damageKind = "hellfire", timeToLive = 0.3, speed = 30, projectileCount = 1, piercing = false }
-            params2 = { power = energyValue/20, damageKind = "hoatfrost", timeToLive = 0.4, speed = 45, projectileCount = 1, piercing = false }
+            params2 = { power = energyValue/20, damageKind = "hoarfrost", timeToLive = 0.4, speed = 45, projectileCount = 1, piercing = false }
             params3 = { power = energyValue/16, damageKind = "bioweapon", timeToLive = 0.5, speed = 60, projectileCount = 1, piercing = true }
             local function spawnProjectile(energy, params)
                 if status.resource("energy") then status.modifyResource("energy", energyValue * energy) end
