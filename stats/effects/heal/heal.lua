6dbf06645d5996a2404f12cc36563572adbed7cf
sayterdarkwynd <sayterdarkwynd@gmail.com>
2020-07-12 16:25:42 -0400

    heal updates

--- stats/effects/heal/heal.lua
+++ stats/effects/heal/heal.lua
@@ -5,7 +5,6 @@ function init()
 	animator.setParticleEmitterOffsetRegion("healing", mcontroller.boundBox())
 	animator.setParticleEmitterEmissionRate("healing", config.getParameter("emissionRate", 3))
 	animator.setParticleEmitterActive("healing", true)
-
 	script.setUpdateDelta(5)
 	self.penaltyRate = config.getParameter("penaltyRate",0)
 	self.duration=effect.duration()
@@ -13,17 +12,34 @@ function init()
 	self.healingBonus = status.stat("healingBonus") or 0
 	self.healingRate = self.healingRate + self.healingBonus
 	bonusHandler=effect.addStatModifierGroup({{stat="healthRegen",amount=(self.healingRate - self.penaltyRate)}})
-	self.cooldown=math.max(20-self.duration,0)
+	self.cooldown=math.max(15-self.duration,0)--base time is 15 seconds
 	effect.modifyDuration(self.cooldown)
 	self.timer=0.0
+	self.timerSound = 0
 end
 
 function update(dt)
 	if not self.timer then return end
 	self.timer=self.timer + dt
+	
 	if self.timer >= self.duration then
 		effect.setStatModifierGroup(bonusHandler,{})
-		animator.setParticleEmitterActive("healing", false)
+		animator.setParticleEmitterActive("healing", false)	
+	else
+		animator.setParticleEmitterActive("cantheal2", false)
+	    animator.setParticleEmitterOffsetRegion("cantheal", mcontroller.boundBox())
+	    animator.setParticleEmitterEmissionRate("cantheal", 0.5 )
+	    animator.setParticleEmitterActive("cantheal", true)	      
+	end
+	if self.timer >= 14.8 then
+		animator.setParticleEmitterActive("cantheal", false)
+		animator.setParticleEmitterOffsetRegion("cantheal2", mcontroller.boundBox())
+	    animator.setParticleEmitterEmissionRate("cantheal2", 3 )
+	    animator.setParticleEmitterActive("cantheal2", true)
+	    if self.timerSound == 0 then
+	    	animator.playSound("ready",1)
+	    	self.timerSound = self.timerSound + 1
+	    end
 	end
 end
 
