d12da5a0708c83e3b6dd04af5387f35e240b4e29
Kherae <cwaldridge@live.com>
2020-07-20 15:07:38 -0700

    onion burn

--- tech/other/moogleglide.lua
+++ tech/other/moogleglide.lua
@@ -82,7 +82,7 @@ function update(args)
 	    if self.pressDown or self.pressDown and self.active2== 1 then  --slowfall stance	
 	      if not mcontroller.onGround() and not mcontroller.zeroG() then
 		      status.setPersistentEffects("glide", {
-			{stat = "fallDamageMultiplier", baseMultiplier = 0.35},
+			{stat = "fallDamageMultiplier", effectiveMultiplier =  0.35},
 			{stat = "gliding", amount = 0}
 		      })	      
 	      end
@@ -94,7 +94,7 @@ function update(args)
 	    if self.pressUp or self.pressUp and self.active3== 1 then  -- glide stance
 	      if not mcontroller.onGround() and not mcontroller.zeroG() then
 		      status.setPersistentEffects("glide", {
-			{stat = "fallDamageMultiplier", baseMultiplier = 0.35}
+			{stat = "fallDamageMultiplier", effectiveMultiplier =  0.35}
 			{stat = "gliding", amount = 1}
 		      })  	      
 	      end
