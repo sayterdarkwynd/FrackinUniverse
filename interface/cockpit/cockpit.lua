sayterdarkwynd/FrackinUniverse

commit 089a848cc5a69122cc4092573d65ea20662a0e84

fixes

interface/cockpit/cockpit.lua
@@ -1142,16 +1142,18 @@ function planetScreenState(planet)
 
     if self.focus.system then
       if self.focus.target and self.focus.target[1] == "coordinate" then
-        local planets = {planet}
-        util.appendLists(planets, celestial.children(planet))
-        for _,planet in pairs(planets) do
-          if compare(planet, self.focus.target[2]) then
-            selection = {"coordinate", planet}
-            View:showPlanetInfo(planet)
-            self.focus = {}
-            break
+        if self.hide == false then
+          local planets = {planet}
+          util.appendLists(planets, celestial.children(planet))
+          for _,planet in pairs(planets) do
+            if compare(planet, self.focus.target[2]) then
+              selection = {"coordinate", planet}
+              View:showPlanetInfo(planet)
+              break
+            end
           end
         end
+        self.focus = {}
       else
         return self.state:set(planetSystemTransition, planet)
       end
@@ -1181,6 +1183,7 @@ function planetScreenState(planet)
           end
 
           if not compare(hover, select) then
+            self.hide = false
             selection = hover
             if selection and selection[1] == "coordinate" then
               View:showPlanetInfo(selection[2])
@@ -1221,7 +1224,7 @@ function planetScreenState(planet)
             orbiting = shipLocation[2]
           end
           if orbiting and compare(coordinatePlanet(orbiting), coordinatePlanet(self.travel.target[2])) then
-            flyShip(self.travel.system, self.travel.target)
+            celestial.flyShip(self.travel.system, self.travel.target)
             self.travel = {}
           end
         end
@@ -1236,7 +1239,7 @@ function planetScreenState(planet)
       hover = nil
     end
     View.planet.hover = hover
-    if selection then
+    if selection and self.hide == false then
       View:select(system, selection)
     else
       View:select(nil)

species/kirhosnamegen.config
@@ -6,99 +6,151 @@
 			[ 
 				{ "mode" : "alts" }, 
       	"", 
-      	"Acid ", 
-      	"Alpha ", 
-      	"Angel ",
-      	"Beta ", 
-      	"Black ",
+      	"Aci ", 
+      	"Alph ", 
+      	"Ang ",
+      	"Bet ", 
+      	"Blat ",
       	"Blue ", 
       	"Burn ", 
-      	"Cataclysm ",
-      	"Chaos ",
+      	"Cata ",
+      	"Cha ",
       	"Chi ", 
-      	"Chill ",
-      	"Chrome ", 
-      	"Control ",
-      	"Cool ", 
-      	"Crash ", 
-      	"Cipher ", 
-      	"Deep ",
-      	"Delta ",
-      	"Devil ",
-      	"Doctor ", 
-      	"Dolphin ",
-      	"Dozer ", 
-      	"Easy ",
-      	"Epoch ", 
+      	"Chil ",
+      	"Chrom ", 
+      	"Cont ",
+      	"Col ", 
+      	"Cas ", 
+      	"Ciph ", 
+      	"Dee ",
+      	"Del ",
+      	"Dev ",
+      	"Docc ", 
+      	"Dolphre ",
+      	"Dozear ", 
+      	"Etassy ",
+      	"Epaocah ", 
       	"Epsilon ", 
-      	"Eta ", 
-      	"Falcon ",
+      	"Esta ", 
+      	"Falfo ",
       	"Freak ", 
       	"Gamma ", 
-      	"Gibson ", 
-      	"Gold ", 
-      	"Grey ", 
-      	"Grim ",
-      	"Hex ",
+      	"Giso ", 
+      	"Gol ", 
+      	"Gry ", 
+      	"Grym ",
+      	"Hexa ",
       	"Iota ", 
-      	"Johnny ", 
-      	"Kappa ", 
-      	"Killer ", 
-      	"Lambda ",
-      	"Lizard ", 
-     		"Lord ",
-      	"Mnemonic ", 
-      	"Molly ", 
-      	"Morpheus ", 
+      	"Joh ", 
+      	"Kaa ", 
+      	"Kii ", 
+      	"Lama ",
+      	"Lizat ", 
+     	"Loarad ",
+      	"Mnemon ", 
+      	"Mol ", 
+      	"Morphep ", 
       	"Mouse ", 
       	"Mu ", 
       	"Nadir ", 
-      	"Neo ", 
-      	"Neon ",
-      	"Night ", 
+      	"Nez ", 
+      	"Nen ",
+      	"Nit ", 
       	"Nu ", 
-      	"Omega ", 
-      	"Omicron ", 
+      	"Ome ", 
+      	"Omiron ", 
       	"Omni ", 
       	"Oracle ", 
       	"Override ", 
       	"Paradox ", 
       	"Phantom ", 
-      	"Phi ", 
-      	"Pi ", 
-      	"Plague ", 
-      	"Posion ",
-      	"Proxy ",
+      	"Phzi ", 
+      	"Pzi ", 
+      	"Plaga ", 
+      	"Posi ",
+      	"Proxi ",
       	"Psi ", 
-      	"Rat ",
-      	"Reaper ",
-      	"Red ", 
-      	"Rho ", 
-      	"Rider ",
-      	"Serial ", 
-      	"Shock ",
-      	"Sigma ", 
-      	"Spider ",
-      	"Strike ",
-      	"Switch ",
-      	"Swordfish ", 
-      	"System ",
-      	"Tank ", 
+      	"Ratta ",
+      	"Reapu ",
+      	"Redu ", 
+      	"Rhou ", 
+      	"Rideu ",
+      	"Seri ", 
+      	"Sho ",
+      	"Sig ", 
+      	"Spi ",
+      	"Strit ",
+      	"Swit ",
+      	"Swork ", 
+      	"Yash ",
+      	"Tanak ", 
       	"Tau ", 
       	"The ", 
       	"Theta ", 
-      	"Trinity ", 
+      	"Trini ", 
       	"Upsilon ", 
-      	"Virus ", 
-      	"Wasp ",
-      	"White ",
-      	"Xi ", 
-      	"Zenith ", 
-      	"Zero ", 
-      	"Zeta " 
+      	"Virs ", 
+      	"Wasat ",
+      	"Whit ",
+      	"Xip ", 
+      	"Zeni ", 
+      	"Zerp ", 
+      	"Zeta ",
+        "Aaralyn ", 
+        "Abi ", 
+        "Abina ", 
+        "Addison ", 
+        "Adora ", 
+        "Adoria ", 
+        "Aelan ", 
+        "Afina ", 
+        "Agatha ", 
+        "Aggie ", 
+        "Dorg ", "Dall ", "Ashi ", "Ava ", "Bal-","Blik-","Ceta ","Cail ","Da ","Dava ","Dannb","Exa-","Elorr-","Frek ","Gyu ","Gikka-gik ","Frem ", "Harg ","Halith ","Ikenz ","Il ","Il-","Jik ","Jikka ","Kilrax ","Kyla ","Norannx ","Shelvor-Nox ","Noc ","Lok-","Metissak ","Morg ","Nintik ","Opa ","Ova ","Pont ","Plyet ", "Qu ","Qyik ","Revvak ","Rika ","Stuk ","Salor ","Tim ","Tak ","Tyu ","Torgum ","Uiolk ", "Zax ","Zax-"      	
       ],
       [ 
       	{ "mode" : "alts" }, 
+      	"Seller",
+      	"Merchant-Born",
+      	"Upper-Decker",
+      	"Wastewalker",
+      	"Arkweaver",
+      	"Enginemaker",
+      	"Warpmaster",
+      	"Jettison",
+      	"Psion",
+      	"Pilot",
+      	"Enfilader",
+      	"Ionic",
+      	"Peaceseller",
+      	"Warmaker",
+      	"Seeker",
+      	"Lore-Bearer",
+      	"Cleaner",
+      	"Mechanis",
+      	"Adept",
+      	"Noviceborn",
+      	"Wailer",
+      	"Freethinker",
+      	"Datahoarder",
+      	"Docmaker",
+      	"Plasmic",
+      	"Enginemaster",
+      	"Patroller",
+      	"Writer",
+      	"Ultrium",
+      	"Lorex",
+      	"Vexavith",
+      	"Lombradie",
+      	"Firstborn",
+      	"Secondborn",
+      	"Third",
+      	"Unfett",
+      	"Jillsong",
+      	"Taleswapper",
+      	"Wanderluster",
+      	"Arkfinder",
+      	"Arkleader",
       	"Acid", 
       	"Alpha", 
       	"Angel",
@@ -189,7 +241,7 @@
       	"Xi", 
       	"Zenith", 
       	"Zero", 
-      	"Zeta"  
+      	"Zeta" 
       ]
     ]
   ]

commit f9f705da15bd9044f03f53cccf4d6a65a7492072

Fix Atmospheric Condenser
Atmospheric Condenser relies on "ship.celestial_type" property to track current planet when installed on ship. "flyShip" it's wrapper to "celestial.flyShip" function which set that property. For some reason it was broken in 089a848cc5a69122cc4092573d65ea20662a0e84.

interface/cockpit/cockpit.lua
@@ -1281,7 +1281,7 @@ function planetScreenState(planet)
             orbiting = shipLocation[2]
           end
           if orbiting and compare(coordinatePlanet(orbiting), coordinatePlanet(self.travel.target[2])) then
-            celestial.flyShip(self.travel.system, self.travel.target)
+            flyShip(self.travel.system, self.travel.target)
             self.travel = {}
           end
         end