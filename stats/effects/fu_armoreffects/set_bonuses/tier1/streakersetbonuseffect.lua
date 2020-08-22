568c79ec5446341637702991228a59d4c576441f
Kherae <cwaldridge@live.com>
2020-08-22 01:49:29 -0700

    streaker

--- stats/effects/fu_armoreffects/set_bonuses/tier1/streakersetbonuseffect.lua
+++ stats/effects/fu_armoreffects/set_bonuses/tier1/streakersetbonuseffect.lua
@@ -15,6 +15,7 @@ function init()
 	
 	effectHandlerList.specialBonusHandle=effect.addStatModifierGroup({})
 	effectHandlerList.armorBonusHandle=effect.addStatModifierGroup(armorBonus)
+	effectHandlerList.capeBonusHandle=effect.addStatModifierGroup({})
 	local elementalTypes=root.assetJson("/damage/elementaltypes.config")
 	local statBuffer={}
 	self.resistanceList={}
@@ -32,21 +33,40 @@ function update(dt)
 	else
 		mcontroller.controlModifiers({speedModifier = 1.20,airJumpModifier = 1.20})
 		self.timer=(self.timer or 0) + dt
+
 		if self.timer > 1.0 then
-			effect.setStatModifierGroup(effectHandlerList.specialBonusHandle,loadElemental())
+			effect.setStatModifierGroup(effectHandlerList.specialBonusHandle,loadElemental(status.stat(setName.."_back")))
 			self.timer=0.0
 		end
 	end
 end
 
-function loadElemental()
+function loadElemental(capeLevel)
 	if not self.resistanceList then return {} end
 	local buffer={}
 	local threatLevel=(world.threatLevel())*0.08
+	
+	if capeLevel>0 then
+		table.insert(buffer,{stat="mentalProtection",effectiveMultiplier=1.0-capeLevel})
+		
+		table.insert(buffer,{stat="breathRegenerationRate",effectiveMultiplier=1+(capeLevel)})
+		table.insert(buffer,{stat="breathDepletionRate",effectiveMultiplier=1-(capeLevel)})
+		
+		for _,stat in pairs({"powerMultiplier","maxHealth","maxEnergy","protection"}) do
+			table.insert(buffer,{stat=stat,effectiveMultiplier=1.0+(capeLevel*0.1)})
+		end
+		
+		threatLevel=threatLevel+(capeLevel*0.5)
+	elseif lastCapeLevel and lastCapeLevel>0 then
+		status.setResource("breath",0)
+		table.insert(buffer,{stat="breathRegenerationRate",effectiveMultiplier=0})
+	end
+	
 	for stat,_ in pairs(self.resistanceList) do
 		table.insert(buffer,{stat=stat,amount=threatLevel})
 		table.insert(buffer,{stat=stat,effectiveMultiplier=0.5})
 	end
 	
+	lastCapeLevel=capeLevel
 	return buffer
 end
