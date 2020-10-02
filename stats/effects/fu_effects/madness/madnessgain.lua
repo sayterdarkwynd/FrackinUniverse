eb13c730f162f3a0d9ce5697a9c56b25bbb085c3
Kherae <cwaldridge@live.com>
2020-10-01 23:24:24 -0700

    f

--- stats/effects/fu_effects/madness/madnessgain.lua
+++ stats/effects/fu_effects/madness/madnessgain.lua
@@ -11,15 +11,16 @@ function update(dt)
 	if (world.entityType(entity.id())~="player") or world.getProperty("invinciblePlayers") or status.statPositive("invulnerable") then
 		return {}
 	end
+	local afkLvl=afkLevel()
 
 	self.penaltyTimer = (self.penaltyTimer or 0.0) + dt
 	if self.penaltyTimer >= 1 then
-		math.max(0,self.penaltyAmount=self.penaltyAmount-(self.penaltyRate*self.penaltyTimer))
+		self.penaltyAmount=((afkLvl>0) and math.max(0,self.penaltyAmount-(self.penaltyRate*self.penaltyTimer*afkLvl))) or 0.0
 		self.penaltyTimer=0.0
 	end
 
 	if (math.abs(mcontroller.xVelocity()) < 5) and (math.abs(mcontroller.yVelocity()) < 5) then
-		self.randVal = (math.floor(((math.random(1,4-afkLevel())+self.baseVal)*(1.0 - status.stat("mentalProtection")))*self.valBonus)) * self.penaltyAmount
+		self.randVal = math.floor((((math.random(1,4-afkLvl)+self.baseVal)*(1.0 - status.stat("mentalProtection")))*self.valBonus) * self.penaltyAmount)
 		if (self.randVal > 0) then
 			world.spawnItem("fumadnessresource",entity.position(),self.randVal)
 		end
