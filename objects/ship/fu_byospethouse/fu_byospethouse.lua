94b9debaa74daeebe8b45dc16b77806552865ed4
Hubnester <31746595+Hubnester@users.noreply.github.com>
2020-10-09 14:35:03 +1000

    Science SAIL Pet Fix

--- objects/ship/fu_byospethouse/fu_byospethouse.lua
+++ objects/ship/fu_byospethouse/fu_byospethouse.lua
@@ -17,7 +17,7 @@ function init()
 	storage.spawnTimer = storage.spawnTimer and 0.5 or 0
 	storage.petParams = storage.petParams or {}
 
-	self.monsterType = config.getParameter("shipPetType", "petweasel")
+	self.monsterType = config.getParameter("shipPetType")
 	self.spawnOffset = config.getParameter("spawnOffset", {0, 2})
 	
 	self.hasInputNode = config.getParameter("inputNodes")
@@ -79,6 +79,8 @@ function setPet(entityId, params)
 end
 
 function update(dt)
+	if not self.monsterType then return end	-- don't try to spawn the pet if it's not set
+	
 	if self.petId and not world.entityExists(self.petId) then
 		self.petId = nil
 	end
