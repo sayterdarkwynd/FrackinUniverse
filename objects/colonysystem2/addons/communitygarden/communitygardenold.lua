b1b9e5378bdac5fca6b0108a48195498a4a476c4
Kherae <cwaldridge@live.com>
2020-10-17 12:46:16 -0700

    perennial nerfbat

--- objects/colonysystem2/addons/communitygarden/communitygardenold.lua
+++ objects/colonysystem2/addons/communitygarden/communitygardenold.lua
@@ -239,7 +239,7 @@ end
 --Returns active seed when tray is removed from world
 function die()
 	if storage.currentseed then
-		storage.currentseed.count = getFertSum("seedUse")
+		--storage.currentseed.count = getFertSum("seedUse")
 		world.spawnItem(storage.currentseed, entity.position())
 	end
 end
@@ -303,7 +303,7 @@ function growPlant(growthmod, dt)
 		-- Perennial plants should return yeild of seeds for balance purposes.
 		-- By returning yield seeds we handle part of perennials regrowing from the same seed.
 		if stage().resetToStage then
-			storage.currentseed.count = getFertSum("yield")
+			--storage.currentseed.count = getFertSum("yield")--removing due to massive seed gen
 			fu_sendOrStoreItems(0, storage.currentseed, seedAvoid)
 			storage.perennialSeedName = storage.currentseed.name
 		end
@@ -389,7 +389,7 @@ function genGrowthData()
 
 	-- Set currentStage and possibly growth depending on perennial seed data
 	if storage.perennialSeedName and storage.stage[storage.stages].resetToStage and
-			storage.currentseed.name == storage.perennialSeedName then
+		storage.currentseed.name == storage.perennialSeedName then
 		storage.currentStage = math.min(storage.stages, math.max(1, storage.stage[storage.stages].resetToStage + 1))
 		storage.growth = storage.currentStage == 1 and 0 or	storage.stage[storage.currentStage - 1].val
 	else
@@ -440,7 +440,9 @@ function doSeedIntake()
 	end
 
 	--Consume seed.
-	world.containerConsumeAt(entity.id(),seedslot,getFertSum("seedUse"))
+	local consumed=getFertSum("seedUse")
+	world.containerConsumeAt(entity.id(),seedslot,consumed)
+	storage.currentseed.count=consumed
 
 	return true
 end
