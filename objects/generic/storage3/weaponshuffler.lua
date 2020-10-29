e567a0dc67121fc0941876bb6fce83c07cc4fc4d
Kherae <cwaldridge@live.com>
2020-10-28 20:43:55 -0700

    shuffler code

--- objects/generic/storage3/weaponshuffler.lua
+++ objects/generic/storage3/weaponshuffler.lua
@@ -1,45 +1,73 @@
 require "/scripts/util.lua"
 
-function craftingRecipe(items)
-	if #items ~= 1 then return end
-	local item = items[1]
-	if not item then return end
-	local newParams = copy(item.parameters) or {}
-	local itemBaseParams=root.itemConfig(item.name)
-	itemLevel=item.parameters.level or itemBaseParams.config.level
-	specialsSeed=item.parameters.seed or itemBaseParams.config.seed or os.time()
-	
-	newSeed=math.abs(sb.staticRandomI32(specialsSeed))
-	local randoItem=root.createItem(item.name,itemLevel,newSeed)
-	
-	
-	local newItem = {name = item.name,count = item.count,parameters = newParams}
-	powerTimer=3.0
-	--retData={input = items,output = newItem,duration = powerTimer}
-	retData={input = items,output = randoItem,duration = powerTimer}
-	
-	if not doOnce then
-		doOnce=true
-		dbgJ(item)
-		dbgJ(randoItem)
-		dbg({specialsSeed,newSeed})
-		--dbg({itemLevel=itemLevel})
-		--dbgJ(itemBaseParams)
-		dbgJ(retData)
-	end
-	return retData
+function init()
+	message.setHandler("doShuffle",doShuffle)
 end
 
 function update(dt)
-	powerTimer=(powerTimer and powerTimer > 0) and (powerTimer-dt) or 0
-	animator.setAnimationState("tetherState", powerTimer and powerTimer > 0 and "on" or "off")
-	doOnce=(powerTimer and powerTimer > 0)
+	if storage.output or storage.input then
+		storage.craftTimer=math.max(0,(storage.craftTimer or 3.0)-dt)
+		if storage.craftTimer<=0 then
+			if storage.output then
+				local throw=world.containerPutItemsAt(entity.id(),storage.output,0)
+				if throw then throw=world.containerPutItemsAt(entity.id(),throw,1) end
+				if throw then world.spawnItem(throw,entity.position()) end
+			else
+				local throw=world.containerPutItemsAt(entity.id(),storage.input,0)
+				if throw then throw=world.containerPutItemsAt(entity.id(),throw,1) end
+				if throw then world.spawnItem(throw,entity.position()) end
+			end
+			storage.output=nil
+			storage.input=nil
+			storage.input2=nil
+		end
+	end
 end
 
-function dbgJ(d)
-	dbg(sb.printJson(d))
+function doShuffle()
+	local item1=world.containerItemAt(entity.id(),0)
+	local item2=world.containerItemAt(entity.id(),1)
+	if (not item1) or (not item2) then return end
+	if (item2.name~="upgrademodule") and (item1.name~="upgrademodule") then return end
+	local inputModule
+	local rngWeapon
+	item1.count=1
+	item2.count=1
+	if item1.name~="upgrademodule" then
+		if not item1.parameters.seed then
+			return
+		else
+			rngWeapon=item1
+			inputModule=item2
+		end
+	else
+		if not item2.parameters.seed then
+			return
+		else
+			rngWeapon=item2
+			inputModule=item1
+		end
+	end
+	world.containerConsume(entity.id(),item1)
+	world.containerConsume(entity.id(),item2)
+	storage.input2 = inputModule
+	storage.input = rngWeapon
+	if not storage.input then return end
+
+	if storage.input.parameters.seed then
+		local randoItem=root.createItem(storage.input.name,storage.input.parameters.level or l,math.abs(sb.staticRandomI32(storage.input.parameters.seed or os.time())))
+		storage.output=randoItem
+	end
+	storage.craftTimer=3.0
 end
 
-function dbg(d)
-	sb.logInfo("%s",d)
+function uninit()
+	if storage.input then
+		world.spawnItem(storage.input,entity.position())
+		storage.input=nil
+	end
+	if storage.input2 then
+		world.spawnItem(storage.input2,entity.position())
+		storage.input2=nil
+	end
 end
