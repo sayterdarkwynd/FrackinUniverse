43c389c84f3e500f90c033f88b64898e614e0458
Kherae <cwaldridge@live.com>
2021-02-04 20:57:20 -0800

    Update energyregen.lua

--- stats/effects/energyregen/energyregen.lua
+++ stats/effects/energyregen/energyregen.lua
@@ -4,7 +4,7 @@ function init()
 	animator.setParticleEmitterActive("energy", true)
 
 	handler=effect.addStatModifierGroup({
-		{stat = "energyRegenPercentageRate", amount = config.getParameter("regenBonusAmount", 10)}
+		{stat = "energyRegenPercentageRate", amount = config.getParameter("regenBonusAmount", 10)},
 		{stat = "energyRegenBlockTime", effectiveMultiplier = 0.5}--vanilla: 0. halving regen block is more than fair.
 	})
 end
