4c7a6af027b80aeaabd5f0cbb8096d98eb6546d1
Kherae <cwaldridge@live.com>
2021-03-05 09:23:26 -0800

    soup opt

--- stats/effects/fu_tileeffects/specialty_effects/organicsoupeffect/organicsoupeffect.lua
+++ stats/effects/fu_tileeffects/specialty_effects/organicsoupeffect/organicsoupeffect.lua
@@ -1,54 +1,22 @@
 function init()
-  if status.isResource("food") then
-    hungerMax = { pcall(status.resourceMax, "food") }
-    hungerMax = hungerMax[1] and hungerMax[2]
-    hungerLevel = status.resource("food")
-    baseValue = config.getParameter("healthDown",0)*(status.resourceMax("food"))
-
-    effect.setParentDirectives("border=1;cc005500;00000000")
-    self.tickDamagePercentage = 0.0
-    self.tickTime = 3.0
-    self.tickTimer = self.tickTime
-    script.setUpdateDelta(3)
-  else
-    script.setUpdateDelta(0)
-  end
+	if status.isResource("food") then
+		effect.setParentDirectives("border=1;cc005500;00000000")
+		script.setUpdateDelta(3)
+	else
+		script.setUpdateDelta(0)
+	end
 end
-
-
-
 
 function update(dt)
-  hungerMax = { pcall(status.resourceMax, "food") }
-  hungerMax = hungerMax[1] and hungerMax[2]
-  healthMax = { pcall(status.resourceMax, "health") }
-  healthMax = healthMax[1] and healthMax[2]
-  hungerLevel = status.resource("food")
-  healthLevel = status.resource("health")
-  baseValue = config.getParameter("healthDown",0)*(status.resourceMax("food"))
-  randVal = math.random(1,2)
-
-  self.tickTimer = self.tickTimer - dt
-  if self.tickTimer <= 0 then
-    self.tickTimer = self.tickTime
-    if randVal == 1 then
-	  if (hungerLevel < hungerMax) then
-	    adjustedHunger = hungerLevel + (hungerLevel * 0.015)
-	    status.setResource("food", adjustedHunger)
-	  end
-    else
-	  if (healthLevel < healthMax) then
-	    adjustedHealth = healthLevel + (healthLevel * 0.015*math.max(0,1+status.stat("healingBonus")))
-	    status.setResource("health", adjustedHealth)
-	  end
-    end
-
-  end
-
-  effect.setParentDirectives("fade=aa00cc="..self.tickTimer * 0.4)
-
-end
-
-function uninit()
-
+	self.tickTimer = (self.tickTimer or 3.0) - dt
+	effect.setParentDirectives("fade=aa00cc="..self.tickTimer * 0.4)
+	if self.tickTimer <= 0 then
+		randVal = math.random(1,2)
+		self.tickTimer = nil
+		if randVal == 1 then
+			status.modifyResource("food", (status.resource("food") * 0.015))
+		else
+			status.modifyResource("health", (status.resource("health") * 0.015*math.max(0,1+status.stat("healingBonus"))))
+		end
+	end
 end
