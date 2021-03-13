c8bc45e7e7feb548ed7d1c93dd944caef2bc7f3c
Kherae <cwaldridge@live.com>
2021-03-13 14:46:50 -0800

    plague rescale

--- stats/effects/fu_effects/draingroup/fuwanderingplague.lua
+++ stats/effects/fu_effects/draingroup/fuwanderingplague.lua
@@ -13,13 +13,15 @@ function init()
 	end
 
 	--damage ticks every second, and much of the effect is oriented around that.
-	self.tickTime = 1.0
-	--damage starts piddly, but ramps up as the effect goes on. at 0.0014, the effect will do approximately 40% of the target's health.
-	self.baseDamage=config.getParameter("baseDamage",0.0014)
+	self.tickTime = 2.0
+	--damage starts piddly, but ramps up as the effect goes on. this value is a percentage.
+	self.baseDamage=config.getParameter("baseDamage",0.0007)
+	--'hard' target damage is scaled by world tier, multiplied by base damage and any special scaling
+	self.hardMult=config.getParameter("baseDamage",500)
 	
-	--hard capped. 30s max duration.
+	--30s internal timer, no refreshing
 	local dur=effect.duration()
-	self.duration=self.duration or math.min(dur,30)
+	self.duration=self.duration or math.min(30,dur)
 	effect.modifyDuration(self.duration-dur)
 end
 
@@ -44,44 +46,41 @@ function update(dt)
 
 	--check duration, if it's not within an expected range we reset it back to the correct duration.
 	if (self.duration~=dur) and (math.abs(self.duration-dur)>(2*dt)) then
-		effect.modifyDuration(1.0-dur)
+		self.stacks=(self.stacks or 0)+1
+		effect.modifyDuration(self.duration-dur)
 	end
 
-	--damage starts off weak, but progressively ramps up by about 5.5x at the end of the duration
-	local damageCalc=self.baseDamage*math.sqrt(math.max(0.1,30-self.duration))
-	--can't forget hard target distinction. percentage of health against beefy targets is broken, and not allowed.
-	damageCalc=math.ceil((status.statPositive("specialStatusImmunity") and world.threatLevel*damageCalc*150) or (status.resourceMax("health") * damageCalc))
+	self.tickTimer = math.max(0,(self.tickTimer or self.tickTime) - dt)
+	if self.tickTimer <= 0 then
+		--damage starts off weak, but progressively ramps up
+		damageCalc=self.baseDamage*((self.stacks or 0)+math.max(1,(30.0-self.duration)/2))
+		--can't forget hard target distinction. percentage of health against beefy targets is broken, and not allowed.
+		damageCalc=math.ceil((status.statPositive("specialStatusImmunity") and world.threatLevel()*damageCalc*self.hardMult) or (status.resourceMax("health") * damageCalc))
 
-	--effect uses two damage instances, this instance is frost and is covered by ice resistance
-	local iceRes=status.stat("iceResistance")
-	self.tickTimer2 = math.max(0,(self.tickTimer2 or self.tickTime) - dt)
-	if iceRes <= 0.8 then
-		if self.tickTimer2 <= 0 then
-			self.tickTimer2 = self.tickTime
+		--effect uses two damage instances, this instance is frost and is covered by ice resistance
+		local iceRes=status.stat("iceResistance")
+		if iceRes <= 0.8 then
 			status.applySelfDamageRequest({damageType = "IgnoresDef",damage = damageCalc,damageSourceKind = "hoarfrost",sourceEntityId = self.source})
 		end
-	end
 
-	--similar to the ice section, but this one is poison
-	local poisonRes=status.stat("poisonResistance")
-	self.tickTimer1 = math.max(0,(self.tickTimer1 or self.tickTime) - dt)
-	if poisonRes <= 0.8 then
-		if self.tickTimer1 <= 0 then
-			self.tickTimer1 = self.tickTime
+		--similar to the ice section, but this one is poison
+		local poisonRes=status.stat("poisonResistance")
+		if poisonRes <= 0.8 then
 			status.applySelfDamageRequest({damageType = "IgnoresDef",damage = damageCalc,damageSourceKind = "bioweapon",sourceEntityId = self.source})
+			effect.setParentDirectives(string.format("fade=00AA00=%.1f", self.tickTimer * 0.4))
 		end
-		effect.setParentDirectives(string.format("fade=00AA00=%.1f", self.tickTimer1 * 0.4))
-	end
 
-	local comboRes=iceRes+poisonRes
-	--bonus effect, hybrid resistance
-	if comboRes < 0.80 then
-		effectUtil.effectSelf("staffslow",self.tickTime*2,self.source)
-	end
+		local comboRes=iceRes+poisonRes
+		--bonus effect, hybrid resistance
+		if comboRes < 0.80 then
+			effectUtil.effectSelf("staffslow",self.tickTime*2,self.source)
+		end
 
-	--spread mechanic; if the subject lacks sufficient sum resistance, the effect spreads at the same duration, sourced to the same entity.
-	if comboRes <= 1.0 then
-		effectUtil.effectAllInRange("fuwanderingplague",30,self.duration,self.source)
+		--spread mechanic; if the subject lacks sufficient sum resistance, the effect spreads at the same duration, sourced to the same entity.
+		if comboRes <= 1.0 then
+			effectUtil.effectAllInRange("fuwanderingplague",16,self.duration,self.source)
+		end
+		self.tickTimer = self.tickTime
 	end
 end
 
