3084baad01984d7e4f159997e6971f5ff422b417
sayterdarkwynd <sayterdarkwynd@gmail.com>
2021-06-06 08:45:06 -0400

    missed one spell file

--- items/active/weapons/staff/abilities/controlprojectile/plasmabarrage.lua
+++ items/active/weapons/staff/abilities/controlprojectile/plasmabarrage.lua
@@ -7,10 +7,25 @@ function ControlProjectile:createProjectiles()
   local basePos = activeItem.ownerAimPosition()
 
   local pCount = self.projectileCount or 1
+  -- bonus projectiles
+  if self.staffMasteryBase > 0.95 then
+      self.bonusProjectiles = 6
+  elseif self.staffMasteryBase > 0.80 then
+     self.bonusProjectiles = 5
+  elseif self.staffMasteryBase > 0.60 then
+     self.bonusProjectiles = 4
+  elseif self.staffMasteryBase > 0.40 then
+     self.bonusProjectiles = 3
+  elseif self.staffMasteryBase > 0.20 then
+     self.bonusProjectiles = 2
+  else
+     self.bonusProjectiles = 1
+  end
+  pCount = pCount + self.bonusProjectiles
 
   local pParams = copy(self.projectileParameters)
   pParams.power = self.baseDamageFactor * pParams.baseDamage * config.getParameter("damageLevelMultiplier") / pCount
-  pParams.powerMultiplier = activeItem.ownerPowerMultiplier()
+  pParams.powerMultiplier = activeItem.ownerPowerMultiplier() * self.staffMastery 
 
   for i = 1, pCount do
     pParams.delayTime = self.projectileDelayFirst + (i - 1) * self.projectileDelayEach
