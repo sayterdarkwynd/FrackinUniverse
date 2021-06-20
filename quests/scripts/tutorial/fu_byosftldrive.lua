0212cd23d5ec6991399398bd9915e9165db9ebe2
Zarra-0 <72447426+Zarra-0@users.noreply.github.com>
2021-06-20 03:47:27 +0100

    Tutorial now teaches FTL drive
    Esther gives a scripted quest to BYOS players after meeting her for the first time, with radio messages, which teaches the player:
    - How to research and build an STL drive
    - How to research and craft an EPP
    - Where to obtain tungsten
    - Where to get the 50 erchius fuel required for an FTL drive
    - How to research and build an FTL drive
    
    Removed Warp Manifolds from the welding penguin, as Esther covers the steps to obtain one.

---
+++ quests/scripts/tutorial/fu_byosftldrive.lua
@@ -0,0 +1,94 @@
+require "/scripts/util.lua"
+require "/quests/scripts/portraits.lua"
+require "/quests/scripts/questutil.lua"
+
+function init()
+  setPortraits()
+
+  storage.complete = storage.complete or false
+
+  storage.stage = storage.stage or 1
+  self.stages = {
+    makeSTL,
+    getErchius,
+    makeFTL
+  }
+
+  self.descriptions = config.getParameter("descriptions")
+
+  self.state = FSM:new()
+  self.state:set(self.stages[storage.stage])
+end
+
+function questInteract(entityId)
+end
+
+function questStart()
+  player.radioMessage("fu_byosftldrive-makeSTL")
+end
+
+function update(dt)
+  self.state:update(dt)
+
+  if storage.complete then
+    quest.complete()
+  end
+end
+
+function questComplete()
+  setPortraits()
+  questutil.questCompleteActions()
+end
+
+function makeSTL()
+  sb.logInfo("makeSTL started")
+
+  while storage.stage == 1 do
+    quest.setObjectiveList({{self.descriptions.makeSTL, false}})
+    if player.hasItem({name = "fu_stldrive", count = 1}) then
+  sb.logInfo("makeSTL finished")
+      storage.stage = 2
+    elseif player.hasItem({name = "fu_ftldrivesmall", count = 1}) then
+      storage.complete = true
+    end
+    coroutine.yield()
+  end
+
+  self.state:set(self.stages[storage.stage])
+end
+
+function getErchius()
+  player.radioMessage("fu_byosftldrive-getErchius1")
+  player.radioMessage("fu_byosftldrive-getErchius2")
+  player.radioMessage("fu_byosftldrive-getErchius3")
+  sb.logInfo("getErchius started")
+
+  while storage.stage == 2 do
+    quest.setObjectiveList({{self.descriptions.getErchius, false}})
+    quest.setProgress(player.hasCountOfItem("solidfuel") / 50)
+    if player.hasItem({name = "solidfuel", count = 50}) then
+  sb.logInfo("getErchius finished")
+      storage.stage = 3
+    elseif player.hasItem({name = "fu_ftldrivesmall", count = 1}) then
+      storage.complete = true
+    end
+    coroutine.yield()
+  end
+
+  self.state:set(self.stages[storage.stage])
+end
+
+function makeFTL()
+  player.radioMessage("fu_byosftldrive-makeFTL")
+  quest.setProgress(nil)
+  sb.logInfo("makeFTL started")
+
+  while storage.stage == 3 do
+    quest.setObjectiveList({{self.descriptions.makeFTL, false}})
+    if player.hasItem({name = "fu_ftldrivesmall", count = 1}) then
+  sb.logInfo("makeFTL finished")
+      storage.complete = true
+    end
+    coroutine.yield()
+  end
+end
