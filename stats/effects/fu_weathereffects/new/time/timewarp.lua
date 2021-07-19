5b043cb3bab0e1f714f113282b96d924288c23e2
Kherae <cwaldridge@live.com>
2021-07-18 23:01:14 -0700

    add chance to freeze

--- stats/effects/fu_weathereffects/new/time/timewarp.lua
+++ stats/effects/fu_weathereffects/new/time/timewarp.lua
@@ -8,14 +8,19 @@ end
 function update(dt)
 	if warpTimer and warpTimer<=0 then
 		local timer=temporalMin+(math.random())*temporalAdder
-		if math.random()>=0.83 then
-			status.addEphemeralEffect("futimewarp",timer)
-		end
-		if math.random()>=0.49 then
-			if math.random()>=0.5 then
-				status.addEphemeralEffect("timeslip",timer)
-			else
-				status.addEphemeralEffect("runboost",timer)
+		if math.random()>=0.95 then
+			status.addEphemeralEffect("
+			status.addEphemeralEffect("futimewarp",timer)",timer)
+		else
+			if math.random()>=0.83 then
+				status.addEphemeralEffect("futimewarp",timer)
+			end
+			if math.random()>=0.49 then
+				if math.random()>=0.5 then
+					status.addEphemeralEffect("timeslip",timer)
+				else
+					status.addEphemeralEffect("runboost",timer)
+				end
 			end
 		end
 		warpTimer=timer
