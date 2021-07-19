26d5f900dc7a618f7c2b53d9c82e2417667eb786
Kherae <cwaldridge@live.com>
2021-07-18 21:44:57 -0700

    safety code

--- stats/effects/fu_effects/warp/autowarp.lua
+++ stats/effects/fu_effects/warp/autowarp.lua
@@ -1,8 +1,17 @@
 function init()
-  script.setUpdateDelta(3)
-  rescuePosition = mcontroller.position()
+	if not world.entityType(entity.id()) then delayInit=true return end
+	script.setUpdateDelta(3)
+	rescuePosition = mcontroller.position()
+end
+
+function update()
+	if delayInit then
+		delayInit=false
+		init()
+	end
 end
 
 function uninit()
+	if delayInit then return end
 	mcontroller.setPosition(rescuePosition)
 end
