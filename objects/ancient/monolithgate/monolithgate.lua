ff2532c66f4547e4a68544c3bd1acfa35019d4e0
Kherae <cwaldridge@live.com>
2021-08-14 05:23:02 -0700

    ruin gate performance fix

---
+++ objects/ancient/monolithgate/monolithgate.lua
@@ -0,0 +1,34 @@
+require "/scripts/util.lua"
+
+function init()
+	self.flagAnimationStates = config.getParameter("flagAnimationStates")
+	object.setInteractive(false)
+	self.states={}
+	message.setHandler("isOpen", function() return contains(world.universeFlags(), "final_gate_key") ~= false end)
+end
+
+function update(dt)
+	for i, flag in ipairs(world.universeFlags()) do
+		if self.flagAnimationStates[flag] then
+			self.states[self.flagAnimationStates[flag]]=true
+		end
+	end
+	if self.states["open"] then
+		if fuzzState("open","on") then
+			object.setInteractive(true)
+		end
+	else
+		for state, _ in pairs(self.states) do
+			fuzzState(state,"on")
+		end
+	end
+end
+
+function fuzzState(state,value)
+	local changed
+	if animator.animationState(state)~=value then
+		animator.setAnimationState(state,value)
+		changed=true
+	end
+	return changed
+end
