69e45a7b43737537b2525c7685d3f3af79e40cef
Kherae <cwaldridge@live.com>
2022-01-16 18:57:53 -0800

    militia

---
+++ stats/effects/fu_armoreffects/set_bonuses/tier2/militiasetbonuseffect.lua
@@ -0,0 +1,36 @@
+require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
+setName="fu_militiaset"
+
+armorBonus={
+	{stat="maxHealth",effectiveMultiplier=1.1 }
+}
+
+weaponBonus={
+	{stat="powerMultiplier",effectiveMultiplier=1.15 }
+}
+
+function init()
+	setSEBonusInit(setName)
+	effectHandlerList.weaponBonus1Handle=effect.addStatModifierGroup({})
+
+	checkWeapons()
+
+	effectHandlerList.armorBonusHandle=effect.addStatModifierGroup(armorBonus)
+end
+
+function update(dt)
+	if not checkSetWorn(self.setBonusCheck) then
+		effect.expire()
+	else
+		checkWeapons()
+	end
+end
+
+function checkWeapons()
+	local weapons=weaponCheck({ "shotgun", "assaultrifle" })
+	if weapons["either"] then
+		effect.setStatModifierGroup(effectHandlerList.weaponBonus1Handle,weaponBonus)
+	else
+		effect.setStatModifierGroup(effectHandlerList.weaponBonus1Handle,{})
+	end
+end
