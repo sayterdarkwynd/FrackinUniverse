5edd3b2c7222295579d2be43cbd04e5260173871
Kherae <cwaldridge@live.com>
2022-01-16 19:04:07 -0800

    lunari

---
+++ stats/effects/fu_armoreffects/set_bonuses/tier2/lunarisetbonuseffect.lua
@@ -0,0 +1,39 @@
+require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
+setName="fu_lunariset"
+
+armorBonus={
+	{stat="energyRegenPercentageRate",baseMultiplier=1.12 },
+	{stat="energyRegenBlockTime",baseMultiplier=0.85 },
+	{stat="shipMass",baseMultiplier=0.9 }
+}
+
+weaponBonus={
+	{stat="critChance",amount=4 },
+	{stat="powerMultiplier",effectiveMultiplier=1.15 }
+}
+
+function init()
+	setSEBonusInit(setName)
+	effectHandlerList.weaponBonus1Handle=effect.addStatModifierGroup({})
+
+	checkWeapons()
+
+	effectHandlerList.armorBonusHandle=effect.addStatModifierGroup(armorBonus)
+end
+
+function update(dt)
+	if not checkSetWorn(self.setBonusCheck) then
+		effect.expire()
+	else
+		checkWeapons()
+	end
+end
+
+function checkWeapons()
+	local weapons=weaponCheck({ "lunari" })
+	if weapons["either"] then
+		effect.setStatModifierGroup(effectHandlerList.weaponBonus1Handle,weaponBonus)
+	else
+		effect.setStatModifierGroup(effectHandlerList.weaponBonus1Handle,{})
+	end
+end
