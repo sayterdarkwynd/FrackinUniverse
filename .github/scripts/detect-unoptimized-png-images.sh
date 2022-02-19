79510e8bd59d93fd8c80b2f5e6aa48afc77a2f31
Edward Chernenko <edwardspec@gmail.com>
2022-02-19 18:18:17 +0300

    (tests) Only check PNG images that changed since the master branch

--- .github/scripts/detect-unoptimized-png-images.sh
+++ .github/scripts/detect-unoptimized-png-images.sh
@@ -12,9 +12,21 @@ OPTIPNG_COMMAND="optipng -o1"
 ###############################################################################
 
 fails=0
-
 IFS=$'\n' # To iterate over filenames with spaces.
-for FILENAME in $(find . -name '*.png'); do
+
+# If some image wasn't added/modified compared to the master branch, then it won't be checked,
+# because we have 30k+ images, and rechecking them every time would take ~8 minutes.
+git fetch --depth=1 origin master
+FILES=$(git diff origin/master --name-only --no-renames --diff-filter=ACM | grep -E '\.png$' | sort)
+
+if [ "x$FILES" == 'x' ]; then
+	echo "Nothing to check: no images changed since the master branch."
+	exit 0
+fi
+
+echo "Checking PNG images: $FILES"
+
+for FILENAME in $FILES; do
 	unset IFS
 	PERCENT=$($OPTIPNG_COMMAND $FILENAME 2>&1 | grep 'Output file size' | grep -E -o '([0-9.]+)%' | cut -d% -f1)
 
