257d7b62f5a1e6123460742d634bcd6c52330fe7
Kherae <cwaldridge@live.com>
2022-06-09 11:35:32 -0700

    simplify incubator

--- objects/crafting/eggstra/eggincubator/incubator.lua
+++ objects/crafting/eggstra/eggincubator/incubator.lua
@@ -1,51 +1,39 @@
 -- Egg Incubation
 -- player places egg in incubator
 -- egg hatch time is eggs base hatch time +- any modifiers
---currently not functional. -- factors that influence egg hatch time are "hardiness", the type of egg, warmth. colder worlds hatch slower (unless a cold-type egg)
 -- plus every egg also gets a random variable attached to it that can change the hatch time, so each egg is a bit unique
 
 function init()
 	-- egg type?
 	self.entityId=entity.id()
 	self.centrifugeType = config.getParameter("centrifugeType")
+
 	incubation = config.getParameter("incubation")
 
-	-- egg modifiers 
-	--eggmodifiers = {
-	--	default = 1
-	--}
-
 	spawnMod = math.random(10) -- + config.getParameter("spawnModValue") -- change this to check the egg instead. each egg has a spawnMod that influences its hatch time
-	--storage.warmth = 0 -- is world temperature suitable? warmer weather reduces spawn time unless it likes cold --does nothing
-	--storage.hardiness = 0 -- how tough is the egg? the tougher it is, the longer it takes to hatch --does nothing
 end
 
 
-function fillPercent(container)
-	if type(container) ~= "number" then return nil end
+function fillAmount()
+	if type(self.entityId) ~= "number" then return nil end
 
-	local size = world.containerSize(container)
-	if type(size)~="number" then return nil end
+	local containerSize = world.containerSize(self.entityId)
+	if type(containerSize)~="number" then return nil end
 
-	local count = 0
-	for i = 0,size,1 do
-		local item = world.containerItemAt(container, i)
-
+	--just count total number of items across all stacks
+	local count=0
+	for i = 0,containerSize,1 do
+		local item = world.containerItemAt(self.entityId, i)
 		if item ~= nil then
-			count = count + 1
-
-			if size == 1 then
-				size = 999999
-				count = item.count
-			end
+			count=count+item.count
 		end
 	end
-	return (count/size)
+	return count
 end
 
 function binInventoryChange()
-	local frames = config.getParameter("binFrames", 10) - 1
-	local fill = math.ceil(frames * fillPercent(self.entityId))
+	--after counting, just use the highest valid frame.
+	local fill = math.min(config.getParameter("binFrames", 10) - 1,fillAmount())
 	if self.fill ~= fill then
 		self.fill = fill
 		animator.setAnimationState("fillState", tostring(fill))
@@ -104,6 +92,11 @@ function checkHatching()
 			animator.setGlobalTag("bin_indicator", self.timer)
 		end
 		self.timer = self.timer + 1
+	else
+		storage.incubationTime = nil
+		self.indicator=0
+		self.timer=0
+		animator.setGlobalTag("bin_indicator", self.timer)
 	end
 end
 
