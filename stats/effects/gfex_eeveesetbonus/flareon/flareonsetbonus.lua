b6dcc77c4669e70202a84d80c0e5fec977ce2554
Day White <dustxwhite@gmail.com>
2022-07-14 14:55:56 -0400

    Flareon Set Bonus
    Ice resistance and immunity to tier 1 ice planets

---
+++ stats/effects/gfex_eeveesetbonus/flareon/flareonsetbonus.lua
@@ -0,0 +1,11 @@
+function init()
+   effect.addStatModifierGroup({{stat = "iceResistance", amount = 0.25}, {stat = "biomecoldImmunity", amount = 1}})
+
+   script.setUpdateDelta(0)
+end
+
+function update(dt)
+end
+
+function uninit()
+end
