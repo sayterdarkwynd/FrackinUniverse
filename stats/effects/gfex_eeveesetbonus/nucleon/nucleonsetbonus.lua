da11a0a39534104a482590b3d260d6cd0ac731ee
Day White <dustxwhite@gmail.com>
2022-07-14 16:29:31 -0400

    Update nucleonsetbonus.lua

--- stats/effects/gfex_eeveesetbonus/nucleon/nucleonsetbonus.lua
+++ stats/effects/gfex_eeveesetbonus/nucleon/nucleonsetbonus.lua
@@ -1,10 +1,9 @@
 function init()
    effect.addStatModifierGroup({
      {stat = "biomeradiationImmunity", amount = 1},
-	   {stat = "radioactiveResistance", amount = 0.15},
+     {stat = "radioactiveResistance", amount = 0.15},
      {stat = "sulphuricImmunity", amount = 1}
    })
-  
   script.setUpdateDelta(0)
 end
 
