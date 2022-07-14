d5c5053fe148e4b2b5c2aed09c65b88409ae6702
Day White <dustxwhite@gmail.com>
2022-07-14 15:08:12 -0400

    Nerf Vaporeon

--- stats/effects/gfex_eeveesetbonus/vaporeon/vaporeonsetbonus.lua
+++ stats/effects/gfex_eeveesetbonus/vaporeon/vaporeonsetbonus.lua
@@ -1,8 +1,8 @@
 function init()
   effect.addStatModifierGroup({
-    {stat = "fireResistance", amount = 0.25},
+    {stat = "fireResistance", amount = 0.15},
     {stat = "wetImmunity", amount = 1},
-    {stat = "maxBreath", amount = 1500}
+    {stat = "maxBreath", amount = 1000}
   })
   self.movementParameters = config.getParameter("movementParameters", {})
 end
