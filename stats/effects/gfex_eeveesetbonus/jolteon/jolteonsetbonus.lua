63c0587ecf8dbcba998d3c5698261a4e28f8e1ab
Day White <dustxwhite@gmail.com>
2022-07-14 15:07:48 -0400

    Nerf Jolteon

--- stats/effects/gfex_eeveesetbonus/jolteon/jolteonsetbonus.lua
+++ stats/effects/gfex_eeveesetbonus/jolteon/jolteonsetbonus.lua
@@ -1,5 +1,5 @@
 function init()
-  effect.addStatModifierGroup({{stat = "electricResistance", amount = 0.25}, {stat = "electricStatusImmunity", amount = 1}})
+  effect.addStatModifierGroup({{stat = "electricResistance", amount = 0.15}, {stat = "electricStatusImmunity", amount = 1}})
 
   script.setUpdateDelta(0)
 end
