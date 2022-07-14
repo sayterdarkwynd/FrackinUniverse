a054d9de713c0da0310473b9d27377ea61cab3e7
Day White <dustxwhite@gmail.com>
2022-07-14 15:35:36 -0400

    Leafeon Set Bonus
    Electric resistance! Immune to tar movement debuff

---
+++ stats/effects/gfex_eeveesetbonus/leafeon/leafeonsetbonus.lua
@@ -0,0 +1,14 @@
+function init()
+   effect.addStatModifierGroup({
+     {stat = "tarStatusImmunity", amount = 1},
+	   {stat = "electricResistance", amount = 0.15}
+   })
+  
+  script.setUpdateDelta(0)
+end
+
+function update(dt)
+end
+
+function uninit()
+end
