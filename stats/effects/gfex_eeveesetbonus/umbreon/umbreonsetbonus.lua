8225283b1a653f75d5e84c3ea4c6850a1cc80037
Day White <dustxwhite@gmail.com>
2022-07-14 15:27:26 -0400

    Umbreon Set Bonus
    Cosmic resistance. Poison thorns (included in base mod).

---
+++ stats/effects/gfex_eeveesetbonus/umbreon/umbreonsetbonus.lua
@@ -0,0 +1,9 @@
+function init()
+  effect.addStatModifierGroup({{stat = "cosmicResistance", amount = 0.1},{stat = "gfex_umbrethorns", amount = 1}})
+end
+
+function update(dt)
+end
+
+function uninit()
+end
