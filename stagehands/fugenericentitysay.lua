dfb493200ae783085cbaacf196ec1417985963df
Kherae <cwaldridge@live.com>
2022-10-01 16:02:01 -0700

    Update fugenericentitysay.lua

--- stagehands/fugenericentitysay.lua
+++ stagehands/fugenericentitysay.lua
@@ -9,7 +9,8 @@ function update(dt)
 	if self and self.message and self.targetId and world.entityExists(self.targetId) then
 		local eType=world.entityType(self.targetId)
 		if (eType=="monster") or (eType=="npc") or ("object") then
-			local pass,result=pcall(world.callScriptedEntity,self.targetId, eType..".say",self.message)
+			--local pass,result=
+			pcall(world.callScriptedEntity,self.targetId, eType..".say",self.message)
 		else
 			effectUtil.messageParticle(world.entityPosition(self.targetId),self.message)
 		end
