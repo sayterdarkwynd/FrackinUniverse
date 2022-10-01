04bbeaecc12ad7d0f44acf4684c5160a2b266bdc
Kherae <cwaldridge@live.com>
2022-09-30 21:27:19 -0700

    minor script opt

--- stagehands/fugenericentitysay.lua
+++ stagehands/fugenericentitysay.lua
@@ -3,18 +3,13 @@ require "/scripts/effectUtil.lua"
 
 function init()
 	self = config.getParameter("messageData")
-	--sb.logInfo("%s",self)
 end
 
 function update(dt)
 	if self and self.message and self.targetId and world.entityExists(self.targetId) then
 		local eType=world.entityType(self.targetId)
-		if eType=="monster" then
-			world.callScriptedEntity(self.targetId, "monster.say",self.message)
-		elseif eType=="npc" then
-			world.callScriptedEntity(self.targetId, "npc.say",self.message)
-		elseif eType=="object" then
-			world.callScriptedEntity(self.targetId, "object.say",self.message)
+		if (eType=="monster") or (eType=="npc") or ("object") then
+			world.callScriptedEntity(self.targetId, eType..".say",self.message)
 		else
 			effectUtil.messageParticle(world.entityPosition(self.targetId),self.message)
 		end
