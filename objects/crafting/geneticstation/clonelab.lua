fa68f4449e5a123e406f8a0c707dffe84b471a6e
kukagres <darth@darth-pc>
2023-02-11 05:35:15 -0300

    Finished adding working code to genetics station. Added to seeds the atribute "retainObjectParametersInItem".

--- objects/crafting/geneticstation/clonelab.lua
+++ objects/crafting/geneticstation/clonelab.lua
@@ -26,17 +26,10 @@ function containerCallback()
             local stemsample = items[1]
             if checkBanlist(stemsample, stemsample) then
                 local selectedCatalyst = catalyst[items[2].name]
-                if (selectedCatalyst.count<=items[2].count) then                
+                if ((selectedCatalyst.count)<=(items[2].count)) then   
                     local itemConfig = root.itemConfig(stemsample)
                     local mergeBuffer={}
-                    local stages=itemConfig.config.stages
-                    local description = itemConfig.config.description
-                    local descArray = string_split(description,"\n")
-                    if(descArray[2])then
-                        -- is modified sapling
-                        local tempArray = string_split(descArray[2],"(")
-                        local saplingLevel = string_split(tempArray[2],")")
-                    end    
+                    local stages=stemsample.parameters.stages or itemConfig.config.stages
                     local minDuration=0
                     local maxDuration=0
                     local canConsume=false
@@ -55,17 +48,15 @@ function containerCallback()
                             end
                         end   
                     end
-                    if(saplingLevel)then
-                        mergeBuffer.description = descArray[1].."\n^green;Modified sapling ("..saplingLevel[1]..")^reset;\n Time ("..minDuration.."/"..maxDuration..")"
-                    else
-                        mergeBuffer.description = descArray[1].."\n^green;Modified sapling (1)^reset;\n Time ("..minDuration.."/"..maxDuration..")"
-                    end
+                    saplingLevel = stemsample.parameters.saplingLevel or 0
+                    mergeBuffer.description = itemConfig.config.description.."\nModified sapling (^green;"..(saplingLevel+1).."^reset;)\n Time (^green;"..minDuration.."^reset;/^green;"..maxDuration.."^reset;)"
+                    mergeBuffer.saplingLevel = saplingLevel+1
                     mergeBuffer.stages = stages
                     if(canConsume)then
-if(world.containerConsumeAt(entity.id(),2,10))then
+                        if(world.containerConsumeAt(entity.id(),1,selectedCatalyst.count))then
                             stemsample.parameters=util.mergeTable(copy(stemsample.parameters),copy(mergeBuffer))
                             world.containerTakeAt(entity.id(),0)
-                            world.containerPutItemsAt(entity.id(),stemsample,0)
+                            world.containerPutItemsAt(entity.id(),stemsample,2)
                         end
                     end
                 end
@@ -76,17 +67,10 @@ if(world.containerConsumeAt(entity.id(),2,10))then
             -- seed combination        
             local seedsample = items[1]
             local selectedCatalyst = catalyst[items[2].name]
-            if (selectedCatalyst.count<=items[2].count) then                
+            if ((selectedCatalyst.count)<=(items[2].count)) then                
                 local itemConfig = root.itemConfig(seedsample)
                 local mergeBuffer={}
-                local stages=itemConfig.config.stages
-                local description = itemConfig.config.description
-                local descArray = string_split(description,"\n")
-                if(descArray[2])then
-                    -- is modified seed
-                    local tempArray = string_split(descArray[2],"(")
-                    local seedLevel = string_split(tempArray[2],")")
-                end    
+                local stages=seedsample.parameters.stages or itemConfig.config.stages
                 local minDuration=0
                 local maxDuration=0
                 local canConsume=false
@@ -105,17 +89,15 @@ if(world.containerConsumeAt(entity.id(),2,10))then
                         end
                     end   
                 end
-                if(seedLevel)then
-                    mergeBuffer.description = descArray[1].."\n^green;Modified seed ("..seedLevel[1]..")^reset;\n Time ("..minDuration.."/"..maxDuration..")"
-                else
-                    mergeBuffer.description = descArray[1].."\n^green;Modified seed (1)^reset;\n Time ("..minDuration.."/"..maxDuration..")"
-                end
+                seedLevel = seedsample.parameters.seedLevel or 0
+                mergeBuffer.description = itemConfig.config.description.."\nModified seed (^green;"..(seedLevel+1).."^reset;)\n Time (^green;"..minDuration.."^reset;/^green;"..maxDuration.."^reset;)"
+                mergeBuffer.seedLevel = seedLevel+1
                 mergeBuffer.stages = stages
                 if(canConsume)then
-                    if(world.containerConsumeAt(entity.id(),2,10))then
+                    if(world.containerConsumeAt(entity.id(),1,selectedCatalyst.count))then
                         seedsample.parameters=util.mergeTable(copy(seedsample.parameters),copy(mergeBuffer))
                         world.containerTakeAt(entity.id(),0)
-                        world.containerPutItemsAt(entity.id(),seedsample,0)
+                        world.containerPutItemsAt(entity.id(),seedsample,2)
                     end
                 end
             end
@@ -143,25 +125,6 @@ function checkBanlist(stemsample, stemsample)
         end
     end
     return true
-end
-
-function string_split(str, pat)
-    local t = {}  -- NOTE: use {n = 0} in Lua-5.0
-    local fpat = "(.-)" .. pat
-    local last_end = 1
-    local s, e, cap = str:find(fpat, 1)
-    while s do
-        if s ~= 1 or cap ~= "" then
-            table.insert(t, cap)
-        end
-        last_end = e+1
-        s, e, cap = str:find(fpat, last_end)
-        end
-        if last_end <= #str then
-            cap = str:sub(last_end)
-            table.insert(t, cap)
-        end
-        return t
 end
 
 function die()
