041c457174fa9adf63d4e83dbfbbf622c4478bbd
Kherae <cwaldridge@live.com>
2023-08-09 12:27:56 -0700

    fix for hidden variant of timewarp

--- stats/effects/fu_effects/warp/timewarp.lua
+++ stats/effects/fu_effects/warp/timewarp.lua
@@ -6,6 +6,8 @@ function init()
 	self.rescuePosition = mcontroller.position()
 	self.resources={}
 	self.lockedResources={}
+	self.hidden=config.getParameter("effectHidden",false)
+
 	local configuredResources=status.resourceNames() --root.assetJson("/player.config:statusControllerSettings.resources")
 	for _,v in pairs(configuredResources) do
 		self.resources[v]=status.resource(v)
@@ -16,15 +18,17 @@ function init()
 	if animator.hasSound("timewarp_windup") then
 		animator.playSound("timewarp_windup", -1)
 	end
-	effect.setParentDirectives("fade=6a2284=0.4")
+	if not self.hidden then
+		effect.setParentDirectives("fade=6a2284=0.4")
 
-	self.numeralList={"n1","n2","n3","n4","n5","n6","n7","n8","n9","n10","n11","n12"}
-	for _, numeral in ipairs(self.numeralList) do
-		animator.setParticleEmitterOffsetRegion(numeral, mcontroller.boundBox())
-		animator.setParticleEmitterBurstCount(numeral, 1)
+		self.numeralList={"n1","n2","n3","n4","n5","n6","n7","n8","n9","n10","n11","n12"}
+		for _, numeral in ipairs(self.numeralList) do
+			animator.setParticleEmitterOffsetRegion(numeral, mcontroller.boundBox())
+			animator.setParticleEmitterBurstCount(numeral, 1)
+		end
+
+		self.particle_n=0
 	end
-
-	self.particle_n=0
 end
 
 function update(dt)
@@ -34,11 +38,13 @@ function update(dt)
 		effect.expire()
 	end
 
-	if self.particle_n>=9 then --every 10 updates
-		animator.burstParticleEmitter(self.numeralList[math.random(12)]) --play random numeral
-		self.particle_n=0
-	else
-		self.particle_n=self.particle_n+1
+	if not self.hidden then
+		if self.particle_n>=9 then --every 10 updates
+			animator.burstParticleEmitter(self.numeralList[math.random(12)]) --play random numeral
+			self.particle_n=0
+		else
+			self.particle_n=self.particle_n+1
+		end
 	end
 end
 
