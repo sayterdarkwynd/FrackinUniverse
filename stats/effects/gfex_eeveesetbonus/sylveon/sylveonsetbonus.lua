91dd8ba0d21f697a7465e140e480b8672275376c
Kherae <cwaldridge@live.com>
2024-04-20 08:05:37 -0700

    mary sue fairy type pokemon...

--- stats/effects/gfex_eeveesetbonus/sylveon/sylveonsetbonus.lua
+++ stats/effects/gfex_eeveesetbonus/sylveon/sylveonsetbonus.lua
@@ -1,16 +1,27 @@
 function init()
-  effect.addStatModifierGroup({{stat = "shadowResistance", amount = 0.15}})
-  script.setUpdateDelta(5)
-  self.healingRate = 1.0 / 150
-  effect.addStatModifierGroup({
-    {stat = "energyRegenPercentageRate", amount = 10},
-    {stat = "energyRegenBlockTime", effectiveMultiplier = 0}
-  })
+	script.setUpdateDelta(5)
+	self.healingRate = 1.0 / 150
+	bonusHandler=effect.addStatModifierGroup({
+		{stat = "shadowResistance", amount = 0.15},
+		--{stat = "energyRegenPercentageRate", amount = 10}, {stat = "energyRegenBlockTime", effectiveMultiplier = 0}--no. you don't get to have unlimited energy.
+		{stat = "energyRegenPercentageRate", baseMultiplier = 1.5 }, {stat = "energyRegenBlockTime", effectiveMultiplier = 0.9} --you get some FAIR values instead.
+	})
+	bonusHandler2=effect.addStatModifierGroup({})
 end
 
 function update(dt)
-  status.modifyResourcePercentage("health", self.healingRate * dt)
+	if (world.entityType(entity.id())=="player") or status.resource("health")>=1 then
+		effect.setStatModifierGroup(bonusHandler2,{{stat="healthRegen",amount=status.resourceMax("health")*self.healingRate*math.max(0,1+status.stat("healingBonus"))}})
+	else
+		effect.setStatModifierGroup(bonusHandler2,{})
+	end
 end
 
 function uninit()
+	if bonusHandler then
+		effect.removeStatModifierGroup(bonusHandler)
+	end
+	if bonusHandler2 then
+		effect.removeStatModifierGroup(bonusHandler2)
+	end
 end
