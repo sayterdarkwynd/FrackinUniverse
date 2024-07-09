6327dbcf3b528fdde4eb819a89ca938773946a88
Kherae <cwaldridge@live.com>
2024-07-08 17:49:20 -0700

    more inheriting abilities

---
+++ items/active/weapons/melee/abilities/spear/spin/fuspin.lua
@@ -0,0 +1,53 @@
+require "/scripts/util.lua"
+require "/items/active/weapons/weapon.lua"
+
+Spin = WeaponAbility:new()
+
+function Spin:init()
+	self.cooldownTimer = self.cooldownTime
+	self:reset()
+	self.primaryAbility = getPrimaryAbility()
+	self.damageConfigMerged=util.mergeTable(self.primaryAbility.damageConfig,self.damageConfig)
+end
+
+function Spin:update(dt, fireMode, shiftHeld)
+	WeaponAbility.update(self, dt, fireMode, shiftHeld)
+
+	self.cooldownTimer = math.max(0, self.cooldownTimer - dt)
+
+	if self.weapon.currentAbility == nil
+		and self.cooldownTimer == 0
+		and not status.resourceLocked("energy")
+		and self.fireMode == "alt" then
+
+		self:setState(self.spin)
+	end
+end
+
+function Spin:spin()
+	self.weapon:setStance(self.stances.spin)
+	self.weapon:updateAim()
+
+	animator.setAnimationState("spinSwoosh", "spin")
+	self.weapon.aimAngle = 0
+	activeItem.setOutsideOfHand(true)
+
+	while self.fireMode == "alt" and status.overConsumeResource("energy", self.energyUsage * self.dt) do
+		self.weapon.relativeWeaponRotation = self.weapon.relativeWeaponRotation + util.toRadians(self.spinRate * self.dt)
+		local damageArea = partDamageArea("spinSwoosh")
+		self.weapon:setDamage(self.damageConfigMerged, damageArea)
+		mcontroller.controlModifiers({runningSuppressed=true})
+
+		coroutine.yield()
+	end
+	self.cooldownTimer = self.cooldownTime
+end
+
+function Spin:reset()
+	animator.setAnimationState("spinSwoosh", "idle")
+	activeItem.setOutsideOfHand(false)
+end
+
+function Spin:uninit()
+	self:reset()
+end
