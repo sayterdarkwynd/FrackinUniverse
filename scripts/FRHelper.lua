db31d35d0cdfe9eccb37bfe915947bb244d21068
Kherae <cwaldridge@live.com>
2024-08-24 02:47:12 -0700

    fix stupidcode

--- scripts/FRHelper.lua
+++ scripts/FRHelper.lua
@@ -198,7 +198,7 @@ function FRHelper:applyControlModifiers(cM, cP)
 end
 
 function filterModifiers(stuff)
-	if (status.statPositive("spikeSphereActive") and 1.0) and stuff["speedModifier"] then stuff["speedModifier"]=1.0 end
+	if status.statPositive("spikeSphereActive") and stuff["speedModifier"] then stuff["speedModifier"]=1.0 end
 	return stuff
 end
 
