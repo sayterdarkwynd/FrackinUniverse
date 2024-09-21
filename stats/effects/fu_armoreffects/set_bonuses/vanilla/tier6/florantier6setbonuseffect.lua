ddbe637fb5e401b2744768856567d061b17a1b0e
Kherae <cwaldridge@live.com>
2024-09-21 15:33:33 -0700

    floran

--- stats/effects/fu_armoreffects/set_bonuses/vanilla/tier6/florantier6setbonuseffect.lua
+++ stats/effects/fu_armoreffects/set_bonuses/vanilla/tier6/florantier6setbonuseffect.lua
@@ -1,19 +1,21 @@
 require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
 setName="fu_florantier6setnew"
 
-weaponBonus={
-	{stat = "powerMultiplier", effectiveMultiplier = 1.25}
-}
+-- weaponBonus={
+	-- {stat = "powerMultiplier", effectiveMultiplier = 1.25}
+-- }
 
 armorBonus={
+	{stat = "spearMastery", amount = 0.25},
+	{stat = "bowMastery", amount = 0.25},
 	{stat = "fireStatusImmunity", amount = 1}
 }
 
 function init()
 	setSEBonusInit(setName)
-	effectHandlerList.weaponBonusHandle=effect.addStatModifierGroup({})
+	-- effectHandlerList.weaponBonusHandle=effect.addStatModifierGroup({})
 
-	checkWeapons()
+	-- checkWeapons()
 
 	effectHandlerList.armorBonusHandle=effect.addStatModifierGroup(armorBonus)
 end
@@ -21,18 +23,18 @@ end
 function update(dt)
 	if not checkSetWorn(self.setBonusCheck) then
 		effect.expire()
-	else
+	-- else
 
-		checkWeapons()
+		-- checkWeapons()
 	end
 end
 
-function checkWeapons()
-	local weapons=weaponCheck({"spear","shortspear","bow","lance"})
+-- function checkWeapons()
+	-- local weapons=weaponCheck({"spear","shortspear","bow","lance"})
 
-	if weapons["either"] then
-		effect.setStatModifierGroup(effectHandlerList.weaponBonusHandle,weaponBonus)
-	else
-		effect.setStatModifierGroup(effectHandlerList.weaponBonusHandle,{})
-	end
-end
+	-- if weapons["either"] then
+		-- effect.setStatModifierGroup(effectHandlerList.weaponBonusHandle,weaponBonus)
+	-- else
+		-- effect.setStatModifierGroup(effectHandlerList.weaponBonusHandle,{})
+	-- end
+-- end
