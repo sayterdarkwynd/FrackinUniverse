ff9b1c396e7abececcecdabeef79102b174f3957
Kherae <cwaldridge@live.com>
2024-09-21 15:41:55 -0700

    glitch

--- stats/effects/fu_armoreffects/set_bonuses/vanilla/tier5/glitchtier5setbonuseffect.lua
+++ stats/effects/fu_armoreffects/set_bonuses/vanilla/tier5/glitchtier5setbonuseffect.lua
@@ -1,19 +1,23 @@
 require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
 setName="fu_glitchtier5setnew"
 
-weaponBonus={
-	{stat = "powerMultiplier", effectiveMultiplier = 1.22}
-}
+-- weaponBonus={
+	-- {stat = "powerMultiplier", effectiveMultiplier = 1.22}
+-- }
 
 armorBonus={
+	{stat = "maceMastery", amount = 0.22},
+	{stat = "hammerMastery", amount = 0.22},
+	{stat = "axeMastery", amount = 0.22},
+	{stat = "greataxeMastery", amount = 0.22},
 	{stat ="electricStatusImmunity" , amount = 1 }
 }
 
 function init()
 	setSEBonusInit(setName)
-	effectHandlerList.weaponBonusHandle=effect.addStatModifierGroup({})
+	-- effectHandlerList.weaponBonusHandle=effect.addStatModifierGroup({})
 
-	checkWeapons()
+	-- checkWeapons()
 
 	effectHandlerList.armorBonusHandle=effect.addStatModifierGroup(armorBonus)
 end
@@ -21,18 +25,18 @@ end
 function update(dt)
 	if not checkSetWorn(self.setBonusCheck) then
 		effect.expire()
-	else
+	-- else
 
-		checkWeapons()
+		-- checkWeapons()
 	end
 end
 
-function checkWeapons()
-	local weapons=weaponCheck({"axe","hammer","greataxe","mace"})
+-- function checkWeapons()
+	-- local weapons=weaponCheck({"axe","hammer","greataxe","mace"})
 
-	if weapons["either"] then
-		effect.setStatModifierGroup(effectHandlerList.weaponBonusHandle,weaponBonus)
-	else
-		effect.setStatModifierGroup(effectHandlerList.weaponBonusHandle,{})
-	end
-end
+	-- if weapons["either"] then
+		-- effect.setStatModifierGroup(effectHandlerList.weaponBonusHandle,weaponBonus)
+	-- else
+		-- effect.setStatModifierGroup(effectHandlerList.weaponBonusHandle,{})
+	-- end
+-- end
