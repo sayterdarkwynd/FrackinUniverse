3c8f9806fa7f7c3e9182737760dc49b6930872c3
Kherae <cwaldridge@live.com>
2024-09-21 16:08:04 -0700

    nova

--- stats/effects/fu_armoreffects/set_bonuses/vanilla/tier4/novakidtier4setbonuseffect.lua
+++ stats/effects/fu_armoreffects/set_bonuses/vanilla/tier4/novakidtier4setbonuseffect.lua
@@ -1,19 +1,23 @@
 require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
 setName="fu_novakidtier4setnew"
 
-weaponBonus={
-	{stat = "powerMultiplier", effectiveMultiplier = 1.19}
-}
+-- weaponBonus={
+	-- {stat = "powerMultiplier", effectiveMultiplier = 1.19}
+-- }
 
 armorBonus={
+	{stat = "shotgunMastery", amount = 0.19},
+	{stat = "sniperrifleMastery", amount = 0.19},
+	{stat = "rifleMastery", amount = 0.19},
+	{stat = "pistolMastery", amount = 0.19},
 	{stat = "shadowImmunity", amount = 1}
 }
 
 function init()
 	setSEBonusInit(setName)
-	effectHandlerList.weaponBonusHandle=effect.addStatModifierGroup({})
+	-- effectHandlerList.weaponBonusHandle=effect.addStatModifierGroup({})
 
-	checkWeapons()
+	-- checkWeapons()
 
 	effectHandlerList.armorBonusHandle=effect.addStatModifierGroup(armorBonus)
 end
@@ -21,18 +25,18 @@ end
 function update(dt)
 	if not checkSetWorn(self.setBonusCheck) then
 		effect.expire()
-	else
+	-- else
 
-		checkWeapons()
+		-- checkWeapons()
 	end
 end
 
-function checkWeapons()
-	local weapons=weaponCheck({"shotgun","sniperrifle","rifle","pistol"})
+-- function checkWeapons()
+	-- local weapons=weaponCheck({"shotgun","sniperrifle","rifle","pistol"})
 
-	if weapons["either"] then
-		effect.setStatModifierGroup(effectHandlerList.weaponBonusHandle,weaponBonus)
-	else
-		effect.setStatModifierGroup(effectHandlerList.weaponBonusHandle,{})
-	end
-end
+	-- if weapons["either"] then
+		-- effect.setStatModifierGroup(effectHandlerList.weaponBonusHandle,weaponBonus)
+	-- else
+		-- effect.setStatModifierGroup(effectHandlerList.weaponBonusHandle,{})
+	-- end
+-- end
--- stats/effects/fu_armoreffects/set_bonuses/vanilla/tier6/novakidtier6setbonuseffect.lua
+++ stats/effects/fu_armoreffects/set_bonuses/vanilla/tier6/novakidtier6setbonuseffect.lua
@@ -1,19 +1,23 @@
 require "/stats/effects/fu_armoreffects/setbonuses_common.lua"
 setName="fu_novakidtier6setnew"
 
-weaponBonus={
-	{stat = "powerMultiplier", effectiveMultiplier = 1.25}
-}
+-- weaponBonus={
+	-- {stat = "powerMultiplier", effectiveMultiplier = 1.25}
+-- }
 
 armorBonus={
+	{stat = "shotgunMastery", amount = 0.25},
+	{stat = "sniperrifleMastery", amount = 0.25},
+	{stat = "rifleMastery", amount = 0.25},
+	{stat = "pistolMastery", amount = 0.25},
 	{stat = "shadowImmunity", amount = 1}
 }
 
 function init()
 	setSEBonusInit(setName)
-	effectHandlerList.weaponBonusHandle=effect.addStatModifierGroup({})
+	-- effectHandlerList.weaponBonusHandle=effect.addStatModifierGroup({})
 
-	checkWeapons()
+	-- checkWeapons()
 
 	effectHandlerList.armorBonusHandle=effect.addStatModifierGroup(armorBonus)
 end
@@ -21,18 +25,18 @@ end
 function update(dt)
 	if not checkSetWorn(self.setBonusCheck) then
 		effect.expire()
-	else
+	-- else
 
-		checkWeapons()
+		-- checkWeapons()
 	end
 end
 
-function checkWeapons()
-	local weapons=weaponCheck({"shotgun","sniperrifle","rifle","pistol"})
+-- function checkWeapons()
+	-- local weapons=weaponCheck({"shotgun","sniperrifle","rifle","pistol"})
 
-	if weapons["either"] then
-		effect.setStatModifierGroup(effectHandlerList.weaponBonusHandle,weaponBonus)
-	else
-		effect.setStatModifierGroup(effectHandlerList.weaponBonusHandle,{})
-	end
-end
+	-- if weapons["either"] then
+		-- effect.setStatModifierGroup(effectHandlerList.weaponBonusHandle,weaponBonus)
+	-- else
+		-- effect.setStatModifierGroup(effectHandlerList.weaponBonusHandle,{})
+	-- end
+-- end
