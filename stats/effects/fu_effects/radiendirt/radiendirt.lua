79cc7866c07c42cc69568e82372438ef3dfb9eaa
Kherae <cwaldridge@live.com>
2024-10-12 07:19:50 -0700

    dirt drunkenness

--- stats/effects/fu_effects/radiendirt/radiendirt.lua
+++ stats/effects/fu_effects/radiendirt/radiendirt.lua
@@ -5,14 +5,14 @@ function init()
 	animator.setParticleEmitterEmissionRate("healing", config.getParameter("emissionRate", 3))
 	animator.setParticleEmitterActive("healing", true)
 
-	self.frEnabled=status.statusProperty("fr_enabled")
-	self.species = status.statusProperty("fr_race") or world.entitySpecies(entity.id())
+	-- self.frEnabled=status.statusProperty("fr_enabled")
+	-- self.species = status.statusProperty("fr_race") or world.entitySpecies(entity.id())
 	self.didInit=true
 end
 
 function update(dt)
 	if not self.didInit then init() end
-	if self.frEnabled and (self.species == "radien") then
+	if status.statPositive("fuDirtBeer") then
 		status.addEphemeralEffect("booze3", 240, entity.id())
 		status.addEphemeralEffect("slow", 240, entity.id())
 		status.addEphemeralEffect("maxhealthboostneg20", 240, entity.id())
