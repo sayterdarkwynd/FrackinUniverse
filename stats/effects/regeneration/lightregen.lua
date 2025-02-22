4e274371deb46ac75dfe5693861efd01576420f6
Edward Chernenko <edwardspec@gmail.com>
2025-02-22 18:47:12 +0300

    Made "Heal in Light" effect act as what player would expect from name
    New behavior: heals when light >0.25, doesn't heal if light <0.25.
    
    Behavior before this change:
    - it healed in complete darkness if it's underground
    - it didn't heal in highly illumnated areas during the night

--- stats/effects/regeneration/lightregen.lua
+++ stats/effects/regeneration/lightregen.lua
@@ -4,33 +4,20 @@ require "/scripts/interp.lua"
 require "/stats/effects/fu_statusUtil.lua"
 
 function init()
-	self.healingRate = 1.01 / config.getParameter("healTime", 320)
 	script.setUpdateDelta(5)
 	self.healTime=config.getParameter("healTime", 140)
 	bonusHandler=effect.addStatModifierGroup({})
 end
 
 function update(dt)
-	local daytime = daytimeCheck()
-	local underground = undergroundCheck()
 	local lightLevel = getLight()
+	local healingRate = 0.0
 
-	if daytime then
-		if underground and lightLevel < 40 then
-			self.healingRate = 1.0009 / self.healTime
-		elseif underground and lightLevel > 40 then
-			self.healingRate = 1.001 / self.healTime
-		else
-		    if lightLevel > 25 then
-				self.healingRate=((((lightLevel-25.0)/37.5)+1.0)/self.healTime)
-			else
-				self.healingRate=0.0
-			end
-		end
-	else
-		self.healingRate=0.0
+	if lightLevel > 25 then
+		healingRate = ((((lightLevel-25.0)/37.5)+1.0)/self.healTime)
 	end
-	effect.setStatModifierGroup(bonusHandler,{{stat="healthRegen",amount=status.resourceMax("health")*self.healingRate*math.max(0,1+status.stat("healingBonus"))}})
+
+	effect.setStatModifierGroup(bonusHandler,{{stat="healthRegen",amount=status.resourceMax("health")*healingRate*math.max(0,1+status.stat("healingBonus"))}})
 end
 
 function uninit()
