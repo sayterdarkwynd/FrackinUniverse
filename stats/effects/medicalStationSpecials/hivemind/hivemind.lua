d70f254b153a3fe950279a44032d8d156f871ded
Kherae <cwaldridge@live.com>
2026-07-02 01:17:19 -0700

    better

--- stats/effects/medicalStationSpecials/hivemind/hivemind.lua
+++ stats/effects/medicalStationSpecials/hivemind/hivemind.lua
@@ -1,69 +1,97 @@
 
 require "/stats/effects/medicalStationSpecials/medicalStatusBase.lua"
 require "/scripts/vec2.lua"
+require "/scripts/util.lua"
+
+local intervalMult=10
 
 function init()
 	self.dummyStatus = "medicalhiveminddummy"
+	baseInit()
 	self.healthDrainPcnt = config.getParameter("healthDrainPcnt") or 0
 	self.damagePercent = config.getParameter("damagePercent") or 0
 	self.minHealthPcnt = config.getParameter("minHealthPcnt") or 0
 	self.powerScalar= config.getParameter("powerScalar") or 0.5
 	self.drainTimer = 0
 	self.spawnSearchRadius = config.getParameter("spawnSearchRadius") or 0
-	self.spawnTimer = 0
-	self.spawnInterval=config.getParameter("spawnInterval") or 0
+	self.spawnInterval=config.getParameter("spawnInterval") or 1
 	self.controlForce=config.getParameter("controlForce") or 30
 	self.maxSpeed=config.getParameter("maxSpeed") or 14
 	self.recentTargets={}
-	baseInit()
+	-- self.fireTimer=0.0
 end
 
-function update(dt)
-	if self.spawnTimer <= 0 then
-		for id,timer in pairs(self.recentTargets) do
-			self.recentTargets[id]=timer-self.spawnInterval
-			if self.recentTargets[id]<=0 then
-				self.recentTargets[id]=nil
-			end
+function findOldest(tab)
+	local workcopy=copy(tab)
+	local lowid
+	local lowtime
+	for i,t in pairs(workcopy) do
+		if not lowid then
+			lowid = i
+			lowtime = t
+		elseif t<lowtime then
+			lowtime=t
+			lowid=i
 		end
+	end
+	return lowid
+end
 
-		local currentTarget
-		local targets=world.entityQuery(entity.position(), self.spawnSearchRadius, { order = "nearest",includedTypes={"creature"}})
-		for _, monsterID in ipairs(targets) do
-			if not self.recentTargets[monsterID] then
-				self.recentTargets[monsterID]=self.spawnInterval*5
+function findTarget()
+	for monsterID,timer in pairs(self.recentTargets) do
+		if (not entity.isValidTarget(monsterID)) or (not entity.entityInSight(monsterID)) then
+			self.recentTargets[monsterID]=nil
+		end
+	end
+
+	local currentTarget
+	local targets=world.entityQuery(entity.position(), self.spawnSearchRadius, { order = "nearest",includedTypes={"creature"}, withoutEntityId=entity.id()})
+
+	for _, monsterID in ipairs(targets) do
+		if (entity.isValidTarget(monsterID) and entity.entityInSight(monsterID)) then
+			if (not self.recentTargets[monsterID]) then
+				self.recentTargets[monsterID]=self.spawnInterval*intervalMult
 				currentTarget=monsterID
 				break
 			end
 		end
-		if not currentTarget then
-			local incr=1
-			for _, monsterID in ipairs(targets) do
-				if self.recentTargets[monsterID]<=self.spawnInterval*incr then
-					self.recentTargets[monsterID]=self.spawnInterval*5
-					currentTarget=monsterID
-					break
-				end
-			end
-			incr=incr+1
-		end
-
-		if currentTarget then
-			if status.resourcePercentage("health") > self.minHealthPcnt and entity.isValidTarget(currentTarget) and entity.entityInSight(currentTarget) then
-				world.spawnProjectile("hivemindspawn", entity.position(), entity.id(), vec2.rotate({1, 0}, vec2.angle(vec2.sub(world.entityPosition(currentTarget), entity.position()))), nil,{
-						controlForce=self.controlForce,maxSpeed=self.maxSpeed,spawnSearchRadius=self.spawnSearchRadius,target=currentTarget,
-						power = status.stat("maxHealth") * self.damagePercent * (((status.stat("powerMultiplier")-1.0)*self.powerScalar)+1.0)
-					}
-				)
-				status.consumeResource("health", status.resourceMax("health") * self.healthDrainPcnt)
-			end
-		end
-
-		self.spawnTimer = self.spawnInterval
-	else
-		self.spawnTimer = self.spawnTimer - dt
 	end
 
+	if not currentTarget then
+		currentTarget=findOldest(self.recentTargets)
+	end
+	return currentTarget
+end
+
+function fire(dt)
+	if status.resourcePercentage("health") > self.minHealthPcnt and (self.fireTimer or 0)<=0 then
+		local currentTarget=findTarget()
+		if currentTarget then
+			world.spawnProjectile("hivemindspawn", entity.position(), entity.id(), vec2.rotate({1, 0}, vec2.angle(vec2.sub(world.entityPosition(currentTarget), entity.position()))), nil,{
+					controlForce=self.controlForce,maxSpeed=self.maxSpeed,spawnSearchRadius=self.spawnSearchRadius,target=currentTarget,
+					power = status.stat("maxHealth") * self.damagePercent * (((status.stat("powerMultiplier")-1.0)*self.powerScalar)+1.0)
+				}
+			)
+			status.consumeResource("health", status.resourceMax("health") * self.healthDrainPcnt)
+
+			self.fireTimer=self.spawnInterval
+		end
+	end
+end
+
+function updateTimers(dt)
+	self.fireTimer=math.max(0,(self.fireTimer or 0)-dt)
+	for id,timer in pairs(self.recentTargets) do
+		self.recentTargets[id]=self.recentTargets[id]-dt
+		if self.recentTargets[id]<=0 then
+			self.recentTargets[id]=nil
+		end
+	end
+end
+
+function update(dt)
+	updateTimers(dt)
+	fire(dt)
 	baseUpdate(dt)
 end
 
